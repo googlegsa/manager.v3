1aea7aed91a47b47c6d810cf61843f418a7da59a
donald.ziff <donald.ziff@56db1fc5-3b1b-0410-9cff-4307dac5e09e>
2006-08-23 23:51:57 +0000

    Meghna, could you review?
    New package: jcradaptor.  This adapts a jcr-implementation as a connector.spi implementation.  This could be used by customers or 3rd-party developers to adapt to existing jcr implementations.  For us, we will use it with the MockRepository and MockJcrRepository for writing stand-alone unit tests.  We could also use it to write test connectors to open-source jcr-compliant repositories, such as alfresco and jackrabbit.
    
    The hardest part is implementing resumeTraversal().  Suppose the last repository has multiple documents that have the same date stamp.  This can happen through bulk-load or some kinds of backup-restore.  If the query for "documents-modified-since" uses a > (strictly-greater-than) operator, there's a danger that some documents might be missed.  If it uses a >= (greater-than-or-equal) operator, there's a danger that no progress will be made, it might process the same documents over and over.
    
    Thus (using SQL here for clarity), it is important to do a query like this:
       select ... from documents where lastModified >= date-constant order by lastModified, documentid
    That way, the >= makes sure you don't miss anything while the second order-by clause allows you to skip up to the last one you processed, avoiding repeatedly processing the same documents and the danger of making no progress.
    
    This is reflected primarily in SpiQueryTraversalManagerFromJcr.java, which generates the query (xpath, not sql, sadly), and advances through the result to the right point.  There is also complication in SpiResultSetFromJcr.java, where we need to compensate for the fact the java Iterators do not permit you to take something, then push it back.
    
    The other classes are quite straightforward mappings from jcr classes to nearly identical spi classes.
    
    In addition, test data files are in this check-in - I think they should have gone in earlier.  Also, a useful end-to-end traversal test.

---
+++ trunk/projects/connector-manager/source/javatests/com/google/enterprise/connector/jcradaptor/SpiSessionFromJcr.java
@@ -0,0 +1,59 @@
+// Copyright (C) 2006 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.enterprise.connector.jcradaptor;
+
+import com.google.enterprise.connector.spi.AuthenticationManager;
+import com.google.enterprise.connector.spi.AuthorizationManager;
+import com.google.enterprise.connector.spi.QueryTraversalManager;
+import com.google.enterprise.connector.spi.RepositoryException;
+import com.google.enterprise.connector.spi.Session;
+
+import javax.jcr.Workspace;
+import javax.jcr.query.QueryManager;
+
+/**
+ * Adaptor to JCR class of the same name
+ */
+public class SpiSessionFromJcr implements Session {
+
+  javax.jcr.Session session;
+
+  public SpiSessionFromJcr(javax.jcr.Session session) {
+    this.session = session;
+  }
+
+  public QueryTraversalManager getQueryTraversalManager()
+      throws RepositoryException {
+    Workspace workspace = session.getWorkspace();
+    QueryManager queryManager = null;
+    try {
+      queryManager = workspace.getQueryManager();
+    } catch (javax.jcr.RepositoryException e) {
+      throw new RepositoryException(e);
+    }
+    return new SpiQueryTraversalManagerFromJcr(queryManager);
+  }
+
+  public AuthenticationManager getAuthenticationManager()
+      throws RepositoryException {
+    throw new UnsupportedOperationException();
+  }
+
+  public AuthorizationManager getAuthorizationManager()
+      throws RepositoryException {
+    throw new UnsupportedOperationException();
+  }
+
+}
