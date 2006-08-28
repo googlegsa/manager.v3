d33f25bcf75dd06420013dbd09d35f4598b4bed5
Max Ziff <donald.ziff@gmail.com>
2006-08-28 18:09:58 +0000

    Meghna, please review.
    The MockConnectorStateStore.java is trivial - just a HashMap.
    The pusher is more interesting, and it has its own minimal test.

---
+++ projects/connector-manager/source/javatests/com/google/enterprise/connector/pusher/MockPusherTest.java
@@ -0,0 +1,62 @@
+// Copyright (C) 2006 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.enterprise.connector.pusher;
+
+import com.google.enterprise.connector.jcradaptor.SpiPropertyMapFromJcr;
+import com.google.enterprise.connector.jcradaptor.SpiQueryTraversalManagerFromJcr;
+import com.google.enterprise.connector.mock.MockRepository;
+import com.google.enterprise.connector.mock.MockRepositoryDocument;
+import com.google.enterprise.connector.mock.MockRepositoryEventList;
+import com.google.enterprise.connector.mock.jcr.MockJcrNode;
+import com.google.enterprise.connector.mock.jcr.MockJcrQueryManager;
+import com.google.enterprise.connector.spi.PropertyMap;
+import com.google.enterprise.connector.spi.QueryTraversalManager;
+import com.google.enterprise.connector.spi.RepositoryException;
+import com.google.enterprise.connector.spi.ResultSet;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+import java.util.Iterator;
+
+import javax.jcr.query.QueryManager;
+
+public class MockPusherTest extends TestCase {
+  public void testSimple() throws RepositoryException {
+    MockRepositoryEventList mrel =
+        new MockRepositoryEventList("MockRepositoryEventLog1.txt");
+    MockRepository r = new MockRepository(mrel);
+    QueryManager qm = new MockJcrQueryManager(r.getStore());
+    QueryTraversalManager qtm = new SpiQueryTraversalManagerFromJcr(qm);
+    
+    MockPusher pusher = new MockPusher(System.out);
+
+    {
+      MockRepositoryDocument doc = r.getStore().getDocByID("doc4");
+      PropertyMap pm = new SpiPropertyMapFromJcr(new MockJcrNode(doc));
+
+      ResultSet resultSet = qtm.startTraversal();
+
+      int counter = 0;
+      for (Iterator iter = resultSet.iterator(); iter.hasNext();) {
+        PropertyMap propertyMap = (PropertyMap) iter.next();
+        pusher.take(propertyMap);
+        counter++;
+      }
+      Assert.assertEquals(4, counter);
+      Assert.assertEquals(4, pusher.getTotalDocs());
+    }
+  }
+}
