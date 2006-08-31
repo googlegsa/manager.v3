e22e267a8300994a182f895023c2f666ef6ebd73
grace.g.h <grace.g.h@56db1fc5-3b1b-0410-9cff-4307dac5e09e>
2006-08-31 20:20:36 +0000

    Phuong, please review.

---
+++ trunk/projects/connector-manager/source/java/com/google/enterprise/connector/servlet/TestConnectivity.java
@@ -0,0 +1,46 @@
+// Copyright (C) 2006 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
+package com.google.enterprise.connector.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class TestConnectivity extends HttpServlet {
+    protected void doGet(HttpServletRequest req,
+            HttpServletResponse res)
+    	throws ServletException, IOException
+   	{
+    	res.setContentType("text/xml");
+    	PrintWriter out = res.getWriter();
+        out.println("<CmResponse>" +
+                    "  <StatusId>0</StatusId>" +
+                    "</CmResponse>");
+        out.close();
+    }
+
+    protected void doPost(HttpServletRequest req,
+            HttpServletResponse res)
+    	throws ServletException, IOException
+   	{
+    	doGet(req, res);
+   	}
+}
