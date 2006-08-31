c7097ef3d99b48f97d24b74d5966edf6c0d6481f
danny.tom <danny.tom@56db1fc5-3b1b-0410-9cff-4307dac5e09e>
2006-08-31 17:10:02 +0000

    Scheduler module code.  Scheduler runs in its own thread and is in charge of performing operations at scheduled times.
    Max, please review.

---
+++ trunk/projects/connector-manager/source/java/com/google/enterprise/connector/scheduler/Schedule.java
@@ -0,0 +1,47 @@
+// Copyright 2006 Google Inc.  All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.enterprise.connector.scheduler;
+
+import java.util.List;
+
+/**
+ * A traversal schedule.
+ *
+ */
+public class Schedule {
+  private String connectorName;
+  private List timeIntervals;
+  
+  public Schedule(String connectorName, List timeIntervals) {
+    this.connectorName = connectorName;
+    this.timeIntervals = timeIntervals;
+  }
+  
+  public String getConnectorName() {
+    return connectorName;
+  }
+  
+  public void setConnectorName(String connectorName) {
+    this.connectorName = connectorName;
+  }
+
+  public List getTimeIntervals() {
+    return timeIntervals;
+  }
+  
+  public void setTimeIntervals(List timeIntervals) {
+    this.timeIntervals = timeIntervals;
+  }
+}
