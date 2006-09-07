2eac008e9e15741da65e24c7d5f89ef9912ee5a1
donald.ziff <donald.ziff@56db1fc5-3b1b-0410-9cff-4307dac5e09e>
2006-09-07 20:57:33 +0000

    Instantiator interface and mock object.
    Danny, please review.

---
+++ trunk/projects/connector-manager/source/javatests/com/google/enterprise/connector/instantiator/MockInstantiator.java
@@ -0,0 +1,80 @@
+// Copyright (C) 2006 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.enterprise.connector.instantiator;
+
+import com.google.enterprise.connector.jcradaptor.SpiQueryTraversalManagerFromJcr;
+import com.google.enterprise.connector.mock.MockRepository;
+import com.google.enterprise.connector.mock.MockRepositoryEventList;
+import com.google.enterprise.connector.mock.jcr.MockJcrQueryManager;
+import com.google.enterprise.connector.persist.ConnectorNotFoundException;
+import com.google.enterprise.connector.persist.ConnectorStateStore;
+import com.google.enterprise.connector.persist.ConnectorTypeNotFoundException;
+import com.google.enterprise.connector.persist.MockConnectorStateStore;
+import com.google.enterprise.connector.pusher.MockPusher;
+import com.google.enterprise.connector.spi.Configurer;
+import com.google.enterprise.connector.spi.QueryTraversalManager;
+import com.google.enterprise.connector.traversal.QueryTraverser;
+import com.google.enterprise.connector.traversal.Traverser;
+
+import javax.jcr.query.QueryManager;
+
+/**
+ * 
+ */
+public class MockInstantiator implements Instantiator {
+
+  private static final Configurer CONFIGURER;
+  private static final Traverser TRAVERSER;
+
+  static {
+    CONFIGURER = null;
+    
+    MockRepositoryEventList mrel =
+        new MockRepositoryEventList("MockRepositoryEventLog1.txt");
+    MockRepository r = new MockRepository(mrel);
+    QueryManager qm = new MockJcrQueryManager(r.getStore());
+
+    String connectorName = "foo";
+    QueryTraversalManager qtm = new SpiQueryTraversalManagerFromJcr(qm);
+    MockPusher pusher = new MockPusher(System.out);
+    ConnectorStateStore connectorStateStore = new MockConnectorStateStore();
+
+    TRAVERSER =
+        new QueryTraverser(pusher, qtm, connectorStateStore, connectorName);
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see com.google.enterprise.connector.instantiator.Instantiator
+   *      #getConfigurer(java.lang.String)
+   */
+  public Configurer getConfigurer(String connectorTypeName)
+      throws ConnectorTypeNotFoundException{
+    return CONFIGURER;
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see com.google.enterprise.connector.instantiator.Instantiator
+   *      #getTraverser(java.lang.String)
+   */
+  public Traverser getTraverser(String connectorName)
+      throws ConnectorNotFoundException {
+    return TRAVERSER;
+  }
+
+}
