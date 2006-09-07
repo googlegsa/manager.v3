140104d168d858e991f881cc4fa5c02f3e2e8cba
grace.g.h <grace.g.h@56db1fc5-3b1b-0410-9cff-4307dac5e09e>
2006-09-07 21:34:07 +0000

    Updated to conform with newly changed Manager interface and merged with Danny's r45.
    Danny, Please review.
    
    Thanks,
    Grace

--- trunk/projects/connector-manager/source/java/com/google/enterprise/connector/manager/MockManager.java
+++ trunk/projects/connector-manager/source/java/com/google/enterprise/connector/manager/MockManager.java
@@ -12,19 +12,23 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-
 package com.google.enterprise.connector.manager;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
 import com.google.enterprise.connector.persist.ConnectorNotFoundException;
 import com.google.enterprise.connector.persist.ConnectorTypeNotFoundException;
+import com.google.enterprise.connector.persist.PersistentStoreException;
 import com.google.enterprise.connector.spi.ConfigureResponse;
 
 /**
- * Implement interface Manager.
+ * A mock implementation for the Manager interface. This implementation
+ * basically hardcodes a bunch of responses and can be used before the actual
+ * implementation is complete.
+ * 
  * TODO: move to javatests when possible.
  * 
  */
@@ -38,62 +42,143 @@ public class MockManager implements Manager {
 		return INSTANCE;
 	}
 
-	public boolean authenticate(String connectorInstanceName, String username,
-			String password) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	public List authorizeDocids(String connectorInstanceName, List docidList,
-			String username) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.google.enterprise.connector.manager.Manager#authorizeTokens(java.lang.String,
+	 *      java.util.List, java.lang.String)
+	 */
 	public List authorizeTokens(String connectorInstanceName, List tokenList,
 			String username) {
-		// TODO Auto-generated method stub
-		return null;
+		List tokenAuthList = new ArrayList();
+		for (int i = 0; i < tokenList.size(); i++) {
+			tokenAuthList.add(new Boolean(true));
+		}
+		return tokenAuthList;
 	}
 
-	public List getConnectorStatuses() {
-		// TODO Auto-generated method stub
-		return null;
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.google.enterprise.connector.manager.Manager#authenticate(java.lang.String,
+	 *      java.lang.String, java.lang.String)
+	 */
+	public boolean authenticate(String connectorInstanceName, String username,
+			String password) {
+		return true;
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.google.enterprise.connector.manager.Manager#authorizeDocids(java.lang.String,
+	 *      java.util.List, java.lang.String)
+	 */
+	public List authorizeDocids(String connectorInstanceName, List docidList,
+			String username) {
+		List docidAuthList = new ArrayList();
+		for (int i = 0; i < docidList.size(); i++) {
+			docidAuthList.add(new Boolean(true));
+		}
+		return docidAuthList;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.google.enterprise.connector.manager.Manager#getConnectorTypes()
+	 */
 	public List getConnectorTypes() {
-		// TODO Auto-generated method stub
-		List connectorTypes = new ArrayList();
-		connectorTypes.add("documentum");
-		connectorTypes.add("sharepoint");
-		return connectorTypes;
+		return Arrays.asList(new String[] { "Documentum", "Sharepoint",
+				"Filenet" });
 	}
 
 	public void storeConfig(boolean certAuth, String feederGateHost,
 			int feederGatePort, int maxFeedRate) {
-		// TODO Auto-generated method stub
-
 	}
 
-	public ConfigureResponse getConfigForm(String ConnectorType, String language) throws ConnectorTypeNotFoundException, ConnectorManagerException {
-		// TODO Auto-generated method stub
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.google.enterprise.connector.manager.Manager#getConfigForm(java.lang.String,
+	 *      java.lang.String)
+	 */
+	public ConfigureResponse getConfigForm(String ConnectorType, String language)
+			throws ConnectorTypeNotFoundException {
+		// have caller use default form by returning null
 		return null;
 	}
 
-	public ConfigureResponse getConfigFormForConnector(String connectorName, String language) throws ConnectorNotFoundException, ConnectorManagerException {
-		// TODO Auto-generated method stub
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.google.enterprise.connector.manager.Manager#getConfigFormForConnector(java.lang.String,
+	 *      java.lang.String)
+	 */
+	public ConfigureResponse getConfigFormForConnector(String connectorName,
+			String language) throws ConnectorNotFoundException {
+		// have caller use default form by returning null
 		return null;
 	}
 
-	public ConnectorStatus getConnectorStatus(String connectorInstanceName) throws ConnectorManagerException {
-		// TODO Auto-generated method stub
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.google.enterprise.connector.manager.Manager#getConnectorStatus(java.lang.String)
+	 */
+	public ConnectorStatus getConnectorStatus(String connectorInstanceName) {
+		String name = connectorInstanceName;
+		String type = "Documentum";
+		int status = 0;
+		return new ConnectorStatus(name, type, status);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.google.enterprise.connector.manager.Manager#getConnectorStatuses()
+	 */
+	public List getConnectorStatuses() {
+		List statuses = new ArrayList();
+		statuses.add(getConnectorStatus("foo"));
+		return statuses;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.google.enterprise.connector.manager.Manager#setConfig(java.lang.String,
+	 *      java.util.Map, java.lang.String)
+	 */
+	public ConfigureResponse setConfig(String connectorName, Map configData,
+			String language) throws ConnectorNotFoundException,
+			ConnectorManagerException {
 		return null;
 	}
 
-	public ConfigureResponse setConfig(String connectorName, Map configData, String language) throws ConnectorNotFoundException, ConnectorManagerException {
-		// TODO Auto-generated method stub
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.google.enterprise.connector.manager.Manager#setConfig(java.lang.String,
+	 *      java.util.Map, java.lang.String)
+	 */
+	public ConfigureResponse setConnectorConfig(String connectorName,
+			Map configData, String language) throws ConnectorNotFoundException,
+			PersistentStoreException {
+		// null is a success response
 		return null;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.google.enterprise.connector.manager.Manager#storeConfig(boolean,
+	 *      java.lang.String, int, int)
+	 */
+	public void setConnectorManagerConfig(boolean certAuth,
+			String feederGateHost, int feederGatePort, int maxFeedRate)
+			throws PersistentStoreException {
+		// do nothing
 	}
 
 }
-
