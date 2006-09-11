26470c9de651cb6f78304ee554ef2d22ef04594e
donald.ziff <donald.ziff@56db1fc5-3b1b-0410-9cff-4307dac5e09e>
2006-09-11 21:24:45 +0000

    Danny, please review.  You may want to wait until I send the writely document explaining the methodology - should be later today.
    First instantiator check-in.
    
    Two new projects: test-connector1 and test-connector2.  At present, all each one has is a build.xml, a config/conf/connectorType.xml and some empty source directories.  The build creates a jar with just the conf/connectorType.xml.  This is what Spring looks for to instantiate connector Types: specifically, all classpath resources that match conf/connectorType.xml.
    
    The new file ConnectorTypeInstantiator.java is the actual instantiation code.  The corresponding test exercises this code.  For this test to succeed you must build the two test projects and add their jars to your classpath.  
    
    There are two Spring jars added to third_party: spring.jar and sprin-context.jar.  There are many more spring jars that correspond to features we aren't using (so far).  I'll add them as needed.

---
+++ trunk/projects/connector-manager/source/javatests/com/google/enterprise/connector/instantiator/ConnectorTypeInstantiatorTest.java
@@ -0,0 +1,98 @@
+// Copyright (C) 2006 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.enterprise.connector.instantiator;
+
+import com.google.enterprise.connector.persist.ConnectorTypeNotFoundException;
+import com.google.enterprise.connector.spi.ConnectorType;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+/**
+ * Tests for ConnectorTypeInstantiator. These tests rely on setup: the jars for
+ * TestConnector1 and TestConnector2 should be installed on the classpath
+ */
+public class ConnectorTypeInstantiatorTest extends TestCase {
+
+  /**
+   * Test method for
+   * {@link com.google.enterprise.connector.instantiator.ConnectorTypeInstantiator
+   * #getConnectorType(java.lang.String)}.
+   */
+  public final void testGetConnectorType() {
+    ConnectorTypeInstantiator connectorTypeInstantiator =
+        new ConnectorTypeInstantiator();
+    verifyConnectorTypeFound(connectorTypeInstantiator, "TestConnector1");
+    verifyConnectorTypeFound(connectorTypeInstantiator, "TestConnector2");
+    verifyConnectorTypeNotFound(connectorTypeInstantiator, "NoConnector");
+  }
+
+  private void verifyConnectorTypeFound(
+      ConnectorTypeInstantiator connectorTypeInstantiator,
+      String connectorTypeName) {
+    ConnectorType connectorType = null;
+    try {
+      connectorType =
+          connectorTypeInstantiator.getConnectorType(connectorTypeName);
+    } catch (ConnectorTypeNotFoundException e) {
+      fail("Should find connector type " + connectorTypeName);
+    }
+    Assert.assertNotNull(connectorType);
+  }
+
+  private void verifyConnectorTypeNotFound(
+      ConnectorTypeInstantiator connectorTypeInstantiator,
+      String connectorTypeName) {
+    ConnectorType connectorType = null;
+    try {
+      connectorType =
+          connectorTypeInstantiator.getConnectorType(connectorTypeName);
+    } catch (ConnectorTypeNotFoundException e) {
+      return;
+    }
+    fail("Should find connector type " + connectorTypeName);
+  }
+
+  /**
+   * Test method for
+   * {@link com.google.enterprise.connector.instantiator.ConnectorTypeInstantiator
+   * #getConnectorTypeNames()}.
+   */
+  public final void testGetConnectorTypeNames() {
+    String[] expectedConnectorTypeNames =
+        new String[] {"TestConnector1", "TestConnector2"};
+    Set expectedNamesSet =
+        new HashSet(Arrays.asList(expectedConnectorTypeNames));
+    ConnectorTypeInstantiator connectorTypeInstantiator =
+        new ConnectorTypeInstantiator();
+    Set actualNames = new HashSet();
+    int counter = 0;
+    for (Iterator i = connectorTypeInstantiator.getConnectorTypeNames(); i
+        .hasNext(); counter++) {
+      String connectorTypeName = (String) i.next();
+      verifyConnectorTypeFound(connectorTypeInstantiator, connectorTypeName);
+      actualNames.add(connectorTypeName);
+    }
+    Assert.assertTrue(actualNames.containsAll(expectedNamesSet));
+    // we purposely don't assert the converse so that this test will continue to
+    // work even if other connector types are installed
+  }
+
+}
