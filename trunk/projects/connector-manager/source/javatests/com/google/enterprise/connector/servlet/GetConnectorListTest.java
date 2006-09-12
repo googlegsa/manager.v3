ce92d46ab2c39f003928cc03656deeff1e4c2b15
grace.g.h <grace.g.h@56db1fc5-3b1b-0410-9cff-4307dac5e09e>
2006-09-12 17:47:21 +0000

    Added admin servlet class GetConnectorStatus and unit tests. Modified TestConnectivity servlet.
    Max, please review.
    
    Thanks,
    Grace

--- trunk/projects/connector-manager/source/javatests/com/google/enterprise/connector/servlet/GetConnectorListTest.java
+++ trunk/projects/connector-manager/source/javatests/com/google/enterprise/connector/servlet/GetConnectorListTest.java
@@ -1,3 +1,17 @@
+// Copyright 2006 Google Inc.  All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package com.google.enterprise.connector.servlet;
 
 import junit.framework.Assert;
@@ -10,6 +24,10 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.logging.Logger;
 
+/**
+ * Tests GetConnectorList servlet class.
+ *
+ */
 public class GetConnectorListTest extends TestCase {
   private static final Logger logger = Logger
     .getLogger(GetConnectorListTest.class.getName());
@@ -17,7 +35,7 @@ public class GetConnectorListTest extends TestCase {
   /**
    * Test method for
    * {@link com.google.enterprise.connector.servlet.GetConnectorList 
-   * #handleDoGet
+   * #handleDoGet(java.io.PrintWriter, java.util.List)
    * @throws IOException
    * 
    * connectorTypes = null
@@ -40,7 +58,7 @@ public class GetConnectorListTest extends TestCase {
    * 
    * connectorTypes = {"Documentum", "Sharepoint", "Filenet"}
    */
-  public void testHandleDo2() throws IOException {
+  public void testHandleDoGet2() throws IOException {
     List connectorTypes = Arrays.asList(new String[]{
         "Documentum", "Sharepoint", "Filenet"});
     String expectedResult =
