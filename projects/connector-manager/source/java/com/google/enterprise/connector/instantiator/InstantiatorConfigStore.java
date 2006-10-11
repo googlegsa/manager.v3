717215e25c3a5147ba4e5cee9edcd38c96ff8122
Max Ziff <donald.ziff@gmail.com>
2006-10-11 02:53:17 +0000

    This file was missing from r180.  Danny, please review this with r180.

---
+++ projects/connector-manager/source/java/com/google/enterprise/connector/instantiator/InstantiatorConfigStore.java
@@ -0,0 +1,8 @@
+package com.google.enterprise.connector.instantiator;
+
+import com.google.enterprise.connector.persist.ConnectorConfigStore;
+
+public interface InstantiatorConfigStore extends Instantiator,
+    ConnectorConfigStore {
+
+}
