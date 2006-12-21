d54ade124fdc1eb859a4710458c77ac3c0a61d32
Max Ziff <donald.ziff@gmail.com>
2006-12-21 00:58:07 +0000

    Remove sharepoint connector.  It is moving to its own code site.

--- projects/sharepoint-connector/source/javatests/com/google/enterprise/connector/sharepoint/ICase.java
+++
@@ -1,7 +0,0 @@
-package com.google.enterprise.connector.sharepoint;
-
-import com.google.enterprise.connector.sharepoint.impl.ClientContext;
-
-public interface ICase {
-	void perform(ClientContext context, Object obj);
-}
