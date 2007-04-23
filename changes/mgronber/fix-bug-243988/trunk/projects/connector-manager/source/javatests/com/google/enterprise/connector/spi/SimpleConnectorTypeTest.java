2e01b09e90fcc8accf8e05ec15a5678ef0e781d6
mgronber <mgronber@56db1fc5-3b1b-0410-9cff-4307dac5e09e>
2007-04-23 17:27:42 +0000

    Fix Issue #243988: Working directory undefined in servlet context.
    * source/java/com/google/enterprise/connector/servlet/UpdateConnector.java
      (handleDoGet): Added extra contextPath arg and removed use of hard
      coded servlet context and replaced with given arg 
    
    * source/java/com/google/enterprise/connector/manager/Context.java
      (getCommonDirPath): Removed use of hard coded working directory and
      replaced with call to servletContext.
    
    * source/javatests/com/google/enterprise/connector/servlet/UpdateConnectorNoGSA.java
      (doGet): Added new contextPath arg to UpdateConnector.handleDoGet()
    
    * source/javatests/com/google/enterprise/connector/servlet/UpdateConnectorTest.java
      (TestHandledoGet): Added new contextPath arg to UpdateConnector.handleDoGet()
    
    Patch by: M. Gronberg <martyg@google.com>

---
+++ changes/mgronber/fix-bug-243988/trunk/projects/connector-manager/source/javatests/com/google/enterprise/connector/spi/SimpleConnectorTypeTest.java
@@ -0,0 +1,169 @@
+// Copyright (C) 2006 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.enterprise.connector.spi;
+
+import com.google.enterprise.connector.test.JsonObjectAsMap;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Map;
+
+/**
+ * 
+ */
+public class SimpleConnectorTypeTest extends TestCase {
+
+  /**
+   * Test method for
+   * {@link com.google.enterprise.connector.spi.SimpleConnectorType
+   * #getConfigForm(java.lang.String)}.
+   */
+  public final void testGetConfigForm() {
+    {
+      SimpleConnectorType simpleConnectorType = new SimpleConnectorType();
+      simpleConnectorType.setConfigKeys(new String[] {"foo", "bar"});
+      ConfigureResponse configureResponse = simpleConnectorType
+          .getConfigForm(null);
+      String initialConfigForm = configureResponse.getFormSnippet();
+      String expectedResult = "<tr>\r\n" + "<td>foo</td>\r\n"
+          + "<td><input type=\"text\" name=\"foo\"/></td>\r\n" + "</tr>\r\n"
+          + "<tr>\r\n" + "<td>bar</td>\r\n"
+          + "<td><input type=\"text\" name=\"bar\"/></td>\r\n" + "</tr>\r\n";
+      Assert.assertEquals(expectedResult, initialConfigForm);
+    }
+    {
+      SimpleConnectorType simpleConnectorType = new SimpleConnectorType();
+      simpleConnectorType.setConfigKeys(new String[] {"user", "password"});
+      ConfigureResponse configureResponse = simpleConnectorType
+          .getConfigForm(null);
+      String initialConfigForm = configureResponse.getFormSnippet();
+      String expectedResult = "<tr>\r\n" + "<td>user</td>\r\n"
+          + "<td><input type=\"text\" name=\"user\"/></td>\r\n" + "</tr>\r\n"
+          + "<tr>\r\n" + "<td>password</td>\r\n"
+          + "<td><input type=\"password\" name=\"password\"/></td>\r\n"
+          + "</tr>\r\n";
+      Assert.assertEquals(expectedResult, initialConfigForm);
+    }
+  }
+
+  /**
+   * Test method for
+   * {@link com.google.enterprise.connector.spi.SimpleConnectorType#validateConfig(java.util.Map, java.lang.String)}.
+   * 
+   * @throws JSONException
+   */
+  public final void testValidateConfig() throws JSONException {
+    {
+      SimpleConnectorType simpleConnectorType = new SimpleConnectorType();
+      simpleConnectorType.setConfigKeys(new String[] {"user", "password"});
+      JSONObject jo = new JSONObject(
+          "{user:max, dog:snickers, destination:heaven}");
+      Map map = new JsonObjectAsMap(jo);
+      ConfigureResponse configureResponse = simpleConnectorType.validateConfig(
+          map, null);
+      String configForm = configureResponse.getFormSnippet();
+      String expectedResult = 
+    	  "<tr>\r\n"
+          + "<td>user</td>\r\n"
+          + "<td><input type=\"text\" value=\"max\" name=\"user\"/></td>\r\n"
+          + "</tr>\r\n"
+          + "<tr>\r\n"
+          + "<td><font color=red>password</font></td>\r\n"
+          + "<td><input type=\"password\" name=\"password\"/></td>\r\n"
+          + "</tr>\r\n"
+          + "<input type=\"hidden\" value=\"heaven\" name=\"destination\"/>\r\n"
+          + "<input type=\"hidden\" value=\"snickers\" name=\"dog\"/>\r\n" + "";
+      Assert.assertEquals(expectedResult, configForm);
+      String message = configureResponse.getMessage();
+      Assert.assertTrue(message.length() > 0);
+    }
+
+    {
+      SimpleConnectorType simpleConnectorType = new SimpleConnectorType();
+      simpleConnectorType.setConfigKeys(new String[] {"user", "password"});
+      JSONObject jo = new JSONObject("{user:max, password:xyzzy, dog:snickers}");      
+      Map map = new JsonObjectAsMap(jo);
+      ConfigureResponse configureResponse = simpleConnectorType.validateConfig(
+          map, null);
+      Assert.assertNull(configureResponse);
+    }
+    
+    {
+      TestConnectorType testConnectorType = new TestConnectorType();
+      testConnectorType.setConfigKeys(new String[] {"user", "password"});
+      JSONObject jo = new JSONObject("{user:max, password:xyzzy}");      
+      Map map = new JsonObjectAsMap(jo);
+      ConfigureResponse configureResponse = testConnectorType.validateConfig(
+          map, null);
+      String configForm = configureResponse.getFormSnippet();
+      String expectedResult = 
+        "<tr>\r\n"
+          + "<td>user</td>\r\n"
+          + "<td><input type=\"text\" value=\"max\" name=\"user\"/></td>\r\n"
+          + "</tr>\r\n"
+          + "<tr>\r\n"
+          + "<td><font color=red>password</font></td>\r\n"
+          + "<td><input type=\"password\" name=\"password\"/></td>\r\n"
+          + "</tr>\r\n";
+      Assert.assertEquals(expectedResult, configForm);
+      String message = configureResponse.getMessage();
+      Assert.assertTrue(message.length() > 0);
+    }
+  }
+
+  /**
+   * Test method for
+   * {@link com.google.enterprise.connector.spi.SimpleConnectorType#
+   * getPopulatedConfigForm(java.util.Map, java.lang.String)}.
+   * 
+   * @throws JSONException
+   */
+  public final void testGetPopulatedConfigForm() throws JSONException {
+    {
+      SimpleConnectorType simpleConnectorType = new SimpleConnectorType();
+      simpleConnectorType.setConfigKeys(new String[] {"user", "password"});
+      Map map = new JsonObjectAsMap(new JSONObject("{user:max, password:foo}"));
+      ConfigureResponse configureResponse = simpleConnectorType
+          .getPopulatedConfigForm(map, null);
+      String configForm = configureResponse.getFormSnippet();
+      String expectedResult = "<tr>\r\n" + "<td>user</td>\r\n"
+          + "<td><input type=\"text\" name=\"user\" value=\"max\"/></td>\r\n"
+          + "</tr>\r\n" + "<tr>\r\n" + "<td>password</td>\r\n"
+          + "<td><input type=\"password\" name=\"password\" value=\"foo\"/>"
+          + "</td>\r\n" + "</tr>\r\n";
+      Assert.assertEquals(expectedResult, configForm);
+      String message = configureResponse.getMessage();
+      Assert.assertTrue(message.length() == 0);
+    }
+  }
+  private static class TestConnectorType extends SimpleConnectorType {
+    public TestConnectorType() {
+      super();
+    }
+    public boolean validateConfigPair(String key, String val) {
+      if (!super.validateConfigPair(key, val)) {
+        return false;
+      }
+      if (val.equals("xyzzy")) {
+        return false;
+      }
+      return true;
+    }
+  }
+}
