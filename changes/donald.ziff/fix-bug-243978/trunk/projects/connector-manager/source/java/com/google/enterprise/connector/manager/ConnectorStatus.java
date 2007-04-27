2ce70523106d75484dc30bfcf85eae1173701cc2
donald.ziff <donald.ziff@56db1fc5-3b1b-0410-9cff-4307dac5e09e>
2007-04-27 21:37:45 +0000

    Write with an OutputStream not an OutputStreamWriter

---
+++ changes/donald.ziff/fix-bug-243978/trunk/projects/connector-manager/source/java/com/google/enterprise/connector/manager/ConnectorStatus.java
@@ -0,0 +1,63 @@
+// Copyright (C) 2006 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.enterprise.connector.manager;
+
+/**
+ * Short info on a named connector.
+ */
+public class ConnectorStatus {
+  
+  private final String name;
+  private final String type;
+  private final int status;
+  
+  /**
+   * Primary constructor.  
+   * @param name the connector's name
+   * @param type the coneector's type
+   * @param status the connector's status
+   */
+  public ConnectorStatus(String name, String type, int status) {
+    super();
+    this.name = name;
+    this.type = type;
+    this.status = status;
+  }
+
+  /**
+   * Gets the name
+   * @return the name
+   */
+  public String getName() {
+    return name;
+  }
+
+  /**
+   * Gets the type
+   * @return the type
+   */
+  public String getType() {
+    return type;
+  }
+
+  /**
+   * Gets the status
+   * @return the status
+   */
+  public int getStatus() {
+    return status;
+  }
+
+}
---
+++ changes/donald.ziff/fix-bug-243978/trunk/projects/connector-manager/source/java/com/google/enterprise/connector/pusher/GsaFeedConnection.java
@@ -0,0 +1,79 @@
+// Copyright 2006 Google Inc.  All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package com.google.enterprise.connector.pusher;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+/**
+ * Opens a connection to a url and sends data to it.
+ */
+public class GsaFeedConnection implements FeedConnection {
+  
+  private URL url = null;
+  private String host;
+  private int port;
+  
+  public GsaFeedConnection(String host, int port) {
+    this.host = host;
+    this.port = port;
+  }
+
+  /*
+   * Generates the feed url for a given GSA host.
+   */
+  private URL getFeedUrl() throws MalformedURLException{
+    String feedUrl = "http://" + host + ":" + port + "/xmlfeed";
+    URL url = new URL(feedUrl);
+    return url;
+  }
+  
+  public String sendData(InputStream data) throws IOException {
+    if (url == null) {
+      url = getFeedUrl();
+    }
+    URLConnection uc = url.openConnection();
+    uc.setDoInput(true);
+    uc.setDoOutput(true);
+    uc.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+    OutputStream outputStream = uc.getOutputStream();
+    byte[] bytebuf = new byte[2048];
+    int val;
+    while (-1 != (val = data.read(bytebuf))) {
+      outputStream.write(bytebuf, 0, val);
+    }
+    outputStream.flush();
+    
+    StringBuffer buf = new StringBuffer();
+    InputStream inputStream = uc.getInputStream();
+	BufferedReader br =
+      new BufferedReader(new InputStreamReader(inputStream,"UTF8"));
+    String line;
+    while ((line = br.readLine()) != null) {
+      buf.append(line);
+    }
+    outputStream.close();
+    br.close();
+    return buf.toString();
+  }
+  
+  
+}
