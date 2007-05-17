8b96625debc8a3a97d55972f60196a5764a6c5a3
donald.ziff <donald.ziff@56db1fc5-3b1b-0410-9cff-4307dac5e09e>
2007-05-17 22:28:12 +0000

    Rename QueryTraversalManager to TraversalManager

---
+++ changes/donald.ziff/rename-QueryTraversalManager-to-TraversalManager/trunk/projects/connector-manager/source/javatests/com/google/enterprise/connector/test/MockJcrAdaptedToSpiTraversalTest.java
@@ -0,0 +1,28 @@
+package com.google.enterprise.connector.test;
+
+import com.google.enterprise.connector.jcradaptor.SpiRepositoryFromJcr;
+import com.google.enterprise.connector.mock.MockRepository;
+import com.google.enterprise.connector.mock.MockRepositoryEventList;
+import com.google.enterprise.connector.mock.jcr.MockJcrRepository;
+import com.google.enterprise.connector.spi.LoginException;
+import com.google.enterprise.connector.spi.TraversalManager;
+import com.google.enterprise.connector.spi.Connector;
+import com.google.enterprise.connector.spi.RepositoryException;
+import com.google.enterprise.connector.spi.Session;
+
+import junit.framework.TestCase;
+
+public class MockJcrAdaptedToSpiTraversalTest extends TestCase {
+
+  public void testTraversal() throws LoginException, RepositoryException {
+    MockRepositoryEventList mrel = 
+      new MockRepositoryEventList("MockRepositoryEventLog1.txt");
+    MockRepository r = new MockRepository(mrel);
+    javax.jcr.Repository jcrRepo = new MockJcrRepository(r);
+    Connector repo = new SpiRepositoryFromJcr(jcrRepo);
+    Session session = repo.login();
+    TraversalManager qtm = session.getQueryTraversalManager();
+    QueryTraversalUtil.runTraversal(qtm, 2);
+  }
+  
+}
