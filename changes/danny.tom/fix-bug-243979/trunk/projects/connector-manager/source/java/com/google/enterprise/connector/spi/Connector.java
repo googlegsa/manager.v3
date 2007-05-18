fe301db209f0535992d55121d1e0c5836a8fd209
danny.tom <danny.tom@56db1fc5-3b1b-0410-9cff-4307dac5e09e>
2007-05-18 00:16:03 +0000

    Fix bug 243979.  Call setBatchHint() so that connector implementors know how many we plan on retrieving.

---
+++ changes/danny.tom/fix-bug-243979/trunk/projects/connector-manager/source/java/com/google/enterprise/connector/spi/Connector.java
@@ -0,0 +1,42 @@
+// Copyright (C) 2006 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.enterprise.connector.spi;
+
+/**
+ * The root of the SPI. In the connector manager, objects that implement this
+ * interface will be instantiated using Spring, according to Spring bean
+ * definition files generated with the help of the corresponding ConnectorType
+ * implementation.
+ * <p>
+ * A Connector object is used as something against which to authenticate, via
+ * the login method. By authenticating, one gets a Session, which then gives
+ * access to all other services.
+ */
+public interface Connector {
+
+  /**
+   * Gets a session with sufficient privileges to perform all actions related to
+   * the SPI. Credentials, if needed, are supplied externally through injection
+   * via Spring or some other mechanism. If an exception is thrown, the
+   * implementor should provide an explanatory message.
+   * 
+   * @return An object implementing the {@link Session} interface
+   * @throws LoginException if there is a credentials-related problem
+   * @throws RepositoryException if there is a more general problem, such as the
+   *         system is unreachable or down
+   */
+  public Session login() throws LoginException,
+      RepositoryException;
+}
---
+++ changes/danny.tom/fix-bug-243979/trunk/projects/connector-manager/source/javatests/com/google/enterprise/connector/persist/PrefsStoreTest.java
@@ -0,0 +1,115 @@
+// Copyright 2006 Google Inc.  All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package com.google.enterprise.connector.persist;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+/**
+ * Class to test ScheduleStore.
+ */
+public class PrefsStoreTest extends TestCase {
+
+  protected PrefsStore store;
+  
+  protected void setUp() {
+    store = new PrefsStore();
+    store.clear();
+  }
+  
+  protected void tearDown() {
+    store.clear();
+  }
+  
+  // Tests getting and setting for a valid connector name and schedule.
+  
+  public void testGetandSetConnectorSchedule() {
+    String expectedSchedule = "schedule of connectorA";
+    String connectorName = "connectorA";
+    store.storeConnectorSchedule(connectorName, expectedSchedule);
+    String resultSchedule = store.getConnectorSchedule(connectorName);
+    Assert.assertTrue(resultSchedule.equals(expectedSchedule));
+  }
+  
+  // Tests getting schedule for an unknown connector
+  public void testGetConnectorSchedule1() {
+    String schedule = store.getConnectorSchedule("some wierd connector name");
+    Assert.assertNull(schedule);
+  }
+  
+  // Tests if the exception is thrown correctly when the connector name is null. 
+  public void testGetConnectorSchedule2() {
+    boolean exceptionCaught = false;
+    String schedule = null;
+    try {
+      schedule = store.getConnectorSchedule(null);
+    } catch (NullPointerException e) {
+        exceptionCaught = true;
+    }
+    Assert.assertTrue(exceptionCaught);
+    Assert.assertNull(schedule);
+  }
+
+  public void testRemoveConnector() {
+    String connectorName = "foo";
+    String connectorSchedule = "foo's schedule";
+    String schedule = store.getConnectorSchedule(connectorName);
+    Assert.assertNull(schedule);
+    store.storeConnectorSchedule(connectorName, connectorSchedule);
+    schedule = store.getConnectorSchedule(connectorName);
+    Assert.assertEquals(connectorSchedule, schedule);
+    store.removeConnectorSchedule(connectorName);
+    schedule = store.getConnectorSchedule(connectorName);
+    Assert.assertNull(schedule);
+  }
+  
+  //Tests getting and setting for a valid connector name and state.  
+  public void testGetandSetConnectorState() {
+    String expectedState = "state of connectorA";
+    String connectorName = "connectorA";
+    store.storeConnectorState(connectorName, expectedState);
+    String resultState = store.getConnectorState(connectorName);
+    Assert.assertTrue(resultState.equals(expectedState));
+  }
+  
+  //Tests getting state for an unknown connector.
+  public void testGetConnectorState1() {
+    String state = store.getConnectorState("some wierd connector name");
+    Assert.assertNull(state);
+  }
+  
+  // Tests if the exception is thrown correctly when the connector name is null. 
+  public void testGetConnectorState2() {
+    boolean exceptionCaught = false;
+    try {
+      String state = store.getConnectorState(null);
+    } catch (NullPointerException e) {
+        exceptionCaught = true;
+    }
+    Assert.assertTrue(exceptionCaught);
+  }  
+
+  public void testRemoveState() {
+    String connectorName = "foo";
+    String connectorState = "foo's state";
+    String state = store.getConnectorState(connectorName);
+    Assert.assertNull(state);
+    store.storeConnectorState(connectorName, connectorState);
+    state = store.getConnectorState(connectorName);
+    Assert.assertEquals(connectorState, state);
+    store.removeConnectorState(connectorName);
+    state = store.getConnectorState(connectorName);
+    Assert.assertNull(state);
+  }
+}
