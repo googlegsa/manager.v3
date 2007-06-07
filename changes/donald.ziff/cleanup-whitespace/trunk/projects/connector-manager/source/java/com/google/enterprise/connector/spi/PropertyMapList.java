eb7ac29c68c8ff507c53f872de686ddf9e7dd4bf
donald.ziff <donald.ziff@56db1fc5-3b1b-0410-9cff-4307dac5e09e>
2007-06-07 20:15:23 +0000

    This is a whitespace-only change. Some lines ended with cr-lf, some with just lf.  This makes things uniform.

---
+++ changes/donald.ziff/cleanup-whitespace/trunk/projects/connector-manager/source/java/com/google/enterprise/connector/spi/PropertyMapList.java
@@ -0,0 +1,33 @@
+// Copyright (C) 2006 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.enterprise.connector.spi;
+
+import java.util.Iterator;
+
+/**
+ * A handle to a list of documents for indexing.
+ */
+public interface PropertyMapList {
+
+  /**
+   * Produces an iterator through which query results can be explored. Each
+   * result is a {@link PropertyMap}. This method should only be called once. 
+   * @return Iterator of {@link PropertyMap} objects
+   * @throws RepositoryException if something is wrong with the repository, 
+   * such as no connectivity
+   */
+  public Iterator iterator() throws RepositoryException;
+
+}
