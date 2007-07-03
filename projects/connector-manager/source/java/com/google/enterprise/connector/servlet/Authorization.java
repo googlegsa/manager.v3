3af15f1a22a20ee8c50931fc4dd6d76cab1b0615
Max Ziff <donald.ziff@gmail.com>
2007-07-03 16:39:15 +0000

    Refactor some pretty incomprehensible code. More testing,

--- projects/connector-manager/source/java/com/google/enterprise/connector/servlet/Authorization.java
+++ projects/connector-manager/source/java/com/google/enterprise/connector/servlet/Authorization.java
@@ -17,27 +17,14 @@ package com.google.enterprise.connector.servlet;
 
 import com.google.enterprise.connector.manager.Manager;
 
-import org.w3c.dom.Element;
-import org.w3c.dom.NodeList;
-
 import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.TreeMap;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  * Admin servlet for authorization
  *
  */
 public class Authorization extends ConnectorManagerServlet {
-  private static final Logger LOGGER =
-    Logger.getLogger(Authorization.class.getName());
-
+  
   /*
    * (non-Javadoc)
    * @see com.google.enterprise.connector.servlet.ConnectorManagerServlet
@@ -46,138 +33,8 @@ public class Authorization extends ConnectorManagerServlet {
    */
   protected void processDoPost(
       String xmlBody, Manager manager, PrintWriter out) {
-    handleDoPost(xmlBody, manager, out);
+    AuthorizationHandler authorizationHandler = new AuthorizationHandler(xmlBody, manager, out);
+    authorizationHandler.handleDoPost();
   }
-
-  /**
-   * Writes an answer for each resource from the request.
-   *
-   * @param xmlBody String the XML request body string 
-   * @param manager Manager
-   * @param out PrintWriter where the response is written
-   */
-  public static void handleDoPost(
-      String xmlBody, Manager manager, PrintWriter out) {
-    Element root = ServletUtil.parseAndGetRootElement(
-        xmlBody, ServletUtil.XMLTAG_AUTHZ_QUERY);
-    if (root == null) {
-      ServletUtil.writeResponse(
-          out, ConnectorMessageCode.ERROR_PARSING_XML_REQUEST);
-      return;
-    }
-
-    NodeList queryList =
-        root.getElementsByTagName(ServletUtil.XMLTAG_CONNECTOR_QUERY);
-    if (queryList.getLength() == 0) {
-      LOGGER.log(Level.WARNING, ServletUtil.LOG_RESPONSE_EMPTY_NODE);
-      ServletUtil.writeResponse(
-          out, ConnectorMessageCode.RESPONSE_EMPTY_NODE);
-      return;
-    }
-
-    int status = ConnectorMessageCode.SUCCESS;
-    int numDocs = 0;
-    Map map = new TreeMap();
-    for (int i = 0; i < queryList.getLength(); ++i) {
-      Element queryItem = (Element) queryList.item(i);
-      String identity = ServletUtil.getFirstElementByTagName(
-          queryItem, ServletUtil.XMLTAG_IDENTITY);
-      String source = ServletUtil.getFirstAttribute(
-          queryItem, ServletUtil.XMLTAG_IDENTITY, "source");
-      List resources = ServletUtil.getAllElementsByTagName(
-          queryItem, ServletUtil.XMLTAG_RESOURCE);
-      if (resources.isEmpty()) {
-    	status = ConnectorMessageCode.RESPONSE_NULL_RESOURCE;
-    	continue;
-      }
-      for (Iterator iter = resources.iterator(); iter.hasNext();) {
-        String url = (String) iter.next();
-        String connectorName = url.substring(
-            url.indexOf(ServletUtil.PROTOCOL) + ServletUtil.PROTOCOL.length());
-        if (url.lastIndexOf(ServletUtil.DOCID) == -1) {
-          status = ConnectorMessageCode.RESPONSE_NULL_DOCID;
-          continue;
-        }
-        if (connectorName.indexOf('.') != -1) {
-          connectorName = connectorName.substring(0, connectorName.indexOf('.'));
-        } else {
-          connectorName = connectorName.substring(0, connectorName.indexOf('/'));
-        }
-        if (connectorName.length() < 1) {
-          status = ConnectorMessageCode.RESPONSE_NULL_CONNECTOR;
-          continue;
-        }
-        Map connDocs = null;
-        List urlList = null;
-        if (map.containsKey(identity) == false) {
-          connDocs = new TreeMap();
-          urlList = new ArrayList();
-          connDocs.put(connectorName, urlList);
-          map.put(identity, connDocs);
-        } else {
-          connDocs = (Map) map.get(identity);
-          if (connDocs.containsKey(connectorName)) {
-            urlList = (List) connDocs.get(connectorName);
-          } else {
-            urlList = new ArrayList();
-            connDocs.put(connectorName, urlList);
-          }
-        }
-        urlList.add(url);
-        ++numDocs;
-      }
-    }
-    if (numDocs == 0) {
-      LOGGER.warning("No docid available.");
-      ServletUtil.writeResponse(out, status);
-      return;
-    }
-
-    Set set = map.entrySet();
-    Iterator iterator = set.iterator();
-    ServletUtil.writeRootTag(out, false);
-    ServletUtil.writeXMLTag(out, 1, ServletUtil.XMLTAG_AUTHZ_RESPONSE, false);
-    while (iterator.hasNext()) {
-      Map.Entry entry = (Map.Entry)iterator.next();
-      Map connDocs = (Map) entry.getValue();
-      Set connDocSet = connDocs.entrySet();
-      Iterator iterConnDocs = connDocSet.iterator();
-      while (iterConnDocs.hasNext()) {
-        Map.Entry entryConn = (Map.Entry)iterConnDocs.next();
-        List docidList = new ArrayList();
-        for (Iterator iterUrl = ((List) entryConn.getValue()).iterator();
-            iterUrl.hasNext();) {
-          String url = (String) iterUrl.next();
-          docidList.add(url.substring(
-              url.lastIndexOf(ServletUtil.DOCID) +
-              ServletUtil.DOCID.length(), url.length()));
-        }
-        Set answerSet = manager.authorizeDocids((String) entryConn.getKey(),
-            docidList, (String) entry.getKey());    
-        // Assume that iter and iterDocid are in parallel.
-        Iterator iter = ((List) entryConn.getValue()).iterator();
-        Iterator iterDocid = docidList.iterator();
-        while (iter.hasNext() && iterDocid.hasNext()) {
-          ServletUtil.writeXMLTag(
-              out, 2, ServletUtil.XMLTAG_ANSWER, false);
-          ServletUtil.writeXMLElement(
-              out, 3, ServletUtil.XMLTAG_RESOURCE, (String) iter.next());
-          if (answerSet.contains(iterDocid.next())) {
-            ServletUtil.writeXMLElement(
-              out, 3, ServletUtil.XMLTAG_DECISION, "Permit");
-          } else {
-            ServletUtil.writeXMLElement(
-              out, 3, ServletUtil.XMLTAG_DECISION, "Deny");
-          }
-          ServletUtil.writeXMLTag(
-              out, 2, ServletUtil.XMLTAG_ANSWER, true);
-        }
-      }
-    }
-    ServletUtil.writeXMLTag(out, 1, ServletUtil.XMLTAG_AUTHZ_RESPONSE, true);
-    ServletUtil.writeStatusId(out, status);
-    ServletUtil.writeRootTag(out, true);
-
-    return;
-  }
+  
 }
--- projects/connector-manager/source/javatests/com/google/enterprise/connector/servlet/AuthorizationTest.java
+++ projects/connector-manager/source/javatests/com/google/enterprise/connector/servlet/AuthorizationTest.java
@@ -1,10 +1,10 @@
-// Copyright 2006 Google Inc.  All Rights Reserved.
+// Copyright 2006 Google Inc. All Rights Reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+// http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -27,158 +27,256 @@ import java.util.logging.Logger;
 
 /**
  * Tests the Authorization servlet class
- *
+ * 
  */
 public class AuthorizationTest extends TestCase {
   private static final Logger LOGGER =
-    Logger.getLogger(AuthorizationTest.class.getName());
+      Logger.getLogger(AuthorizationTest.class.getName());
 
-  /**
-   * Test method for
-   * {@link com.google.enterprise.connector.servlet.Authorization#handleDoPost(
-   * java.lang.String, com.google.enterprise.connector.manager.Manager)}.
-   */
-  public void testHandleDoPost1() {
-    String xmlBody =
-        "<AuthorizationQuery>\n" + 
-        "<ConnectorQuery>\n" + 
-        "  <Identity source=\"gsa\">CN=foo</Identity>\n" + 
-        "  <Resource>" + ServletUtil.PROTOCOL + "connector1.localhost" +
-           ServletUtil.DOCID + "foo1</Resource>\n" + 
-        "  <Resource>" + ServletUtil.PROTOCOL + "connector2.localhost" +
-           ServletUtil.DOCID + "foo2</Resource>\n" + 
-        "</ConnectorQuery>\n" + 
-        "<ConnectorQuery>\n" + 
-        "  <Identity source=\"connector\">username</Identity>\n" + 
-        "  <Resource>" + ServletUtil.PROTOCOL + "connector3.localhost" +
-           ServletUtil.DOCID + "foo3</Resource>\n" + 
-        "</ConnectorQuery>\n" + 
-        "</AuthorizationQuery>";
+  private static final String TEST_XML1 =
+      "<AuthorizationQuery>\n" + 
+      "<ConnectorQuery>\n" + 
+      "  <Identity source=\"gsa\">CN=foo</Identity>\n" + 
+      "  <Resource>" + ServletUtil.PROTOCOL + "connector1.localhost" +
+         ServletUtil.DOCID + "foo1</Resource>\n" + 
+      "  <Resource>" + ServletUtil.PROTOCOL + "connector2.localhost" +
+         ServletUtil.DOCID + "foo2</Resource>\n" + 
+      "</ConnectorQuery>\n" + 
+      "<ConnectorQuery>\n" + 
+      "  <Identity source=\"connector\">username</Identity>\n" + 
+      "  <Resource>" + ServletUtil.PROTOCOL + "connector3.localhost" +
+         ServletUtil.DOCID + "foo3</Resource>\n" + 
+      "</ConnectorQuery>\n" + 
+      "</AuthorizationQuery>";
+  
+  private static final String TEST_XML2 =
+      "<AuthorizationQuery>\n" + 
+      "<ConnectorQuery>\n" + 
+      "  <Identity source=\"gsa\">CN=foo</Identity>\n" + 
+      "  <Resource>" + ServletUtil.PROTOCOL + ".localhost" +
+         ServletUtil.DOCID + "foo1</Resource>\n" + 
+      "</ConnectorQuery>\n" + 
+      "</AuthorizationQuery>";
 
-    String expectedResult =
-    	"<CmResponse>\n" +
-    	"  <AuthorizationResponse>\n" + 
-        "    <Answer>\n" + 
-        "      <Resource>" + ServletUtil.PROTOCOL + "connector1.localhost" +
-               ServletUtil.DOCID + "foo1</Resource>\n" + 
-        "      <Decision>Permit</Decision>\n" + 
-        "    </Answer>\n" + 
-        "    <Answer>\n" + 
-        "      <Resource>" + ServletUtil.PROTOCOL + "connector2.localhost" +
-               ServletUtil.DOCID + "foo2</Resource>\n" + 
-        "      <Decision>Permit</Decision>\n" + 
-        "    </Answer>\n" + 
-        "    <Answer>\n" + 
-        "      <Resource>" + ServletUtil.PROTOCOL + "connector3.localhost" +
-               ServletUtil.DOCID + "foo3</Resource>\n" + 
-        "      <Decision>Permit</Decision>\n" + 
-        "    </Answer>\n" + 
-        "  </AuthorizationResponse>\n" +
-        "  <StatusId>0</StatusId>\n" +
-        "</CmResponse>\n";
-    doTest(xmlBody, expectedResult);
+  private static final String TEST_XML3 =
+      "<AuthorizationQuery>\n" + 
+      "<ConnectorQuery>\n" + 
+      "  <Identity source=\"gsa\">CN=foo</Identity>\n" + 
+      "  <Resource>" + ServletUtil.PROTOCOL + "Connector3.localhost" +
+         "/doc?DOC=foo1</Resource>\n" + 
+      "</ConnectorQuery>\n" + 
+      "</AuthorizationQuery>";
+
+  private static final String TEST_XML4 =
+      "<AuthorizationQuery>\n" + 
+      "<ConnectorQuery>\n" + 
+      "  <Identity source=\"gsa\"></Identity>\n" + 
+      "  <Resource>" + ServletUtil.PROTOCOL + "connector1.localhost" +
+         ServletUtil.DOCID + "foo1</Resource>\n" + 
+      "</ConnectorQuery>\n" + 
+      "</AuthorizationQuery>";
+
+
+  private static final String TEST_XML5 =
+      "<AuthorizationQuery>\n" + "<ConnectorQuery>\n"
+          + "  <Identity source=\"gsa\">username</Identity>\n"
+          + "</ConnectorQuery>\n" + "</AuthorizationQuery>";
+
+  private static final String TWO_IDENTITIES_TWO_CONNECTORS =
+      "<AuthorizationQuery>\n"
+          + "<ConnectorQuery>\n"
+          + "  <Identity source=\"connector\">username</Identity>\n"
+          + "  <Resource>googleconnector://connector1.localhost/" +
+                "doc?docid=doc1a</Resource>\n"
+          + "  <Resource>googleconnector://connector2.localhost/" +
+                "doc?docid=doc2a</Resource>\n"
+          + "  <Resource>googleconnector://connector1.localhost/" +
+                "doc?docid=doc1b</Resource>\n"
+          + "  <Resource>googleconnector://connector2.localhost/" +
+                "doc?docid=doc2b</Resource>\n"
+          + "</ConnectorQuery>\n"
+          + "<ConnectorQuery>\n"
+          + "  <Identity source=\"connector\">username2</Identity>\n"
+          + "  <Resource>googleconnector://connector1.localhost/" +
+                "doc?docid=doc1c</Resource>\n"
+          + "  <Resource>googleconnector://connector2.localhost/doc?" +
+                "docid=doc2c</Resource>\n"
+          + "  <Resource>googleconnector://connector1.localhost/doc?" +
+                "docid=doc1d</Resource>\n"
+          + "  <Resource>googleconnector://connector2.localhost/doc?" +
+                "docid=doc2d</Resource>\n"
+          + "</ConnectorQuery>\n" + "</AuthorizationQuery>\n";
+  private static final String ONE_IDENTITY_TWO_QUERIES =
+      "<AuthorizationQuery>\n"
+          + "<ConnectorQuery>\n"
+          + "  <Identity source=\"connector\">username</Identity>\n"
+          + "  <Resource>googleconnector://connector1.localhost/" +
+                "doc?docid=doc1a</Resource>\n"
+          + "  <Resource>googleconnector://connector2.localhost/" +
+                "doc?docid=doc2a</Resource>\n"
+          + "  <Resource>googleconnector://connector1.localhost/" +
+                "doc?docid=doc1b</Resource>\n"
+          + "  <Resource>googleconnector://connector2.localhost/" +
+                "doc?docid=doc2b</Resource>\n"
+          + "</ConnectorQuery>\n"
+          + "<ConnectorQuery>\n"
+          + "  <Identity source=\"connector\">username</Identity>\n"
+          + "  <Resource>googleconnector://connector1.localhost/" +
+                "doc?docid=doc1c</Resource>\n"
+          + "  <Resource>googleconnector://connector2.localhost/" +
+                "doc?docid=doc2c</Resource>\n"
+          + "  <Resource>googleconnector://connector1.localhost/" +
+                "doc?docid=doc1d</Resource>\n"
+          + "  <Resource>googleconnector://connector2.localhost/" +
+                "doc?docid=doc2d</Resource>\n"
+          + "</ConnectorQuery>\n" + "</AuthorizationQuery>\n";
+  private static final String TWO_IDENTITIES_MULTIPLE_QUERIES =
+      "<AuthorizationQuery>\n"
+          + "<ConnectorQuery>\n"
+          + "  <Identity source=\"connector\">username1</Identity>\n"
+          + "  <Resource>googleconnector://connector1.localhost/" +
+                "doc?docid=doc1a</Resource>\n"
+          + "</ConnectorQuery>\n"
+          + "<ConnectorQuery>\n"
+          + "  <Identity source=\"connector\">username2</Identity>\n"
+          + "  <Resource>googleconnector://connector2.localhost/" +
+                "doc?docid=doc2a</Resource>\n"
+          + "</ConnectorQuery>\n"
+          + "<ConnectorQuery>\n"
+          + "  <Identity source=\"connector\">username1</Identity>\n"
+          + "  <Resource>googleconnector://connector1.localhost/" +
+                "doc?docid=doc1b</Resource>\n"
+          + "</ConnectorQuery>\n"
+          + "<ConnectorQuery>\n"
+          + "  <Identity source=\"connector\">username2</Identity>\n"
+          + "  <Resource>googleconnector://connector2.localhost/" +
+                "doc?docid=doc2b</Resource>\n"
+          + "</ConnectorQuery>\n" + "</AuthorizationQuery>\n";
+
+  private static final String MALFORMED_XML =
+      "<AuthorizationQuery>\n" + "<ConnectorQuery>\n"
+          + "</AuthorizationQuery>\n" + "";
+
+
+  public void testParsing() {
+    {
+      AuthorizationParser authorizationParser = 
+        new AuthorizationParser(TEST_XML1);
+      authorizationParser.parse();
+      Assert.assertEquals(2, authorizationParser.countParsedIdentities());
+      Assert.assertEquals(1, authorizationParser
+          .countConnectorsForIdentity("username"));
+      Assert.assertEquals(2, authorizationParser
+          .countConnectorsForIdentity("CN=foo"));
+      Assert.assertEquals(1, authorizationParser
+          .countUrlsForIdentityConnectorPair("username", "connector3"));
+    }
+    {
+      AuthorizationParser authorizationParser =
+          new AuthorizationParser(TWO_IDENTITIES_TWO_CONNECTORS);
+      authorizationParser.parse();
+      Assert.assertEquals(2, authorizationParser.countParsedIdentities());
+      Assert.assertEquals(2, authorizationParser
+          .countConnectorsForIdentity("username"));
+      Assert.assertEquals(2, authorizationParser
+          .countConnectorsForIdentity("username2"));
+      Assert.assertEquals(2, authorizationParser
+          .countUrlsForIdentityConnectorPair("username", "connector2"));
+    }
+    {
+      AuthorizationParser authorizationParser = 
+        new AuthorizationParser(ONE_IDENTITY_TWO_QUERIES);
+      authorizationParser.parse();
+      Assert.assertEquals(1, authorizationParser.countParsedIdentities());
+      Assert.assertEquals(2, authorizationParser
+          .countConnectorsForIdentity("username"));
+      Assert.assertEquals(4, authorizationParser
+          .countUrlsForIdentityConnectorPair("username", "connector2"));
+    }
+    {
+      AuthorizationParser authorizationParser =
+          new AuthorizationParser(TWO_IDENTITIES_MULTIPLE_QUERIES);
+      authorizationParser.parse();
+      Assert.assertEquals(2, authorizationParser.countParsedIdentities());
+      Assert.assertEquals(1, authorizationParser
+          .countConnectorsForIdentity("username1"));
+      Assert.assertEquals(0, authorizationParser
+          .countUrlsForIdentityConnectorPair("username1", "connector2"));
+    }
+    {
+      AuthorizationParser authorizationParser = 
+        new AuthorizationParser(MALFORMED_XML);
+      authorizationParser.parse();
+      Assert.assertNull(authorizationParser.getParseMap());
+      Assert.assertEquals(ConnectorMessageCode.ERROR_PARSING_XML_REQUEST,
+          authorizationParser.getStatus());
+    }
   }
 
   /**
-   * Test method for
-   * {@link com.google.enterprise.connector.servlet.Authorization#handleDoPost(
-   * java.lang.String, com.google.enterprise.connector.manager.Manager)}.
-   * 
+   */
+  public void testHandleDoPost1() {
+    String expectedResult =
+        "<CmResponse>\n" + "  <AuthorizationResponse>\n" + "    <Answer>\n"
+            + "      <Resource>" + ServletUtil.PROTOCOL
+            + "connector1.localhost" + ServletUtil.DOCID + "foo1</Resource>\n"
+            + "      <Decision>Permit</Decision>\n" + "    </Answer>\n"
+            + "    <Answer>\n" + "      <Resource>" + ServletUtil.PROTOCOL
+            + "connector2.localhost" + ServletUtil.DOCID + "foo2</Resource>\n"
+            + "      <Decision>Permit</Decision>\n" + "    </Answer>\n"
+            + "    <Answer>\n" + "      <Resource>" + ServletUtil.PROTOCOL
+            + "connector3.localhost" + ServletUtil.DOCID + "foo3</Resource>\n"
+            + "      <Decision>Permit</Decision>\n" + "    </Answer>\n"
+            + "  </AuthorizationResponse>\n" + "  <StatusId>0</StatusId>\n"
+            + "</CmResponse>\n";
+    doTest(TEST_XML1, expectedResult);
+  }
+
+  /**
    * The connector name is null.
    */
   public void testHandleDoPost2() {
-    String xmlBody =
-        "<AuthorizationQuery>\n" + 
-        "<ConnectorQuery>\n" + 
-        "  <Identity source=\"gsa\">CN=foo</Identity>\n" + 
-        "  <Resource>" + ServletUtil.PROTOCOL + ".localhost" +
-           ServletUtil.DOCID + "foo1</Resource>\n" + 
-        "</ConnectorQuery>\n" + 
-        "</AuthorizationQuery>";
-
     String expectedResult =
-        "<CmResponse>\n" + 
-        "  <StatusId>" + ConnectorMessageCode.RESPONSE_NULL_CONNECTOR +
-        "</StatusId>\n" + "</CmResponse>\n";
-    doTest(xmlBody, expectedResult);
+        "<CmResponse>\n" + "  <StatusId>"
+            + ConnectorMessageCode.RESPONSE_NULL_CONNECTOR + "</StatusId>\n"
+            + "</CmResponse>\n";
+    doTest(TEST_XML2, expectedResult);
   }
 
   /**
-   * Test method for
-   * {@link com.google.enterprise.connector.servlet.Authorization#handleDoPost(
-   * java.lang.String, com.google.enterprise.connector.manager.Manager)}.
-   * 
    * docid does not exist.
    */
   public void testHandleDoPost3() {
-    String xmlBody =
-        "<AuthorizationQuery>\n" + 
-        "<ConnectorQuery>\n" + 
-        "  <Identity source=\"gsa\">CN=foo</Identity>\n" + 
-        "  <Resource>" + ServletUtil.PROTOCOL + "Connector3.localhost" +
-           "/doc?DOCID=foo1</Resource>\n" + 
-        "</ConnectorQuery>\n" + 
-        "</AuthorizationQuery>";
-
     String expectedResult =
-        "<CmResponse>\n" + 
-        "  <StatusId>" + ConnectorMessageCode.RESPONSE_NULL_DOCID +
-        "</StatusId>\n" + "</CmResponse>\n";
-    doTest(xmlBody, expectedResult);
+        "<CmResponse>\n" + "  <StatusId>"
+            + ConnectorMessageCode.RESPONSE_NULL_DOCID + "</StatusId>\n"
+            + "</CmResponse>\n";
+    doTest(TEST_XML3, expectedResult);
   }
 
   /**
-   * Test method for
-   * {@link com.google.enterprise.connector.servlet.Authorization#handleDoPost(
-   * java.lang.String, com.google.enterprise.connector.manager.Manager)}.
-   * 
    * The identity is null (empty).
-   * 
    */
   public void testHandleDoPost4() {
-    String xmlBody =
-        "<AuthorizationQuery>\n" + 
-        "<ConnectorQuery>\n" + 
-        "  <Identity source=\"gsa\"></Identity>\n" + 
-        "  <Resource>" + ServletUtil.PROTOCOL + "connector1.localhost" +
-           ServletUtil.DOCID + "foo1</Resource>\n" + 
-        "</ConnectorQuery>\n" + 
-        "</AuthorizationQuery>";
-
     String expectedResult =
-    	"<CmResponse>\n" +
-    	"  <AuthorizationResponse>\n" + 
-        "    <Answer>\n" + 
-        "      <Resource>" + ServletUtil.PROTOCOL + "connector1.localhost" +
-               ServletUtil.DOCID + "foo1</Resource>\n" + 
-        "      <Decision>Permit</Decision>\n" + 
-        "    </Answer>\n" + 
-        "  </AuthorizationResponse>\n" +
-        "  <StatusId>0</StatusId>\n" +
-        "</CmResponse>\n";
-    doTest(xmlBody, expectedResult);
+        "<CmResponse>\n" + "  <AuthorizationResponse>\n" + "    <Answer>\n"
+            + "      <Resource>" + ServletUtil.PROTOCOL
+            + "connector1.localhost" + ServletUtil.DOCID + "foo1</Resource>\n"
+            + "      <Decision>Permit</Decision>\n" + "    </Answer>\n"
+            + "  </AuthorizationResponse>\n" + "  <StatusId>0</StatusId>\n"
+            + "</CmResponse>\n";
+    doTest(TEST_XML4, expectedResult);
   }
 
   /**
-   * Test method for
-   * {@link com.google.enterprise.connector.servlet.Authorization#handleDoPost(
-   * java.lang.String, com.google.enterprise.connector.manager.Manager)}.
-   * 
    * The resource is null.
    */
   public void testHandleDoPost5() {
-	    String xmlBody =
-	        "<AuthorizationQuery>\n" + 
-	        "<ConnectorQuery>\n" + 
-	        "  <Identity source=\"gsa\">username</Identity>\n" + 
-	        "</ConnectorQuery>\n" + 
-	        "</AuthorizationQuery>";
-
-	    String expectedResult =
-	        "<CmResponse>\n" + 
-	        "  <StatusId>" + ConnectorMessageCode.RESPONSE_NULL_RESOURCE +
-            "</StatusId>\n" + "</CmResponse>\n";
-	    doTest(xmlBody, expectedResult);
+    String expectedResult =
+        "<CmResponse>\n" + "  <StatusId>"
+            + ConnectorMessageCode.RESPONSE_NULL_RESOURCE + "</StatusId>\n"
+            + "</CmResponse>\n";
+    doTest(TEST_XML5, expectedResult);
   }
 
   private void doTest(String xmlBody, String expectedResult) {
@@ -186,12 +284,15 @@ public class AuthorizationTest extends TestCase {
     Manager manager = MockManager.getInstance();
     StringWriter writer = new StringWriter();
     PrintWriter out = new PrintWriter(writer);
-    Authorization.handleDoPost(xmlBody, manager, out);
+    AuthorizationHandler authorizationHandler =
+        AuthorizationHandler.makeAuthorizationHandlerForTest(xmlBody, manager,
+            out);
+    authorizationHandler.handleDoPost();
     out.flush();
     StringBuffer result = writer.getBuffer();
     LOGGER.info(result.toString());
     LOGGER.info(expectedResult);
-    Assert.assertEquals (StringUtils.normalizeNewlines(expectedResult), 
+    Assert.assertEquals(StringUtils.normalizeNewlines(expectedResult),
         StringUtils.normalizeNewlines(result.toString()));
     out.close();
   }
