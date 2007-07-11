6fb5787c280fa56193e57cd9ea4802944e0eff13
rndmcnlly <rndmcnlly@56db1fc5-3b1b-0410-9cff-4307dac5e09e>
2007-07-11 00:10:30 +0000

    Adding retryDelayMillis logic, affects scheduling of connector instances.

---
+++ changes/rndmcnlly/add-retryDelay/connector-manager/source/java/com/google/enterprise/connector/scheduler/Schedule.java
@@ -0,0 +1,126 @@
+// Copyright 2006 Google Inc.  All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.enterprise.connector.scheduler;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * A traversal schedule.
+ * 
+ */
+public class Schedule {
+  private String connectorName;
+  private int load;
+  private int retryDelayMillis; // maximum of ~24 days
+  private List timeIntervals;
+
+  public Schedule(String connectorName, int load, int retryDelayMillis, List timeIntervals) {
+    if (null == timeIntervals) {
+      throw new IllegalArgumentException();
+    }
+    this.connectorName = connectorName;
+    this.load = load;
+    this.retryDelayMillis = retryDelayMillis;
+    this.timeIntervals = timeIntervals;
+  }
+
+  /**
+   * Create a schedule object.
+   * 
+   * @param scheduleProto String readable by readString() method
+   */
+  public Schedule(String scheduleProto) {
+    readString(scheduleProto);
+  }
+
+  /**
+   * Populate a schedule.
+   * 
+   * @param schedule String of the form:
+   *    <connectorName>:<load>:<retryDelayMillis>:<timeIntervals>
+   * e.g. "connector1:60:86400000:1-2:3-5"
+   * 
+   */
+  public void readString(String schedule) {
+    String[] strs = schedule.split(":");
+    if (strs.length >= 4) {  // must have at least one time interval
+      connectorName = strs[0];
+      load = Integer.parseInt(strs[1]);
+      retryDelayMillis = Integer.parseInt(strs[2]);
+      timeIntervals = new ArrayList();
+      for (int i = 3; i < strs.length; i++) {
+        String[] strs2 = strs[i].split("-");
+        String startTime = strs2[0];
+        String endTime = strs2[1];
+        ScheduleTime t1 = new ScheduleTime(Integer.parseInt(startTime));
+        ScheduleTime t2 = new ScheduleTime(Integer.parseInt(endTime));
+        ScheduleTimeInterval interval = new ScheduleTimeInterval(t1, t2);
+        timeIntervals.add(interval);
+      }
+    } else {
+      throw new IllegalArgumentException("Schedule should have at least one " +
+            "time interval: " + schedule);
+    }
+  }
+
+  /**
+   * @return String of the form: e.g. "connector1:1-2:3-5"
+   */
+  public String toString() {
+    StringBuffer buf = new StringBuffer();
+    buf.append(connectorName);
+    buf.append(":" + load);
+    buf.append(":" + getTimeIntervalsAsString());
+    return buf.toString();
+  }
+
+  public String getConnectorName() {
+    return connectorName;
+  }
+
+  public int getLoad() {
+    return load;
+  }
+  
+  public int getRetryDelayMillis() {
+    return retryDelayMillis;
+  }
+
+  public List getTimeIntervals() {
+    return timeIntervals;
+  }
+
+  /**
+   * @return String of the form: e.g. "1-2:3-5"
+   */
+  public String getTimeIntervalsAsString() {
+    StringBuffer buf = new StringBuffer();
+    Iterator iter = timeIntervals.iterator();
+    for (int index = 0; iter.hasNext(); index++) {
+      ScheduleTimeInterval interval = (ScheduleTimeInterval) iter.next();
+      ScheduleTime startTime = interval.getStartTime();
+      ScheduleTime endTime = interval.getEndTime();
+      if (index != 0) {
+        buf.append(":");
+      }
+      buf.append(startTime.getHour());
+      buf.append("-");
+      buf.append(endTime.getHour());
+    }
+    return buf.toString();
+  }
+}
---
+++ changes/rndmcnlly/add-retryDelay/connector-manager/source/javatests/com/google/enterprise/connector/jcradaptor/SpiTraversalManagerFromJcrTest.java
@@ -0,0 +1,162 @@
+// Copyright (C) 2006 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.enterprise.connector.jcradaptor;
+
+import com.google.enterprise.connector.mock.MockRepository;
+import com.google.enterprise.connector.mock.MockRepositoryDocument;
+import com.google.enterprise.connector.mock.MockRepositoryEventList;
+import com.google.enterprise.connector.mock.MockRepositoryPropertyTest;
+import com.google.enterprise.connector.mock.jcr.MockJcrNode;
+import com.google.enterprise.connector.mock.jcr.MockJcrQueryManager;
+import com.google.enterprise.connector.spi.PropertyMap;
+import com.google.enterprise.connector.spi.TraversalManager;
+import com.google.enterprise.connector.spi.RepositoryException;
+import com.google.enterprise.connector.spi.PropertyMapList;
+import com.google.enterprise.connector.spi.SpiConstants;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Calendar;
+import java.util.Iterator;
+import java.util.logging.Logger;
+
+import javax.jcr.LoginException;
+import javax.jcr.query.QueryManager;
+
+public class SpiTraversalManagerFromJcrTest extends TestCase {
+  private static final Logger logger =
+      Logger.getLogger(MockRepositoryPropertyTest.class.getName());
+
+  /**
+   * Test generating checkpoints
+   * 
+   * @throws RepositoryException
+   * @throws javax.jcr.RepositoryException
+   * @throws RepositoryLoginException
+   * @throws JSONException
+   */
+  public void testCheckpoint() throws RepositoryException, LoginException,
+      javax.jcr.RepositoryException, JSONException {
+
+    MockRepositoryEventList mrel =
+        new MockRepositoryEventList("MockRepositoryEventLog1.txt");
+    MockRepository r = new MockRepository(mrel);
+    QueryManager qm = new MockJcrQueryManager(r.getStore());
+    TraversalManager qtm = new SpiTraversalManagerFromJcr(qm);
+
+    {
+      MockRepositoryDocument doc = r.getStore().getDocByID("doc1");
+      PropertyMap pm = new SpiPropertyMapFromJcr(new MockJcrNode(doc));
+
+      String checkpointString = qtm.checkpoint(pm);
+      logger.info(checkpointString);
+
+      JSONObject jo = new JSONObject(checkpointString);
+
+      String lastModified = jo.getString("lastModified");
+      Assert.assertEquals("1970-01-01T00:00:10.000Z", lastModified);
+      String uuid = jo.getString("uuid");
+      Assert.assertEquals("doc1", uuid);
+    }
+  }
+
+  public void testExtractFromCheckpoint() throws JSONException {
+    String checkpointString =
+        "{\"uuid\":\"doc1\","
+            + "\"lastModified\":\"1970-01-01T00:00:10.000Z\"}";
+    SpiTraversalManagerFromJcr qtm =
+        new SpiTraversalManagerFromJcr(null);
+
+    JSONObject jo = new JSONObject(checkpointString);
+    Calendar c = qtm.extractCalendarFromCheckpoint(jo, checkpointString);
+    long millis = c.getTimeInMillis();
+    Assert.assertEquals(millis, 10000);
+
+    String id = qtm.extractDocidFromCheckpoint(jo, checkpointString);
+    Assert.assertEquals(id, "doc1");
+  }
+
+  public void testResumeTraversal() throws RepositoryException {
+
+    MockRepositoryEventList mrel =
+        new MockRepositoryEventList("MockRepositoryEventLog1.txt");
+    MockRepository r = new MockRepository(mrel);
+    QueryManager qm = new MockJcrQueryManager(r.getStore());
+    TraversalManager qtm = new SpiTraversalManagerFromJcr(qm);
+
+    {
+      MockRepositoryDocument doc = r.getStore().getDocByID("doc2");
+      PropertyMap pm = new SpiPropertyMapFromJcr(new MockJcrNode(doc));
+
+      String checkpointString = qtm.checkpoint(pm);
+
+      PropertyMapList propertyMapList = qtm.resumeTraversal(checkpointString);
+
+      int counter = 0;
+      for (Iterator iter = propertyMapList.iterator(); iter.hasNext();) {
+        PropertyMap propertyMap = (PropertyMap) iter.next();
+        logger.info(propertyMap.getProperty(SpiConstants.PROPNAME_DOCID)
+            .getValue().getString());
+        counter++;
+      }
+      Assert.assertEquals(2, counter);
+    }
+
+    {
+      MockRepositoryDocument doc = r.getStore().getDocByID("doc4");
+      PropertyMap pm = new SpiPropertyMapFromJcr(new MockJcrNode(doc));
+
+      String checkpointString = qtm.checkpoint(pm);
+
+      PropertyMapList propertyMapList = qtm.resumeTraversal(checkpointString);
+
+      int counter = 0;
+      for (Iterator iter = propertyMapList.iterator(); iter.hasNext();) {
+        PropertyMap propertyMap = (PropertyMap) iter.next();
+        logger.info(propertyMap.getProperty(SpiConstants.PROPNAME_DOCID)
+            .getValue().getString());
+        counter++;
+      }
+      Assert.assertEquals(0, counter);
+    }
+  }
+
+  public void testStartTraversal() throws RepositoryException {
+
+    MockRepositoryEventList mrel =
+        new MockRepositoryEventList("MockRepositoryEventLog1.txt");
+    MockRepository r = new MockRepository(mrel);
+    QueryManager qm = new MockJcrQueryManager(r.getStore());
+    TraversalManager qtm = new SpiTraversalManagerFromJcr(qm);
+
+    {
+      PropertyMapList propertyMapList = qtm.startTraversal();
+
+      int counter = 0;
+      for (Iterator iter = propertyMapList.iterator(); iter.hasNext();) {
+        PropertyMap propertyMap = (PropertyMap) iter.next();
+        logger.info(propertyMap.getProperty(SpiConstants.PROPNAME_DOCID)
+            .getValue().getString());
+        counter++;
+      }
+      Assert.assertEquals(4, counter);
+    }
+  }
+
+}
