a42a42845697238dbcf43149e660a90984a06e0b
Max Ziff <donald.ziff@gmail.com>
2007-07-19 16:52:38 +0000

    Small cleanups from previous checkin

--- donald.ziff/spiwork/projects/connector-manager/source/java/com/google/enterprise/connector/spi/SimpleDocument.java
+++ donald.ziff/spiwork/projects/connector-manager/source/java/com/google/enterprise/connector/spi/SimpleDocument.java
@@ -8,27 +8,16 @@ public class SimpleDocument implements Document {
   private Map properties;
   private Iterator iterator;
   private Property property;
-  private String checkpoint;
 
   public SimpleDocument(Map properties) {
     this.properties = properties;
     this.iterator = null;
     this.property = null;
-    this.checkpoint = null;
-  }
-
-  private void setCheckpoint() throws RepositoryException {
-    if (checkpoint != null) {
-      return;
-    }
-    Value v = Value.getSingleValueByPropertyName(this,
-        SpiConstants.PROPNAME_LASTMODIFIED);
-    checkpoint = v.toString();
   }
 
   public String checkpoint() throws RepositoryException {
-    setCheckpoint();
-    return checkpoint;
+    return Value.getSingleValueByPropertyName(this,
+        SpiConstants.PROPNAME_LASTMODIFIED).toString();
   }
 
   public boolean findProperty(String name) {
@@ -39,26 +28,19 @@ public class SimpleDocument implements Document {
     return (property != null);
   }
 
+  public Property getProperty() {
+    return property;
+  }
+
   public boolean nextProperty() {
     if (iterator == null) {
       iterator = properties.values().iterator();
-      try {
-        setCheckpoint();
-      } catch (RepositoryException e) {
-        throw new IllegalStateException();
-      }
     }
     boolean hasNext = iterator.hasNext();
     if (hasNext) {
       property = (Property) iterator.next();
-    } else {
-      property = null;
-    }
+    } 
     return hasNext;
-  }
-
-  public Property getProperty() {
-    return property;
   }
 
 }
