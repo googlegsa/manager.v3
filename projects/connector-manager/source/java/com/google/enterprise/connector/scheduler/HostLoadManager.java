34e8abeb69618cc74169c891457760efa6828c46
Vivek Haldar <haldar@google.com>
2007-07-26 00:27:01 +0000

    Make CM build with java 1.4.2 again

--- projects/connector-manager/source/java/com/google/enterprise/connector/scheduler/HostLoadManager.java
+++ projects/connector-manager/source/java/com/google/enterprise/connector/scheduler/HostLoadManager.java
@@ -126,7 +126,7 @@ public class HostLoadManager {
    * @param connectorName name of the connector instance
    */
   public void connectorFinishedTraversal(String connectorName) {
-    long now = System.currentTimeMillis();
+    Long now = new Long(System.currentTimeMillis());
     connectorNameToFinishTime.put(connectorName, now);
   }
   
