dfe68c9d19ed4c1564a2c112fd79475b884491b3
mgronber <mgronber@56db1fc5-3b1b-0410-9cff-4307dac5e09e>
2007-08-07 16:24:56 +0000

    Changing to create the default keystore within the context of the WebApp so we know it will be accessable for read/write.  Also changing the default location to put the keystore in the WEB-INF directory so it will be protected by the Servlet Container.  This fix is needed on the GSA to insure the initial keystore can be created using the UID running Tomcat.

---
+++ changes/mgronber/fix-keystore-file-create/trunk/projects/connector-manager/source/java/com/google/enterprise/connector/servlet/StartUp.java
@@ -0,0 +1,94 @@
+// Copyright (C) 2006 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.enterprise.connector.servlet;
+
+import com.google.enterprise.connector.instantiator.EncryptedPropertyPlaceholderConfigurer;
+import com.google.enterprise.connector.manager.Context;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.logging.Logger;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * The main purpose of this servlet is to have its "init" method called when the
+ * container starts up. This is by done by means of the web.xml file. But I also
+ * gave it a get and post that do the same thing.
+ * 
+ */
+public class StartUp extends HttpServlet {
+  private static final Logger LOGGER =
+      Logger.getLogger(StartUp.class.getName());
+
+  public void init() {
+    LOGGER.info("init");
+    ServletContext servletContext = this.getServletContext();
+    try {
+      doConnectorManagerStartup(servletContext);
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+    LOGGER.info("init done");
+  }
+
+  public void destroy() {
+    LOGGER.info("destroy");
+    Context.getInstance().shutdown(true);
+  }
+  
+  protected void doGet(HttpServletRequest req, HttpServletResponse res)
+      throws ServletException, IOException {
+    doPost(req, res);
+  }
+
+  protected void doPost(HttpServletRequest req, HttpServletResponse res)
+      throws ServletException, IOException {
+    ServletContext servletContext = this.getServletContext();
+    doConnectorManagerStartup(servletContext);
+    res.setContentType(ServletUtil.MIMETYPE_HTML);
+    PrintWriter out = res.getWriter();
+    out.println("<HTML><HEAD><TITLE>Connector Manager Started</TITLE></HEAD>"
+        + "<BODY>Connector manager has been successfully started.</BODY>"
+        + "</HTML>");
+    out.close();
+    LOGGER.info("Connector Manager started.");
+  }
+
+  public static void doConnectorManagerStartup(ServletContext servletContext)
+      throws IOException {
+
+    // read in and set initialization parameters
+    String kp = servletContext.getInitParameter("keystore_passwd_file");
+    EncryptedPropertyPlaceholderConfigurer.setKeyStorePasswdPath(kp);
+    
+    String ks = servletContext.getInitParameter("keystore_file");
+    String realks = servletContext.getRealPath(ks);
+    EncryptedPropertyPlaceholderConfigurer.setKeyStorePath(realks);
+
+    String kt = servletContext.getInitParameter("keystore_type");
+    EncryptedPropertyPlaceholderConfigurer.setKeyStoreType(kt);
+
+    String ka = servletContext.getInitParameter("keystore_crypto_algo");
+    EncryptedPropertyPlaceholderConfigurer.setKeyStoreCryptoAlgo(ka);
+
+    Context context = Context.getInstance(servletContext);
+    context.start();
+  }
+}
