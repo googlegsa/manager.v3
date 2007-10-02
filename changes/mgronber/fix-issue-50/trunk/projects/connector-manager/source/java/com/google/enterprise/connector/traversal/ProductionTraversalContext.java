ae7afe6cbd058c9fc903ca1e0994874128b94058
mgronber <mgronber@56db1fc5-3b1b-0410-9cff-4307dac5e09e>
2007-10-02 04:49:28 +0000

    Added a decryption step to reading the property file when used to create and store the InstanceInfo that is used when the GSA requests Connector Configuration to edit.

---
+++ changes/mgronber/fix-issue-50/trunk/projects/connector-manager/source/java/com/google/enterprise/connector/traversal/ProductionTraversalContext.java
@@ -0,0 +1,46 @@
+// Copyright 2007 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.enterprise.connector.traversal;
+
+import com.google.enterprise.connector.spi.TraversalContext;
+
+/**
+ * Wrapper for the context provided by the Connector Manager to the terversal
+ * process (if the developer requests it by the TraversalContextAware interface).
+ * This class is quasi-immutable - in practice it is initialized by the setters
+ * but never changes afterwards.
+ */
+public class ProductionTraversalContext implements TraversalContext {
+
+  FileSizeLimitInfo fileSizeLimitInfo;
+  MimeTypeMap mimeTypeMap;
+  
+  public void setFileSizeLimitInfo(FileSizeLimitInfo fileSizeLimitInfo) {
+    this.fileSizeLimitInfo = fileSizeLimitInfo;
+  }
+
+  public void setMimeTypeMap(MimeTypeMap mimeTypeMap) {
+    this.mimeTypeMap = mimeTypeMap;
+  }
+
+  public long maxDocumentSize() {
+    return fileSizeLimitInfo.maxDocumentSize();
+  }
+
+  public int mimeTypeSupportLevel(String mimeType) {
+    return mimeTypeMap.mimeTypeSupportLevel(mimeType);
+  }
+
+}
