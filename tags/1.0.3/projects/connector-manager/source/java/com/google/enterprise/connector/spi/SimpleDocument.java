a347cf9e5184622ff46d56775ff71548247697e4
mgronber <mgronber@56db1fc5-3b1b-0410-9cff-4307dac5e09e>
2007-12-16 02:19:38 +0000

    Tagging the 1.0.3 Release from r722 of the branch

---
+++ tags/1.0.3/projects/connector-manager/source/java/com/google/enterprise/connector/spi/SimpleDocument.java
@@ -0,0 +1,35 @@
+// Copyright 2007 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.enterprise.connector.spi;
+
+import java.util.Map;
+import java.util.Set;
+
+public class SimpleDocument implements Document {
+
+  private Map properties;
+
+  public SimpleDocument(Map properties) {
+    this.properties = properties;
+  }
+
+  public Property findProperty(String name) {
+    return (Property) properties.get(name);
+  }
+
+  public Set getPropertyNames() {
+    return properties.keySet();
+  }
+}
