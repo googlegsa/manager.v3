5845a1ca9e4def524cc5cd750980ea57c314d92f
mgronber <mgronber@56db1fc5-3b1b-0410-9cff-4307dac5e09e>
2008-05-06 23:07:57 +0000

    Fix Issue 75 - Http 400 Error Thrown For Correct Feeds (When Using Multiple Connectors)
    This Was Related To The Docpusher.Take() Method Not Being Thread-Safe.  I Have
    Removed The Use Of Several Instance Fields (Xmldata, Datasource, And Feedtype)
    From The Method And Moved The New Feedlogrecord Into A Threadlocal.  The
    Feedconnection.Senddata() Seems To Be Thread-Safe Since It Creates A New
    Connection Each Time So I Have Left That As It Is. 
    
    It Should Be Noted That The Retrieval Gsaresponse Instance Field Is Not
    Thread-Safe, However, It Is Only Used From Tests.
    
    Tested With 2 Connectors Each Feeding 600 Documents And No Problems.

---
+++ changes/mgronber/fix-issue-75/trunk/projects/connector-manager/source/java/com/google/enterprise/connector/servlet/SetConnectorConfig.java
@@ -0,0 +1,105 @@
+// Copyright (C) 2006 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
+package com.google.enterprise.connector.servlet;
+
+import com.google.enterprise.connector.manager.Context;
+import com.google.enterprise.connector.manager.Manager;
+import com.google.enterprise.connector.persist.ConnectorTypeNotFoundException;
+import com.google.enterprise.connector.spi.ConfigureResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+/**
+ * Admin servlet to set connector config.
+ * 
+ */
+public class SetConnectorConfig extends ConnectorManagerServlet {
+  private static final Logger LOGGER =
+    Logger.getLogger(SetConnectorConfig.class.getName());
+
+  /**
+   * doGet just call doPost.
+   * @param req 
+   * @param res 
+   * @throws IOException 
+   * 
+   */
+  protected void doGet(HttpServletRequest req,
+                       HttpServletResponse res)
+      throws IOException {
+    ConnectorMessageCode status = new ConnectorMessageCode();
+    String language = req.getParameter(ServletUtil.QUERY_PARAM_LANG);
+    String connectorType = req.getParameter(ServletUtil.XMLTAG_CONNECTOR_TYPE);
+    PrintWriter out = res.getWriter();
+    res.setContentType(ServletUtil.MIMETYPE_XML);
+
+    ServletContext servletContext = this.getServletContext();
+    Manager manager = Context.getInstance(servletContext).getManager();
+    String formSnippet = null;
+    ConfigureResponse configResponse = null;
+    try {
+      configResponse = manager.getConfigForm(connectorType, language);
+      if (configResponse != null) {
+        formSnippet = configResponse.getFormSnippet();
+      }
+    } catch (ConnectorTypeNotFoundException e) {
+      status = new ConnectorMessageCode(
+          ConnectorMessageCode.EXCEPTION_CONNECTOR_TYPE_NOT_FOUND,
+              connectorType);
+      ServletUtil.writeResponse(out, status);
+      LOGGER.log(Level.WARNING,
+          ServletUtil.LOG_EXCEPTION_CONNECTOR_TYPE_NOT_FOUND, e);
+    }
+
+    if (formSnippet == null) {
+      formSnippet = ServletUtil.DEFAULT_FORM;
+    }
+
+    GetConfigForm.handleDoGet(configResponse, status, out);
+    out.close();
+  }
+
+  /**
+   * Writes the XML response for setting the connector config.
+   */
+  /*
+   * (non-Javadoc)
+   * @see com.google.enterprise.connector.servlet.ConnectorManagerServlet
+   * #processDoPost(java.lang.String,
+   * com.google.enterprise.connector.manager.Manager, java.io.PrintWriter)
+   */
+  protected void processDoPost(
+      String xmlBody, Manager manager, PrintWriter out) {
+    SetConnectorConfigHandler handler =
+        new SetConnectorConfigHandler(xmlBody, manager);
+    ConfigureResponse configRes = handler.getConfigRes();
+    ConnectorMessageCode status = (configRes == null) ? handler.getStatus() :
+    	new ConnectorMessageCode(ConnectorMessageCode.INVALID_CONNECTOR_CONFIG);
+
+    ConnectorManagerGetServlet.writeConfigureResponse(
+        out, status, configRes);
+    out.close();
+  }
+
+}
