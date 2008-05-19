086ceab251fb48a437f0f19e5b65db3936bc2860
Brett.Michael.Johnson <Brett.Michael.Johnson@56db1fc5-3b1b-0410-9cff-4307dac5e09e>
2008-05-19 23:32:22 +0000

    This set of changes addresses Connector Manager Issue 47. When a connector was deleted, its persistent ConnectorStateStore (the stored Checkpoint) and ConnectorScheduleStore are deleted. However, a slow-to-die worker thread may, as its last gasp, rewrite the ConnectorStateStore.  As a result, deleting and recreating a connector would often result in the new connector starting up where the old connector left off, rather than starting from the beginning.
    Many layers of abstraction make it difficult to communicate
    deleted connector status down to the worker threads, so we
    decided to provide a mechanism that can enable and disable 
    the ConnectorStateStore for a specific connector.  The CM
    then disables the store when a connector is deleted, and
    enables the store when a connector is created.  Attempting
    to read or write a disabled store throws an exception.
    
    Similar changes were not needed for the ConnectorScheduleStore
    because it is not managed asynchronously (yet?).
    
    ChangeLog:
    ---------
    
    * java/com/google/enterprise/connector/persist/ConnectorStateStore.java:
      - Add enable and disable methods to the ConnectorStateStore Interface.
    
    * java/com/google/enterprise/connector/persist/PrefsStore.java:
      - Implement enable and disable methods in the ConnectorStateStore
        Implementation.
    
    * java/com/google/enterprise/connector/instantiator/SpringInstantiator.java:
      - Enable a connector's ConnectorStateStore when creating a new 
        instance of a connector.  See also the version of SpringInstantiator
        that fixes Issues 35, 60, and 63.  When both branches are merged
        into the trunk, that version of SpringInstantiator should be used
        (with the commented out Issue 47 line restored);  not this one.
    
    * java/com/google/enterprise/connector/manager/ProductionManager.java:
      - Disable a connector's ConnectorStateStore when deleting the connector.
    
    * java/com/google/enterprise/connector/traversal/QueryTraverser.java:
      - Catch IllegalStateExceptions when reading or writing a connector's 
        state.  These exceptions mean a connector has ceased to exist and 
        they are dealt with accordingly.
    
    * javatests/com/google/enterprise/connector/instantiator/MockInstantiator.java:
      - Enable a connector's ConnectorStateStore when creating a new 
        connector and disable it when deleting a connector.
    
    * javatests/com/google/enterprise/connector/persist/MockConnectorStateStore.java:
      - Implement enable and disable methods in the mock ConnectorStateStore
        implementation.
    
    * javatests/com/google/enterprise/connector/persist/PrefsStoreTest.java:
      - Add tests for the enable/disable functionality.
    
    Brett Johnson

---
+++ changes/bjohnson/fix_issue_47/trunk/projects/connector-manager/source/java/com/google/enterprise/connector/instantiator/SpringInstantiator.java
@@ -0,0 +1,250 @@
+// Copyright (C) 2006 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.enterprise.connector.instantiator;
+
+import com.google.enterprise.connector.persist.ConnectorExistsException;
+import com.google.enterprise.connector.persist.ConnectorNotFoundException;
+import com.google.enterprise.connector.persist.ConnectorStateStore;
+import com.google.enterprise.connector.persist.ConnectorTypeNotFoundException;
+import com.google.enterprise.connector.pusher.Pusher;
+import com.google.enterprise.connector.spi.AuthenticationManager;
+import com.google.enterprise.connector.spi.AuthorizationManager;
+import com.google.enterprise.connector.spi.ConfigureResponse;
+import com.google.enterprise.connector.spi.ConnectorType;
+import com.google.enterprise.connector.traversal.Traverser;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Locale;
+import java.util.Map;
+import java.util.logging.Logger;
+
+/**
+ * 
+ */
+public class SpringInstantiator implements Instantiator {
+  
+  private static final Logger LOGGER =
+    Logger.getLogger(SpringInstantiator.class.getName());
+
+  TypeMap typeMap = null;
+  InstanceMap instanceMap = null;
+  Pusher pusher;
+  ConnectorStateStore connectorStateStore;
+  Map connectorCache;
+
+  public SpringInstantiator(Pusher pusher, 
+      ConnectorStateStore connectorStateStore) {
+    this.pusher = pusher;
+    this.connectorStateStore = connectorStateStore;
+    connectorCache = new HashMap();
+    // NOTE: we can't call initialize() here because then there would be a
+    // circular dependency on the Context, which hasn't been constructed yet
+  }
+
+  private void initialize() {
+    if (typeMap != null) {
+      return;
+    }
+    LOGGER.info("Initializing instantiator");
+    typeMap = new TypeMap();
+    instanceMap = new InstanceMap(typeMap);
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see com.google.enterprise.connector.instantiator.Instantiator
+   *      #dropConnector(java.lang.String)
+   */
+  public void dropConnector(String connectorName) {
+    initialize();
+    instanceMap.dropConnector(connectorName);
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see com.google.enterprise.connector.instantiator.Instantiator#getAuthenticationManager(java.lang.String)
+   */
+  public AuthenticationManager getAuthenticationManager(String connectorName)
+      throws ConnectorNotFoundException, InstantiatorException {
+    initialize();
+    ConnectorInterfaces connectorInterfaces =
+        getConnectorInterfaces(connectorName);
+    return connectorInterfaces.getAuthenticationManager();
+  }
+
+  private ConnectorInterfaces getConnectorInterfaces(String connectorName)
+      throws ConnectorNotFoundException {
+    initialize();
+    ConnectorInterfaces connectorInterfaces =
+        (ConnectorInterfaces) connectorCache.get(connectorName);
+    if (connectorInterfaces == null) {
+      InstanceInfo instanceInfo = (InstanceInfo) instanceMap.get(connectorName);
+      if (instanceInfo == null) {
+        throw new ConnectorNotFoundException();
+      }
+      connectorStateStore.enableConnectorState(connectorName);
+      connectorInterfaces =
+          new ConnectorInterfaces(connectorName, instanceInfo.getConnector(),
+              pusher, connectorStateStore, instanceInfo.getProperties());
+      connectorCache.put(connectorName, connectorInterfaces);
+    }
+    return connectorInterfaces;
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see com.google.enterprise.connector.instantiator.Instantiator#getAuthorizationManager(java.lang.String)
+   */
+  public AuthorizationManager getAuthorizationManager(String connectorName)
+      throws ConnectorNotFoundException, InstantiatorException {
+    initialize();
+    ConnectorInterfaces connectorInterfaces =
+        getConnectorInterfaces(connectorName);
+    return connectorInterfaces.getAuthorizationManager();
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see com.google.enterprise.connector.instantiator.Instantiator#getConfigFormForConnector(java.lang.String,
+   *      java.lang.String, java.lang.String)
+   */
+  public ConfigureResponse getConfigFormForConnector(String connectorName,
+      String connectorTypeName, Locale locale)
+      throws ConnectorNotFoundException {
+    initialize();
+    InstanceInfo instanceInfo = (InstanceInfo) instanceMap.get(connectorName);
+    if (instanceInfo == null) {
+      throw new ConnectorNotFoundException();
+    }
+    TypeInfo typeInfo = instanceInfo.getTypeInfo();
+    ConnectorType connectorType = typeInfo.getConnectorType();
+    Map configMap = instanceInfo.getProperties();
+    ConfigureResponse configureResponse =
+        connectorType.getPopulatedConfigForm(configMap, locale);
+    return configureResponse;
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see com.google.enterprise.connector.instantiator.Instantiator#getConnectorInstancePrototype(java.lang.String)
+   */
+  public String getConnectorInstancePrototype(String connectorTypeName) {
+    throw new UnsupportedOperationException();
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see com.google.enterprise.connector.instantiator.Instantiator#getConnectorType(java.lang.String)
+   */
+  public ConnectorType getConnectorType(String connectorTypeName)
+      throws ConnectorTypeNotFoundException {
+    initialize();
+    TypeInfo typeInfo = typeMap.getTypeInfo(connectorTypeName);
+    if (typeInfo == null) {
+      throw new ConnectorTypeNotFoundException();
+    }
+    return typeInfo.getConnectorType();
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see com.google.enterprise.connector.instantiator.Instantiator#getConnectorTypeNames()
+   */
+  public Iterator getConnectorTypeNames() {
+    initialize();
+    return Collections.unmodifiableSet(typeMap.keySet()).iterator();
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see com.google.enterprise.connector.instantiator.Instantiator#getTraverser(java.lang.String)
+   */
+  public Traverser getTraverser(String connectorName)
+      throws ConnectorNotFoundException, InstantiatorException {
+    initialize();
+    ConnectorInterfaces connectorInterfaces =
+        getConnectorInterfaces(connectorName);
+    return connectorInterfaces.getTraverser();
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see com.google.enterprise.connector.instantiator.Instantiator#setConnectorConfig(java.lang.String,
+   *      java.lang.String, java.util.Map)
+   */
+  public ConfigureResponse setConnectorConfig(String connectorName,
+      String connectorTypeName, Map configKeys, Locale locale,
+      boolean update)
+      throws ConnectorNotFoundException, ConnectorExistsException,
+      InstantiatorException {
+    initialize();
+    return instanceMap.updateConnector(
+        connectorName, connectorTypeName, configKeys, locale, update);
+
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see com.google.enterprise.connector.persist.ConnectorConfigStore#getConnectorNames()
+   */
+  public Iterator getConnectorNames() {
+    initialize();
+    return Collections.unmodifiableSet(instanceMap.keySet()).iterator();
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see com.google.enterprise.connector.persist.ConnectorConfigStore#getConnectorTypeName(java.lang.String)
+   */
+  public String getConnectorTypeName(String connectorName)
+      throws ConnectorNotFoundException {
+    initialize();
+    InstanceInfo instanceInfo = (InstanceInfo) instanceMap.get(connectorName);
+    if (instanceInfo == null) {
+      throw new ConnectorNotFoundException("Connector not found: " 
+        + connectorName);
+    }
+    return instanceInfo.getTypeInfo().getConnectorTypeName();
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see com.google.enterprise.connector.instantiator.Instantiator#getConnectorConfig(java.lang.String)
+   */
+  public Map getConnectorConfig(String connectorName)
+      throws ConnectorNotFoundException {
+    initialize();
+    InstanceInfo instanceInfo = (InstanceInfo) instanceMap.get(connectorName);
+    if (instanceInfo == null) {
+      throw new ConnectorNotFoundException("Connector not found: " 
+        + connectorName);
+    }
+    return instanceInfo.getProperties();
+  }
+}
---
+++ changes/bjohnson/fix_issue_47/trunk/projects/connector-manager/source/javatests/com/google/enterprise/connector/persist/PrefsStoreTest.java
@@ -0,0 +1,195 @@
+// Copyright 2006 Google Inc.  All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package com.google.enterprise.connector.persist;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+/**
+ * Class to test ScheduleStore.
+ */
+public class PrefsStoreTest extends TestCase {
+
+  protected PrefsStore store;
+  
+  protected void setUp() {
+    // Only testing use of userRoot
+    store = new PrefsStore(true);
+  }
+  
+  protected void tearDown() {
+    store.clear();
+  }
+  
+  // Tests getting and setting for a valid connector name and schedule.
+  
+  public void testGetandSetConnectorSchedule() {
+    String expectedSchedule = "schedule of connectorA";
+    String connectorName = "connectorA";
+    store.storeConnectorSchedule(connectorName, expectedSchedule);
+    Assert.assertTrue(store.flush());
+    String resultSchedule = store.getConnectorSchedule(connectorName);
+    Assert.assertTrue(resultSchedule.equals(expectedSchedule));
+  }
+  
+  // Tests getting schedule for an unknown connector
+  public void testGetConnectorSchedule1() {
+    String schedule = store.getConnectorSchedule("some wierd connector name");
+    Assert.assertNull(schedule);
+  }
+  
+  // Tests if the exception is thrown correctly when the connector name is null. 
+  public void testGetConnectorSchedule2() {
+    boolean exceptionCaught = false;
+    String schedule = null;
+    try {
+      schedule = store.getConnectorSchedule(null);
+    } catch (NullPointerException e) {
+        exceptionCaught = true;
+    }
+    Assert.assertTrue(exceptionCaught);
+    Assert.assertNull(schedule);
+  }
+
+  public void testRemoveConnector() {
+    String connectorName = "foo";
+    String connectorSchedule = "foo's schedule";
+    String schedule = store.getConnectorSchedule(connectorName);
+    Assert.assertNull(schedule);
+    store.storeConnectorSchedule(connectorName, connectorSchedule);
+    schedule = store.getConnectorSchedule(connectorName);
+    Assert.assertEquals(connectorSchedule, schedule);
+    store.removeConnectorSchedule(connectorName);
+    schedule = store.getConnectorSchedule(connectorName);
+    Assert.assertNull(schedule);
+  }
+  
+  //Tests getting and setting for a valid connector name and state.  
+  public void testGetandSetConnectorState() {
+    String expectedState = "state of connectorA";
+    String connectorName = "connectorA";
+    store.storeConnectorState(connectorName, expectedState);
+    Assert.assertTrue(store.flush());
+    String resultState = store.getConnectorState(connectorName);
+    Assert.assertTrue(resultState.equals(expectedState));
+  }
+  
+  //Tests getting state for an unknown connector.
+  public void testGetConnectorState1() {
+    String state = store.getConnectorState("some wierd connector name");
+    Assert.assertNull(state);
+  }
+  
+  // Tests if the exception is thrown correctly when the connector name is null. 
+  public void testGetConnectorState2() {
+    boolean exceptionCaught = false;
+    try {
+      String state = store.getConnectorState(null);
+    } catch (NullPointerException e) {
+        exceptionCaught = true;
+    }
+    Assert.assertTrue(exceptionCaught);
+  }  
+
+  public void testRemoveState() {
+    String connectorName = "foo";
+    String connectorState = "foo's state";
+    String state = store.getConnectorState(connectorName);
+    Assert.assertNull(state);
+    store.storeConnectorState(connectorName, connectorState);
+    state = store.getConnectorState(connectorName);
+    Assert.assertEquals(connectorState, state);
+    store.removeConnectorState(connectorName);
+    state = store.getConnectorState(connectorName);
+    Assert.assertNull(state);
+  }
+
+
+  // Test enabling/disabling connector state store.
+  // [Issue 47]  The connector state should not be able
+  // to be read or written in the gap between a connector
+  // being deleted and recreated.
+  public void testEnableDisableState() {
+    String barName = "bar";
+    String barState = "bar's state";
+    String barNewState = "bar's new state";
+
+    String bazName = "baz";
+    String bazState = "baz's state";
+    String bazNewState = "baz's new state";
+
+    String state = store.getConnectorState(barName);
+    Assert.assertNull(state);
+    store.storeConnectorState(barName, barState);
+
+    store.storeConnectorState(bazName, bazState);
+
+    state = store.getConnectorState(barName);
+    Assert.assertEquals(barState, state);
+
+    state = store.getConnectorState(bazName);
+    Assert.assertEquals(bazState, state);
+
+    // Disable connector state store for bar.
+    store.disableConnectorState(barName);
+
+    // Although the store is disabled for bar,
+    // we should still be able to get and set baz's state.
+    store.storeConnectorState(bazName, bazNewState);
+    state = store.getConnectorState(bazName);
+    Assert.assertEquals(bazNewState, state);
+
+    // Attempting to read or write to a disabled
+    // connector state store should throw an exception.
+    boolean exceptionCaught = false;
+    try {
+        // This should throw an IllegalStateException.
+        state = store.getConnectorState(barName);
+    } catch (IllegalStateException e) {
+        exceptionCaught = true;
+    }
+    Assert.assertTrue(exceptionCaught);
+
+    exceptionCaught = false;
+    try {
+        // This should throw an IllegalStateException.
+        store.storeConnectorState(barName, barNewState);
+    } catch (IllegalStateException e1) {
+        exceptionCaught = true;
+    }
+    Assert.assertTrue(exceptionCaught);
+
+    // Re-enable state store for the connector.
+    store.enableConnectorState(barName);
+
+    // Make sure the new store attempted while disabled didn't take.
+    state = store.getConnectorState(barName);
+    Assert.assertEquals(barState, state);
+
+    // Make sure we can store once re-enabled.
+    store.storeConnectorState(barName, barNewState);
+    state = store.getConnectorState(barName);
+    Assert.assertEquals(barNewState, state);
+
+    // We should still be able to remove the connector state,
+    // even when disabled.  [Issue 47]
+    store.disableConnectorState(barName);
+    store.removeConnectorState(barName);
+    store.enableConnectorState(barName);    
+    state = store.getConnectorState(barName);
+    Assert.assertNull(state);
+
+    store.removeConnectorState(bazName);
+  }
+}
