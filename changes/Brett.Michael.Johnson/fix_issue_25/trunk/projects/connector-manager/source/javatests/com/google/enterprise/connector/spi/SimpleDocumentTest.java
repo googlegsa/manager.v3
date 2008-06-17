ea7f719ab3787a3076c5726b4831a0af40734954
Brett.Michael.Johnson <Brett.Michael.Johnson@56db1fc5-3b1b-0410-9cff-4307dac5e09e>
2008-06-17 04:07:35 +0000

    This set of changes addresses Connector Manager Issue 25 - Forcing a Connector to re-traverse the document repository. This is a useful tool when troubleshooting customer-specific configuration issues.  It is also useful when the customer changes certain configuration options, such as adding or deleting folders to index or changing the traversal user.
    Much of this change supercedes some of the changes made
    to fix Issue 47.  This implementation not only fixes 
    Issue 47, but also addresses this problem and a potential
    fix for hanging worker threads.
    
    This feature is provided by a Connector Manager Servlet
    command, restartConnectorTraversal, supplying it the name 
    of the connector instance to restart.
    
    Usage:
    -----
    To restart the indexing traversal for a Connector:
      http://[cm_host_addr]/connector_manager/restartConnectorTraversal?[connector_name]
    where [connector_name] is the name of a connector instance.
    
    
    Change Log:
    ----------
    
    M webdocs/web.xml
      - Add description for RestartConnectorTraversal servlet.
    
    M java/com/google/enterprise/connector/instantiator/Instantiator.java
      - Prototype restartTraversal() method interface.
    
    M java/com/google/enterprise/connector/instantiator/SpringInstantiator.java
      - Replace ConnectorStateStore.enable() and .disable() calls with
        GenerationalStateStore calls.
    
      - Implement restartTraversal() method.  Blows away the ConnectorStateStore
        for the connector and bumps the generation, preventing any outstanding
        workers from re-writing the store.  The null ConnectorStateStore will
        force the connector's startTraversal() method to be called.
        
    A java/com/google/enterprise/connector/servlet/RestartConnectorTraversal.java
      - New servlet interface provides a mechanism to restart the traversal.
            
    M java/com/google/enterprise/connector/servlet/ServletUtil.java
      - Added an XML tag for the restartConnectorTraversal response.
    
    A java/com/google/enterprise/connector/persist/GenerationalStateStore.java
      - Wrapper over a ConnectorStateStore that adds the concept of 'generations'
        to the store.  Once a new generation is created, older generations are
        prevented from reading or writing the store.
    
    M java/com/google/enterprise/connector/persist/ConnectorStateStore.java
      - Remove the enable() and disable() methods from the interface.
    
    M java/com/google/enterprise/connector/persist/PrefsStore.java
      - Remove the enable() and disable() methods from the implementation.
    
    M java/com/google/enterprise/connector/manager/Manager.java
      - Add restartConnectorTraversal() method prototype.
    
    M java/com/google/enterprise/connector/manager/ProductionManager.java
      - Add restartConnectorTraversal() method implementation.  It is an
        intermediate between the Servlet and the Instantiator.
    
    M javatests/com/google/enterprise/connector/instantiator/MockInstantiator.java
      - Replace ConnectorStateStore.enable() and .disable() calls with
        GenerationalStateStore calls.
    
      - Implement restartTraversal() method.  Blows away the ConnectorStateStore
        for the connector and bumps the generation, preventing any outstanding
        workers from re-writing the store.  The null ConnectorStateStore will
        force the connector's startTraversal() method to be called.
    
    A javatests/com/google/enterprise/connector/persist/GenerationalStateStoreTest.java
      - Test the Generational Connector State Store implementation.
      
    M javatests/com/google/enterprise/connector/persist/MockConnectorStateStore.java
      - Remove the enable() and disable() methods from the implementation.
    
    M javatests/com/google/enterprise/connector/persist/PrefsStoreTest.java
      - Remove the testEnableDisable() test.
    
    M javatests/com/google/enterprise/connector/manager/MockManager.java
      - Add a no-op restartConnectorTraversal() method implementation.

---
+++ changes/Brett.Michael.Johnson/fix_issue_25/trunk/projects/connector-manager/source/javatests/com/google/enterprise/connector/spi/SimpleDocumentTest.java
@@ -0,0 +1,98 @@
+// Copyright (C) 2008 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.enterprise.connector.spi;
+
+import junit.framework.TestCase;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class SimpleDocumentTest extends TestCase {
+  private SimpleDocument document;
+
+  protected void setUp() throws Exception {
+    Map props = new HashMap();
+    Calendar cal = Calendar.getInstance();
+    cal.setTimeInMillis(10 * 1000);
+
+    props.put(SpiConstants.PROPNAME_LASTMODIFIED, cal);
+    props.put(SpiConstants.PROPNAME_ACTION,
+        SpiConstants.ActionType.DELETE.toString());
+    props.put(SpiConstants.PROPNAME_DOCID, "doc1");
+    props.put(SpiConstants.PROPNAME_CONTENT, "now is the time");
+    props.put(SpiConstants.PROPNAME_CONTENTURL,
+        "http://www.comtesturl.com/test");
+
+    document = createSimpleDocument(props);
+  }
+
+  protected void tearDown() throws Exception {
+    document = null;
+  }
+
+  public void testFindProperty() throws RepositoryException {
+    Set propNames = document.getPropertyNames();    
+    for (Iterator iter = propNames.iterator(); iter.hasNext(); ) {
+      String propName = (String) iter.next();
+      Property prop = document.findProperty(propName);
+      assertNotNull("Checking for " + propName + " property", prop.nextValue());
+    }
+
+    // Now try it again since properties should be able to be reused.
+    for (Iterator iter = propNames.iterator(); iter.hasNext(); ) {
+      String propName = (String) iter.next();
+      Property prop = document.findProperty(propName);
+      assertNotNull("Checking for " + propName + " property", prop.nextValue());
+    }
+  }
+
+  public void testGetPropertyNames() {
+    Set propNames = document.getPropertyNames();
+    assertTrue(propNames.contains(SpiConstants.PROPNAME_LASTMODIFIED));
+    assertTrue(propNames.contains(SpiConstants.PROPNAME_ACTION));
+    assertTrue(propNames.contains(SpiConstants.PROPNAME_DOCID));
+    assertTrue(propNames.contains(SpiConstants.PROPNAME_CONTENT));
+    assertTrue(propNames.contains(SpiConstants.PROPNAME_CONTENTURL));
+  }
+
+  /**
+   * Utility method to convert {@link Map} of Java Objects into a
+   * {@link SimpleDocument}.
+   */
+  private SimpleDocument createSimpleDocument(Map props) {
+    Map spiValues = new HashMap();
+    for (Iterator iter = props.keySet().iterator(); iter.hasNext();) {
+      String key = (String) iter.next();
+      Object obj = props.get(key);
+      Value val = null;
+      if (obj instanceof String) {
+        val = Value.getStringValue((String) obj);
+      } else if (obj instanceof Calendar) {
+        val = Value.getDateValue((Calendar) obj);
+      } else {
+        throw new AssertionError(obj);
+      }
+      List values = new ArrayList();
+      values.add(val);
+      spiValues.put(key, values);
+    }
+    return new SimpleDocument(spiValues);
+  }
+}
