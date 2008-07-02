1d5a1fa3bb60a3e5a82754887f98a61d4c1650c8
Brett.Michael.Johnson <Brett.Michael.Johnson@56db1fc5-3b1b-0410-9cff-4307dac5e09e>
2008-07-02 23:19:20 +0000

    This change addresses a minor annoyance: "Schedule not found" warnings when deleting, then recreating a connector instance. Sometimes when just creating a new connector instance.
    Although I did not exactly pin down the circumstances that 
    lead to the warning message, I have high confidence that it 
    is either:
    
     - A lag between the SetConnectorConfiguration servlet and
       the SetScheduleServlet executing, with the scheduler
       noticing the new connector, but not yet seeing a schedule.
    
    OR
     
     - An interaction between the removedConnectors.clear()
       and 'continue;' commands in the scheduling loop in
       TraversalScheduler.  Specifically, one time through
       the loop, it notices the removedConnector, clears the
       removedConnector map, then hits one of the conditions
       that continue, going back to the top of the loop.  This
       time, the removed connector is not in the removedConnectors
       map, but may still be in the InstanceMap.
    
    
    In any case, the condition seems to be transient, and the 
    Scheduler does the right thing in the absence of a schedule
    (do nothing).
    
    
    Change Log:
    ----------
    M  java/com/google/enterprise/connector/common/WorkQueueThread.java
       - Removed InterruptedException stack dump from the INFO log message.
         This looked too much like an uncaught exception in the logs.
    
    M  java/com/google/enterprise/connector/instantiator/InstanceMap.java
       - dropConnector() now removes the connector instance from the
         InstanceMap before it starts to deconstruct it.  This avoids
         having a partially deconstructed connector instance in the map.
    
    M  java/com/google/enterprise/connector/manager/ProductionManager.java
       - Since the TraversalScheduler gets the list of connectors to
         schedule from the InstanceMap, when deleting a connector we
         should remove it from the InstanceMap before deleting its
         schedule.
    
    M  java/com/google/enterprise/connector/scheduler/TraversalScheduler.java
       - Change the log level of the "Schedule not found" message from
         WARNING to INFO, so as not to alarm the users.

---
+++ changes/Brett.Michael.Johnson/fix_schedule_warning/trunk/projects/connector-manager/source/javatests/com/google/enterprise/connector/instantiator/TypeMapTest.java
@@ -0,0 +1,66 @@
+// Copyright (C) 2006 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.enterprise.connector.instantiator;
+
+import java.io.File;
+
+import com.google.enterprise.connector.instantiator.TypeInfo;
+import com.google.enterprise.connector.instantiator.TypeMap;
+import com.google.enterprise.connector.test.ConnectorTestUtils;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+/**
+ * 
+ */
+public class TypeMapTest extends TestCase {
+
+  private static final String TEST_DIR_NAME = "testdata/tempTypeMapTests";
+  private File baseDirectory;
+
+  protected void setUp() throws Exception {
+    super.setUp();
+    // Make sure that the test directory does not exist
+    baseDirectory = new File(TEST_DIR_NAME);
+    Assert.assertTrue(ConnectorTestUtils.deleteAllFiles(baseDirectory));
+    // Then recreate it empty
+    Assert.assertTrue(baseDirectory.mkdirs());
+  }
+
+  protected void tearDown() throws Exception {
+    Assert.assertTrue(ConnectorTestUtils.deleteAllFiles(baseDirectory));
+    super.tearDown();
+  }
+
+  /**
+   * Test method for 
+   * {@link com.google.enterprise.connector.instantiator.TypeMap
+   * #getTypeInfo(java.lang.String)}.
+   */
+  public final void testGetTypeInfo() {
+    TypeMap typeMap = new TypeMap("classpath*:config/connectorType.xml", 
+        TEST_DIR_NAME);
+    verifyType(typeMap, "TestConnectorA");
+    verifyType(typeMap, "TestConnectorB");
+  }
+
+  private void verifyType(TypeMap typeMap, String typeName) {
+    TypeInfo typeInfo = typeMap.getTypeInfo(typeName);
+    Assert.assertNotNull(typeInfo);
+    Assert.assertEquals(typeName, typeInfo.getConnectorTypeName());
+  }
+
+}
