ad53aa66b416ad376516185f4be843ba9acf4dda
mgronber <mgronber@56db1fc5-3b1b-0410-9cff-4307dac5e09e>
2008-09-22 23:18:39 +0000

    Create P4 cutover branch from /trunk:r954.  Use this branch for merging changes back and forth.

---
+++ branches/dev/mgronber/p4-repo/projects/connector-manager/source/java/com/google/enterprise/connector/persist/ConnectorStateStore.java
@@ -0,0 +1,46 @@
+// Copyright (C) 2006 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.enterprise.connector.persist;
+
+/**
+ * Interface describing the persistence needs of a Traverser
+ */
+public interface ConnectorStateStore {
+
+  /**
+   * Gets the stored state of a named connector.
+   *
+   * @param connectorName
+   * @return the state, or null if no state has been stored for this connector
+   * @throws IllegalStateException if state store is disabled for this connector
+   */
+  public String getConnectorState(String connectorName);
+
+  /**
+   * Sets the stored state of a named connector.
+   *
+   * @param connectorName
+   * @param connectorState String to store
+   * @throws IllegalStateException if state store is disabled for this connector
+   */
+  public void storeConnectorState(String connectorName, String connectorState);
+
+  /**
+   * Remove connector state.  If no such connector exists, do nothing.
+   * @param connectorName name of the connector.
+   */
+  public void removeConnectorState(String connectorName);
+
+}
