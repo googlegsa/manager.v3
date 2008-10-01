0e8588c90a3f4ef3b8082fb2436732b9560bf673
Brett.Michael.Johnson <Brett.Michael.Johnson@56db1fc5-3b1b-0410-9cff-4307dac5e09e>
2008-10-01 22:58:11 +0000

    This is the third attempt at deleting ext2mimetype.txt. Why does GVN seem to have such difficulty with deletes?
    Change Log:
    ----------
    D  projects/connector-manager/etc/ext2mimetype.txt
       - "Out, damn'd spot! out, I say!"
       - "What's done cannot be undone"

---
+++ changes/Brett.Michael.Johnson/fix_issue_62_take3/trunk/projects/connector-manager/source/javatests/com/google/enterprise/connector/jcr/JcrAuthenticationManagerTest.java
@@ -0,0 +1,46 @@
+package com.google.enterprise.connector.jcr;
+
+import com.google.enterprise.connector.manager.UserPassIdentity;
+import com.google.enterprise.connector.mock.MockRepository;
+import com.google.enterprise.connector.mock.MockRepositoryEventList;
+import com.google.enterprise.connector.mock.jcr.MockJcrRepository;
+import com.google.enterprise.connector.spi.AuthenticationManager;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+import javax.jcr.Credentials;
+import javax.jcr.LoginException;
+import javax.jcr.RepositoryException;
+import javax.jcr.Session;
+import javax.jcr.SimpleCredentials;
+
+public class JcrAuthenticationManagerTest extends TestCase {
+
+  public final void testAuthenticate() throws LoginException,
+      RepositoryException, com.google.enterprise.connector.spi.RepositoryLoginException,
+      com.google.enterprise.connector.spi.RepositoryException {
+    MockRepositoryEventList mrel =
+        new MockRepositoryEventList("MockRepositoryEventLog2.txt");
+    MockRepository r = new MockRepository(mrel);
+    MockJcrRepository repo = new MockJcrRepository(r);
+    Credentials creds = new SimpleCredentials("admin", "admin".toCharArray());
+    Session session = repo.login(creds);
+    AuthenticationManager authenticationManager =
+        new JcrAuthenticationManager(session);
+
+    Assert.assertFalse(authenticationManager.authenticate(new UserPassIdentity("jimbo","jimbo")).isValid());
+    Assert.assertFalse(authenticationManager.authenticate(new UserPassIdentity("admin","admin1")).isValid());
+    Assert.assertFalse(authenticationManager.authenticate(new UserPassIdentity("joe","password")).isValid());
+    Assert.assertFalse(authenticationManager.authenticate(new UserPassIdentity("jimbo",null)).isValid());
+
+    // in this repository, the superuser account is open with any password
+    Assert.assertTrue(authenticationManager.authenticate(new UserPassIdentity(null,"jimbo")).isValid());
+    Assert.assertTrue(authenticationManager.authenticate(new UserPassIdentity(null,null)).isValid());
+
+    Assert.assertTrue(authenticationManager.authenticate(new UserPassIdentity("admin","admin")).isValid());
+    Assert.assertTrue(authenticationManager.authenticate(new UserPassIdentity("joe","joe")).isValid());
+    Assert.assertTrue(authenticationManager.authenticate(new UserPassIdentity("mary","mary")).isValid());
+  }
+
+}
