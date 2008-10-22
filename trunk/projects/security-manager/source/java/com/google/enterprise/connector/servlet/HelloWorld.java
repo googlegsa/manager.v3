9349f6b44a6bd1c2de3a305be4690f14b89b3a21
donald.ziff <donald.ziff@56db1fc5-3b1b-0410-9cff-4307dac5e09e>
2008-10-22 05:52:29 +0000

    top-level directories and build files for the security-manager

---
+++ trunk/projects/security-manager/source/java/com/google/enterprise/connector/servlet/HelloWorld.java
@@ -0,0 +1,74 @@
+// Copyright (C) 2008 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.enterprise.connector.servlet;
+
+
+import com.google.common.base.Preconditions;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.NavigableSet;
+import java.util.TreeSet;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Simple "Hello world" servlet.
+ *
+ */
+public class HelloWorld extends HttpServlet
+{
+	private static final long serialVersionUID = 1L;
+	
+	private NavigableSet<String> n;
+
+	/**
+     * Generates a "Hello world" response.
+     * @param req 
+     * @param res 
+     * @throws IOException 
+     * 
+     */
+    @Override
+    protected void doGet(HttpServletRequest req,
+                         HttpServletResponse res)
+              throws IOException
+    {
+      Preconditions.checkNotNull(req);
+      res.setContentType("text/html");
+      PrintWriter out = res.getWriter();
+      out.println("<HTML><HEAD><TITLE>Hello World</TITLE>"+
+                  "</HEAD><BODY>Hello World!</BODY></HTML>");
+      out.close();
+      // this is only here to prove that we're using java 1.6
+      n = new TreeSet<String>();
+      if (n == null) {
+    	  return;
+      }
+    }
+    
+    /**
+     * Returns servlet info.
+     * @return informational message
+     * 
+     */
+    @Override
+    public String getServletInfo()
+    {
+      return "Hello World";
+    }
+}
