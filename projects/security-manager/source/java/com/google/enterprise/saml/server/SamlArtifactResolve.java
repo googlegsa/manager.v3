e7ef6bfa6111c7e43daa997027003f378214fd20
tanthalas <tanthalas@gmail.com>
2008-10-28 23:40:26 +0000

    Modify SamlArtifactResolve to use OpenSamlUtil.

--- projects/security-manager/source/java/com/google/enterprise/saml/server/SamlArtifactResolve.java
+++ projects/security-manager/source/java/com/google/enterprise/saml/server/SamlArtifactResolve.java
@@ -14,42 +14,32 @@
 
 package com.google.enterprise.saml.server;
 
-import org.opensaml.DefaultBootstrap;
+import com.google.enterprise.saml.common.OpenSamlUtil;
+
 import org.opensaml.Configuration;
-import org.opensaml.common.SAMLObjectBuilder;
 import org.opensaml.common.SAMLObject;
-import org.opensaml.common.SAMLVersion;
-import org.opensaml.xml.ConfigurationException;
-import org.opensaml.xml.XMLObjectBuilderFactory;
-import org.opensaml.xml.io.MarshallerFactory;
-import org.opensaml.xml.io.Marshaller;
 import org.opensaml.saml2.core.ArtifactResponse;
-import org.opensaml.saml2.core.Response;
-import org.opensaml.saml2.core.Status;
-import org.opensaml.saml2.core.StatusCode;
 import org.opensaml.saml2.core.Assertion;
-import org.opensaml.saml2.core.Subject;
-import org.opensaml.saml2.core.NameID;
-import org.opensaml.saml2.core.AuthnStatement;
-import org.opensaml.saml2.core.Issuer;
 import org.opensaml.saml2.core.AuthnContext;
-import org.opensaml.saml2.core.AuthnContextClassRef;
+import org.opensaml.saml2.core.Response;
+import org.opensaml.saml2.core.StatusCode;
+import org.opensaml.xml.io.Marshaller;
+import org.opensaml.xml.io.MarshallerFactory;
 import org.w3c.dom.Element;
-import org.joda.time.DateTime;
 
 import java.io.IOException;
-import java.util.logging.Logger;
-import java.util.logging.Level;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.ServletException;
-import javax.xml.soap.SOAPMessage;
-import javax.xml.soap.SOAPException;
 import javax.xml.soap.MessageFactory;
+import javax.xml.soap.SOAPException;
+import javax.xml.soap.SOAPMessage;
 
 /**
  * Handler for SAML ArtifactResolve requests.
@@ -60,17 +50,6 @@ public class SamlArtifactResolve extends HttpServlet {
       Logger.getLogger(SamlArtifactResolve.class.getName());
 
   private String hostname;
-  private SAMLObjectBuilder<ArtifactResponse> artifactResponseBuilder;
-  private SAMLObjectBuilder<Response> samlResponseBuilder;
-  private SAMLObjectBuilder<Issuer> issuerBuilder;
-  private SAMLObjectBuilder<StatusCode> statusCodeBuilder;
-  private SAMLObjectBuilder<Status> samlStatusBuilder;
-  private SAMLObjectBuilder<Assertion> samlAssertionBuilder;
-  private SAMLObjectBuilder<Subject> samlSubjectBuilder;
-  private SAMLObjectBuilder<NameID> nameIDBuilder;
-  private SAMLObjectBuilder<AuthnStatement> authnStmtBuilder;
-  private SAMLObjectBuilder<AuthnContext> authnContextBuilder;
-  private SAMLObjectBuilder<AuthnContextClassRef> authnContextClassRefBuilder;
 
   /**
    * Public constructor. Initializes the necessary state for later processing.
@@ -81,11 +60,8 @@ public class SamlArtifactResolve extends HttpServlet {
 
     try {
       resolveHostname();
-      initBuilders();
     } catch (UnknownHostException e) {
       LOGGER.severe("Could not resolve local hostname.");
-    } catch (ConfigurationException e) {
-      LOGGER.severe("Could not initialize builders!\n");
     }
   }
 
@@ -137,17 +113,11 @@ public class SamlArtifactResolve extends HttpServlet {
    * @param authnContext a context string as defined in the AuthnContext class
    * @return an ArtifactResponse object that contains the specified information
    */
-  private ArtifactResponse buildArtifactResponse(String statusCode, String issuer,
+   private ArtifactResponse buildArtifactResponse(String statusCode, String issuer,
                                                  String nameId,
                                                  String authnContext) {
-    ArtifactResponse artifactResp = artifactResponseBuilder.buildObject();
-    artifactResp.setIssueInstant(new DateTime());
-    artifactResp.setID("somerandomstring");
-    artifactResp.setInResponseTo("alsorandomlooking");
-    artifactResp.setIssuer(buildIssuer(issuer));
-    artifactResp.setStatus(buildStatus(statusCode));
-    artifactResp.setMessage(
-        buildResponse(statusCode, issuer, nameId, authnContext));
+    ArtifactResponse artifactResp = OpenSamlUtil.makeArtifactResponse(null, OpenSamlUtil.makeStatus(statusCode), buildResponse(statusCode, issuer, nameId, authnContext));
+    artifactResp.setIssuer(OpenSamlUtil.makeIssuer(issuer));
     return artifactResp;
   }
 
@@ -156,11 +126,7 @@ public class SamlArtifactResolve extends HttpServlet {
    */
   private Response buildResponse(String statusCode, String issuer,
                                      String nameId, String authnContext) {
-    Response resp = samlResponseBuilder.buildObject();
-    resp.setIssueInstant(new DateTime());
-    resp.setID("blahbalh");
-    resp.setVersion(SAMLVersion.VERSION_20);
-    resp.setStatus(buildStatus(statusCode));
+    Response resp = OpenSamlUtil.makeResponse(null, OpenSamlUtil.makeStatus(statusCode));
     resp.getAssertions().add(buildAssertion(issuer, nameId, authnContext));
     return resp;
   }
@@ -170,63 +136,9 @@ public class SamlArtifactResolve extends HttpServlet {
    */
   private Assertion buildAssertion(String issuer, String nameId,
                                    String authnContext) {
-    Assertion assertion = samlAssertionBuilder.buildObject();
-    assertion.setIssueInstant(new DateTime());
-    assertion.setID("blahbalh2");
-    assertion.setIssuer(buildIssuer(issuer));
-    assertion.setSubject(buildSubject(nameId));
-    assertion.getAuthnStatements().add(buildAuthnStatement(authnContext));
+    Assertion assertion = OpenSamlUtil.makeAssertion(OpenSamlUtil.makeIssuer(issuer), OpenSamlUtil.makeSubject(nameId));
+    assertion.getAuthnStatements().add(OpenSamlUtil.makeAuthnStatement(authnContext));
     return assertion;
-  }
-
-  /**
-   * Builds an AuthnStatement object out of the specified AuthnContext.
-   * @param context
-   */
-  private AuthnStatement buildAuthnStatement(String context) {
-    AuthnStatement authnStmt = authnStmtBuilder.buildObject();
-    AuthnContext authnCxt = authnContextBuilder.buildObject();
-    AuthnContextClassRef authnContextClassRef =
-        authnContextClassRefBuilder.buildObject();
-    authnContextClassRef.setAuthnContextClassRef(context);
-    authnCxt.setAuthnContextClassRef(authnContextClassRef);
-    authnStmt.setAuthnContext(authnCxt);
-    authnStmt.setAuthnInstant(new DateTime());
-    return authnStmt;
-  }
-
-  /**
-   * Builds a Status object out of a given StatusCode string.
-   * @param statusCode a status string as defined in the StatusCode class
-   */
-  private Status buildStatus(String statusCode) {
-    StatusCode sc = statusCodeBuilder.buildObject();
-    sc.setValue(statusCode);
-    Status status = samlStatusBuilder.buildObject();
-    status.setStatusCode(sc);
-    return status;
-  }
-
-  /**
-   * Builds an Issuer object out of a given Issuer string.
-   * @param issuerString a string representing the issuer of a SAMLObject
-   */
-  private Issuer buildIssuer(String issuerString) {
-    Issuer issuer = issuerBuilder.buildObject();
-    issuer.setValue(issuerString);
-    return issuer;
-  }
-
-  /**
-   * Builds a Subject object out of a given name.
-   * @param subjectName the name of the subject to be created
-   */
-  private Subject buildSubject(String subjectName) {
-    Subject samlSubject = samlSubjectBuilder.buildObject();
-    NameID nameId = nameIDBuilder.buildObject();
-    nameId.setValue(subjectName);
-    samlSubject.setNameID(nameId);
-    return samlSubject;
   }
 
   // -----------------------------------
@@ -240,49 +152,6 @@ public class SamlArtifactResolve extends HttpServlet {
   private void resolveHostname() throws UnknownHostException {
     InetAddress addr = InetAddress.getLocalHost();
     hostname = addr.getHostName();
-  }
-
-  /**
-   * Initialize OpenSAML object builder factories.
-   *
-   * @throws ConfigurationException if builders could not be made
-   */
-  private void initBuilders() throws ConfigurationException {
-    DefaultBootstrap.bootstrap();
-    XMLObjectBuilderFactory builderFactory = Configuration.getBuilderFactory();
-    artifactResponseBuilder
-        = (SAMLObjectBuilder<ArtifactResponse>)
-        builderFactory.getBuilder(ArtifactResponse.DEFAULT_ELEMENT_NAME);
-    samlResponseBuilder
-        = (SAMLObjectBuilder<Response>)
-        builderFactory.getBuilder(Response.DEFAULT_ELEMENT_NAME);
-    issuerBuilder =
-        (SAMLObjectBuilder<Issuer>) builderFactory
-            .getBuilder(Issuer.DEFAULT_ELEMENT_NAME);
-    statusCodeBuilder =
-        (SAMLObjectBuilder<StatusCode>) builderFactory
-            .getBuilder(StatusCode.DEFAULT_ELEMENT_NAME);
-    samlStatusBuilder
-        = (SAMLObjectBuilder<Status>)
-        builderFactory.getBuilder(Status.DEFAULT_ELEMENT_NAME);
-    samlAssertionBuilder =
-        (SAMLObjectBuilder<Assertion>) builderFactory
-            .getBuilder(Assertion.DEFAULT_ELEMENT_NAME);
-    samlSubjectBuilder =
-        (SAMLObjectBuilder<Subject>) builderFactory
-            .getBuilder(Subject.DEFAULT_ELEMENT_NAME);
-    nameIDBuilder =
-        (SAMLObjectBuilder<NameID>) builderFactory
-            .getBuilder(NameID.DEFAULT_ELEMENT_NAME);
-    authnStmtBuilder =
-        (SAMLObjectBuilder<AuthnStatement>) builderFactory.getBuilder(
-            AuthnStatement.DEFAULT_ELEMENT_NAME);
-    authnContextBuilder =
-        (SAMLObjectBuilder<AuthnContext>) builderFactory.getBuilder(
-            AuthnContext.DEFAULT_ELEMENT_NAME);
-    authnContextClassRefBuilder =
-        (SAMLObjectBuilder<AuthnContextClassRef>) builderFactory.getBuilder(
-            AuthnContextClassRef.DEFAULT_ELEMENT_NAME);
   }
 
   /**
