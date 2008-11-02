fc741f6f97e5f4600244edf9b28789b8802aa499
Max Ziff <donald.ziff@gmail.com>
2008-11-02 01:59:17 +0000

    move to a specific sub-package

--- projects/security-manager/source/javatests/com/google/enterprise/security/connectors/simplecookie/SimpleCookieIdentityConnectorTest.java
+++ projects/security-manager/source/javatests/com/google/enterprise/security/connectors/simplecookie/SimpleCookieIdentityConnectorTest.java
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package com.google.enterprise.security.connectors;
+package com.google.enterprise.security.connectors.simplecookie;
 
 import com.google.enterprise.connector.manager.UserPassIdentity;
 import com.google.enterprise.connector.spi.AuthenticationIdentity;
