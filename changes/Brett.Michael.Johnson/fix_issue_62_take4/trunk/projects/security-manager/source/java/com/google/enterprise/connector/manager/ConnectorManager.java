a389b4c1ae6037d992279186e73c63ddb5525a57
Brett.Michael.Johnson <Brett.Michael.Johnson@56db1fc5-3b1b-0410-9cff-4307dac5e09e>
2008-11-04 00:16:41 +0000

    This is a minor follow-on modification to the changes for Connector Manager Issue 62 - TraversalContext
    After discussing the documentation implications with George,
    I decided to make a couple of minor adjustments to the 
    mime type code:
    
    - Restore the default value of unknownMimeTypeSupportLevel to 1.
      I had changed it from 1 to 2 early in development for a reason
      that eventually didn't pan out.  Currently there is no behavioral
      difference between the two values.  In reality, only values of
      0 or 1 make sense for unknownMimeTypeSupportLevel.
    
    - Rank content types sans subtypes below content types with subtypes.
      For instance, supportLevel("text") < supportLevel("text/x-foo").
    
    
    Change Log:
    M  projects/connector-manager/etc/applicationContext.xml
       - Restore default unknownMimeTypeSupportLevel value to 1
         (This doesn't really change any behaviour.)
    
    M  projects/connector-manager/source/java/com/google/enterprise/connector/traversal/MimeTypeMap.java
       - Restore default unknownMimeTypeSupportLevel value to 1
       - Rank content types sans subtypes below any content types
         with subtypes for a given support level.

---
+++ changes/Brett.Michael.Johnson/fix_issue_62_take4/trunk/projects/security-manager/source/java/com/google/enterprise/connector/manager/ConnectorManager.java
@@ -0,0 +1,82 @@
+// Copyright (C) 2008 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.enterprise.connector.manager;
+
+import com.google.enterprise.connector.instantiator.InstantiatorException;
+import com.google.enterprise.connector.persist.ConnectorNotFoundException;
+import com.google.enterprise.connector.spi.AuthenticationIdentity;
+import com.google.enterprise.connector.spi.AuthenticationManager;
+import com.google.enterprise.saml.server.BackEnd;
+
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * This class is temporary -- while the connector manager and the security
+ * manager are different builds. This gives the security manager a connector
+ * manager with an implementation for the overloading of authenticate() that
+ * includes a security context.
+ * 
+ * This functionality will eventually be merged with the connector manager's
+ * {@link ProductionManager}.
+ */
+public class ConnectorManager extends ProductionManager implements Manager {
+
+  private static final Logger LOGGER = Logger.getLogger(ConnectorManager.class.getName());
+
+  private BackEnd backEnd;
+
+  public BackEnd getBackEnd() {
+    return backEnd;
+  }
+
+  public void setBackEnd(BackEnd backEnd) {
+    this.backEnd = backEnd;
+  }
+
+  @Override
+  public boolean authenticate(String connectorName, String username, String password) {
+    return authenticate(connectorName, username, password, null);
+  }
+
+  @SuppressWarnings("unchecked")
+  /**
+   * This method will become part of the {@link Manager} interface
+   */
+  public boolean authenticate(String connectorName, String username, String password,
+      Map securityContext) {
+    boolean result = false;
+    AuthenticationIdentity identity = new UserPassIdentity(username, password);
+    AuthenticationManager authnManager = null;
+    try {
+      authnManager = instantiator.getAuthenticationManager(connectorName);
+    } catch (ConnectorNotFoundException e) {
+      LOGGER.log(Level.WARNING, "Connector " + connectorName + " Not Found: ", e);
+    } catch (InstantiatorException e) {
+      LOGGER.log(Level.WARNING, "Instantiator: ", e);
+    }
+
+    // Some connectors don't implement the AuthenticationManager interface, or
+    // there may been an instantiation problem.
+    if (authnManager == null) {
+      return false;
+    }
+
+    AuthnCaller authnCaller = new AuthnCaller(authnManager, identity, securityContext);
+
+    return authnCaller.authenticate();
+  }
+}
