22fb3bcd20bb31eaa2fdc55a3c13eb9d39b937a8
Max Ziff <donald.ziff@gmail.com>
2008-11-14 23:23:24 +0000

    http://codereview.appspot.com/8848

---
+++ projects/security-manager/source/regressions/com/google/enterprise/security/connectors/BasicRegressionTest.java
@@ -0,0 +1,17 @@
+package com.google.enterprise.security.connectors;
+
+import com.meterware.httpunit.GetMethodWebRequest;
+import com.meterware.httpunit.WebConversation;
+import com.meterware.httpunit.WebRequest;
+import com.meterware.httpunit.WebResponse;
+
+import junit.framework.TestCase;
+
+public class BasicRegressionTest extends TestCase {
+
+  public void testSamlartifact() throws Exception {
+    WebConversation wc = new WebConversation();
+    WebRequest request = new GetMethodWebRequest("http://localhost:8973/security-manager/samlartifact");
+    WebResponse response = wc.getResponse(request);
+  }
+}
