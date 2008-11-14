46fcba7942ee829cbae97c9600a7cd9783cfad19
Chris Hanson <cph@google.com>
2008-11-14 05:51:34 +0000

    Fix compiler warnings.

--- projects/security-manager/source/java/com/google/enterprise/saml/server/ArtifactStore.java
+++ projects/security-manager/source/java/com/google/enterprise/saml/server/ArtifactStore.java
@@ -1,19 +1,6 @@
 package com.google.enterprise.saml.server;
 
-import com.google.enterprise.connector.manager.Manager;
-import com.google.enterprise.sessionmanager.SessionManagerInterface;
-import com.google.enterprise.saml.common.GsaConstants;
-
-import java.util.logging.Logger;
-import java.util.List;
 import java.util.HashMap;
-import java.net.URLEncoder;
-import java.io.UnsupportedEncodingException;
-
-import org.opensaml.saml2.core.ArtifactResponse;
-import org.opensaml.saml2.core.ArtifactResolve;
-import org.opensaml.saml2.core.Response;
-import org.opensaml.saml2.core.AuthzDecisionQuery;
 
 /**
  * Keep track of the artifacts we have issued.
@@ -21,9 +8,6 @@ import org.opensaml.saml2.core.AuthzDecisionQuery;
  */
 public class ArtifactStore {
   private HashMap<String, String> artMap;
-  
-  private static final Logger LOGGER =
-      Logger.getLogger(ArtifactStore.class.getName());
 
   public ArtifactStore() {
     artMap = new HashMap<String, String>();
