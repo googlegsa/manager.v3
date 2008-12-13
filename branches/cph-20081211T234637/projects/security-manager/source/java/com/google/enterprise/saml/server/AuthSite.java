5c273f303f95da5e6c7c6e7ea216dcea3906096f
cph.mit <cph.mit@56db1fc5-3b1b-0410-9cff-4307dac5e09e>
2008-12-13 01:50:39 +0000

    Move AuthSite configuration setup to common location.

--- branches/cph-20081211T234637/projects/security-manager/source/java/com/google/enterprise/saml/server/AuthSite.java
+++ branches/cph-20081211T234637/projects/security-manager/source/java/com/google/enterprise/saml/server/AuthSite.java
@@ -2,6 +2,11 @@ package com.google.enterprise.saml.server;
 
 import com.google.enterprise.saml.common.GsaConstants.AuthNMechanism;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Keep track of the Auth sites we manage.
  *
@@ -18,6 +23,25 @@ public class AuthSite {
     this.method = method;
     if (uri != null && uri.length() > 0)
       this.loginUri = uri;
+  }
+
+  public static List<AuthSite> getSites(String configFile) throws IOException {
+    CSVReader reader = new CSVReader(new FileReader(configFile));
+    List<AuthSite> sites = new ArrayList<AuthSite>();
+    String[] nextLine;
+   
+    // Each line is FQDN,realm,auth_method,optional_sample_login_url
+    // If sample login is missing, we deduce as FQDN + realm
+    while ((nextLine = reader.readNext()) != null) {
+      AuthNMechanism method = null;
+      if (nextLine[2].equals(AuthNMechanism.BASIC_AUTH.toString()))
+        method = AuthNMechanism.BASIC_AUTH;
+      if (nextLine[2].equals(AuthNMechanism.FORMS_AUTH.toString()))
+        method = AuthNMechanism.FORMS_AUTH;
+      sites.add(new AuthSite(nextLine[0], nextLine[1], method,
+                             nextLine.length > 3 ? nextLine[3] : null));
+    }
+    return sites;
   }
   
   public String getHostname() {
