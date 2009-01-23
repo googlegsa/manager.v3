c27acb6c64fa8a2a7539a99c2d988f941f6d8363
Chris Hanson <cph@google.com>
2009-01-23 08:10:54 +0000

    Don't call generatePostContent() unless there are parameters to encode.

--- projects/security-manager/source/javatests/com/google/enterprise/common/MockHttpClient.java
+++ projects/security-manager/source/javatests/com/google/enterprise/common/MockHttpClient.java
@@ -1,4 +1,4 @@
-// Copyright (C) 2008 Google Inc.
+// Copyright (C) 2008, 2009 Google Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -94,7 +94,8 @@ public class MockHttpClient implements HttpClientInterface {
     }
 
     public int exchange() throws IOException {
-      if (request.getMethod().equalsIgnoreCase("POST")) {
+      if (request.getMethod().equalsIgnoreCase("POST")
+          && request.getParameterNames().hasMoreElements()) {
         generatePostContent(request);
       }
       MockHttpServletResponse response = exchange1(request);
