ff92f3d030b38c81fec0d2131f2aa94cbc6e6c85
Brett.Michael.Johnson <Brett.Michael.Johnson@56db1fc5-3b1b-0410-9cff-4307dac5e09e>
2009-03-31 20:55:46 +0000

    This set of changes addresses Connector Manager Issue 133 - InstanceMap.createNewConnector() should delete partial connector directory if instantiation fails.
    InstanceMap.createNewConnector() now deletes the directory it
    created if the instantiation fails.  This required refactoring
    InstanceMap.removeConnector() to extract the directory removal code.
    
    As it stands, if the customer modified the connectorInstance.xml
    file during the failed configuration, the file will not be removed.
    But that is probably a good thing.
    
    We also detect that no properties file exists and stop instantiation there,
    throwing an InstanceInfoException("Connector configuration not found...").
    This is much more informative that the Spring PropertyPlacholderException
    that was thrown previously.
    
    Finally, if on startup, a connector does not have a schedule and
    none was found in a legacy store, I create a disabled schedule and
    log a warning.  This is better than spamming the log file with
    "Connector Schedule not found" messages once a second.
    
    
    Change Log:
    ----------
    M  projects/connector-manager/source/java/com/google/enterprise/connector/instantiator/InstanceInfo.java
       - Throw exception if connector configuration not found.
       - Create a disabled schedule for connectors that are missing one.
    
    M  projects/connector-manager/source/java/com/google/enterprise/connector/instantiator/InstanceMap.java
       - createNewConnector() deletes the directory it created
         if the instantiation fails, cleaning up detritus.

---
+++ changes/Brett.Michael.Johnson/fix_issue_133/trunk/projects/connector-manager/source/java/com/google/enterprise/connector/pusher/NullFeedConnection.java
@@ -0,0 +1,66 @@
+// Copyright 2008 Google Inc.  All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.enterprise.connector.pusher;
+
+import com.google.enterprise.connector.spi.RepositoryException;
+import com.google.enterprise.connector.spi.RepositoryDocumentException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Null Feed takes the feed data and drops it on the floor.
+ * This can be used to test Connector feeds without actually feeding
+ * the data to the Google Search Appliance.  Turning on teedFeedFile
+ * logging can help determine if the feed contents are correct.
+ * Leaving it off can help determine if Traversal performance is
+ * acceptable.
+ */
+public class NullFeedConnection implements FeedConnection {
+
+  private static final Logger LOGGER =
+      Logger.getLogger(NullFeedConnection.class.getName());
+
+  public NullFeedConnection() {
+    LOGGER.info("Using Null FeedConnection.  Fed Content will be discarded.");
+  }
+
+  public String sendData(String dataSource, FeedData feedData)
+      throws RepositoryException {
+    try {
+      InputStream data = ((GsaFeedData)feedData).getData();
+      int bytesRead = 0;
+      int val = 0;
+      byte[] bytebuf = new byte[32768];
+      // Consume the input stream, but discard the contents.
+      while ((val = data.read(bytebuf)) != -1) {
+        bytesRead += val;
+      }
+      LOGGER.fine("Null FeedConnection discarded " + bytesRead + " bytes.");
+    } catch (IOException ioe) {
+      LOGGER.log(Level.SEVERE,
+                 "IOException while reading: skipping", ioe);
+      Throwable t = ioe.getCause();
+      if (t != null && (t instanceof RepositoryException)) {
+        throw (RepositoryException) t;
+      } else {
+        throw new RepositoryDocumentException(ioe);
+      }
+    }
+    return GsaFeedConnection.SUCCESS_RESPONSE;
+  }
+}
