6f5cb15ce9c195917b1796b17934439a8e6b6669
mgronber <mgronber@56db1fc5-3b1b-0410-9cff-4307dac5e09e>
2009-04-06 19:16:35 +0000

    Added back the call to removeNestedMarkers() for those cases where the formSnippet does not contain any sensitive data and is not filtered but simply returned from the call to filterSensitiveData().  In those cases, if the formSnippet contains script code it many also contain CDATA markers which will become nested when the from is wrapped again.
    Tests Passed:
    
      - All Unit tests
      - Tested on GSA with form with script and no sensitive data.
    
    Change Log:
    
      M source/java/com/google/enterprise/connector/servlet/ConnectorManagerGetServlet.java
        - Put back the call to filter nested markers to cover those cases where the
          form does not contain any sensitive data but may contain CDATA markers for
          scripts.

---
+++ changes/mgronber/fix-issue-139/trunk/projects/connector-manager/source/java/com/google/enterprise/connector/servlet/GetConnectorStatus.java
@@ -0,0 +1,100 @@
+// Copyright (C) 2006-2008 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
+package com.google.enterprise.connector.servlet;
+
+import com.google.enterprise.connector.manager.ConnectorStatus;
+import com.google.enterprise.connector.manager.Manager;
+
+import java.io.PrintWriter;
+import java.util.logging.Logger;
+
+
+/**
+ * Admin servlet to get the connector status for a given connector.
+ *
+ */
+public class GetConnectorStatus extends ConnectorManagerGetServlet {
+  private static final Logger LOGGER =
+      Logger.getLogger(GetConnectorStatus.class.getName());
+
+  /*
+   * (non-Javadoc)
+   *
+   * @see com.google.enterprise.connector.servlet.ConnectorManagerGetServlet#
+   *      processDoGet(java.lang.String, java.lang.String,
+   *      com.google.enterprise.connector.manager.Manager,
+   *      java.io.PrintWriter)
+   */
+  protected void processDoGet(String connectorName, String lang,
+      Manager manager, PrintWriter out) {
+    handleDoGet(connectorName, manager, out);
+  }
+
+  /**
+   * Handler for doGet in order to do unit tests. Returns the connector
+   * status.
+   *
+   * @param connectorName
+   * @param manager
+   * @param out PrintWriter where the response is written
+   */
+  public static void handleDoGet(String connectorName, Manager manager,
+      PrintWriter out) {
+    ConnectorMessageCode status = new ConnectorMessageCode();
+    ConnectorStatus connectorStatus = manager.getConnectorStatus(connectorName);
+    if (connectorStatus == null) {
+      status =
+          new ConnectorMessageCode(
+              ConnectorMessageCode.RESPONSE_NULL_CONNECTOR_STATUS,
+              connectorName);
+      LOGGER
+          .warning("Connector manager returns no status for " + connectorName);
+    }
+
+    ServletUtil.writeRootTag(out, false);
+    ServletUtil.writeMessageCode(out, status);
+    if (connectorStatus != null) {
+      ServletUtil.writeXMLTag(out, 1, ServletUtil.XMLTAG_CONNECTOR_STATUS,
+          false);
+      ServletUtil.writeXMLElement(out, 2, ServletUtil.XMLTAG_CONNECTOR_NAME,
+          connectorStatus.getName());
+      ServletUtil.writeXMLElement(out, 2, ServletUtil.XMLTAG_CONNECTOR_TYPE,
+          connectorStatus.getType());
+      ServletUtil.writeXMLElement(out, 2, ServletUtil.XMLTAG_STATUS,
+          Integer.toString(connectorStatus.getStatus()));
+
+      String schedule = connectorStatus.getSchedule();
+      if (schedule == null) {
+        ServletUtil.writeEmptyXMLElement(out, 2,
+            ServletUtil.XMLTAG_CONNECTOR_SCHEDULES);
+      } else {
+        StringBuffer buffer = new StringBuffer();
+        ServletUtil.writeXMLTagWithAttrs(buffer, 2,
+            ServletUtil.XMLTAG_CONNECTOR_SCHEDULES,
+            ServletUtil.ATTRIBUTE_VERSION + "3" + ServletUtil.QUOTE,
+            false);
+        buffer.append(schedule);
+        ServletUtil.writeXMLTag(buffer, 0,
+            ServletUtil.XMLTAG_CONNECTOR_SCHEDULES, true);
+        out.println(buffer.toString());
+      }
+      ServletUtil.writeXMLTag(out, 1, ServletUtil.XMLTAG_CONNECTOR_STATUS,
+          true);
+    }
+    ServletUtil.writeRootTag(out, true);
+  }
+
+}
