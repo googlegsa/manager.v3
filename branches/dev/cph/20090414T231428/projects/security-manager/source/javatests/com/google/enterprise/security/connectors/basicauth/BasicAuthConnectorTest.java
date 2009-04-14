aacb1bbd4056bdb0246e45f42ed3cd6c84f632f8
cph.mit <cph.mit@56db1fc5-3b1b-0410-9cff-4307dac5e09e>
2009-04-14 23:14:36 +0000

    New working branch.

---
+++ branches/dev/cph/20090414T231428/projects/security-manager/source/javatests/com/google/enterprise/security/connectors/basicauth/BasicAuthConnectorTest.java
@@ -0,0 +1,67 @@
+// Copyright 2008 Google Inc.  All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.enterprise.security.connectors.basicauth;
+
+import com.google.enterprise.common.HttpClientInterface;
+import com.google.enterprise.common.MockHttpClient;
+import com.google.enterprise.common.MockHttpTransport;
+import com.google.enterprise.common.SecurityManagerTestCase;
+import com.google.enterprise.connector.spi.AuthenticationIdentity;
+import com.google.enterprise.connector.spi.AuthenticationResponse;
+import com.google.enterprise.connector.spi.RepositoryException;
+import com.google.enterprise.saml.common.GsaConstants.AuthNMechanism;
+import com.google.enterprise.security.identity.AuthnDomain;
+import com.google.enterprise.security.identity.AuthnDomainGroup;
+import com.google.enterprise.security.identity.CredentialsGroup;
+import com.google.enterprise.security.identity.DomainCredentials;
+
+/*
+ * Tests for the {@link BasicAuthConnector} class.
+ * Maybe should use a mock Idp...
+ */
+public class BasicAuthConnectorTest extends SecurityManagerTestCase {
+
+  private AuthnDomainGroup adg;
+  private AuthnDomain domain;
+  private CredentialsGroup cg;
+  private HttpClientInterface httpClient;
+
+  @Override
+  public void setUp() throws Exception {
+    super.setUp();
+    adg = new AuthnDomainGroup("ADG1");
+    domain = new AuthnDomain(
+        "BasicDomain", AuthNMechanism.BASIC_AUTH,
+        "http://localhost:8973/basic/", adg);
+    cg = new CredentialsGroup(adg);
+    MockHttpTransport transport = new MockHttpTransport();
+    transport.registerServlet(domain.getLoginUrl(), new MockBasicAuthServer.Server1());
+    httpClient = new MockHttpClient(transport);
+  }
+
+  public void testHttpAuthenticate() throws RepositoryException {
+    assertTrue(tryCredentials("joe", "plumber").isValid());
+    assertFalse(tryCredentials("joe", "biden").isValid());
+  }
+
+  private AuthenticationResponse tryCredentials(String username, String password)
+      throws RepositoryException {
+    BasicAuthConnector conn = new BasicAuthConnector(httpClient, domain.getLoginUrl());
+    AuthenticationIdentity id = new DomainCredentials(domain, cg);
+    cg.setUsername(username);
+    cg.setPassword(password);
+    return conn.authenticate(id);
+  }
+}
