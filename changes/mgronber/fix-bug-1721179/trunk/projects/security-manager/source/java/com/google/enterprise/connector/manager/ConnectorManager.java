f529dea8e168d0ef1bb68c562a2585d48e41323e
mgronber <mgronber@56db1fc5-3b1b-0410-9cff-4307dac5e09e>
2009-04-16 00:48:10 +0000

    Description:
    Fix for Bug 1721179.  Discovered that when the Test Connectors are used to send 
    binary content over to the GSA the encoding gets mangled and the GSA cannot
    decode the content for indexing.  Test Connectors are configured as a
    JcrConnector.  After detailed investigation, discovered that the
    JcrConnector.getValues() call was just pulling all values as a String.  This was
    causing the InputStream to be read into a String only to be read back into a
    Stream later.  Reading an InputStream into a String causes the original
    character encoding to be mangled.
    
    This fix removes the conversion from Stream to String when getting the values
    for the case where the MockRepositoryProperty is not a multivalue property.
    
    Tests Passed:
    
      - All Unit Tests - under JDK 1.5.
      - Tested WAR with GSA and used modified MockRepositoryEventLog4.txt to send
        both a .doc and .pdf to the GSA using the Test Connector.  Both are indexed
        and decoded fine.
    
    ChangeLog:
    
    M source/javatests/com/google/enterprise/connector/common/Base64FilterInputStreamTest.java:
      - Added a test for encoding the test.doc file using a JcrConnector.
    
    M source/javatests/com/google/enterprise/connector/mock/jcr/MockJcrValue.java:
      - Added support for converting internal STREAM type to JCR BINARY type.
    
    M source/javatests/com/google/enterprise/connector/mock/jcr/MockJcrProperty.java
      - Changed getValues() to return simple wrapped property in the case where
        multivalue is not an issue.  The problem with converting everything to a
        String is Stream encoding gets mangled.

---
+++ changes/mgronber/fix-bug-1721179/trunk/projects/security-manager/source/java/com/google/enterprise/connector/manager/ConnectorManager.java
@@ -0,0 +1,117 @@
+// Copyright (C) 2008, 2009 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.enterprise.connector.manager;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.enterprise.connector.instantiator.InstantiatorException;
+import com.google.enterprise.connector.persist.ConnectorNotFoundException;
+import com.google.enterprise.connector.persist.PersistentStoreException;
+import com.google.enterprise.connector.spi.AuthenticationIdentity;
+import com.google.enterprise.connector.spi.AuthenticationManager;
+import com.google.enterprise.connector.spi.AuthenticationResponse;
+import com.google.enterprise.saml.server.BackEnd;
+
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * This class is temporary -- while the connector manager and the security
+ * manager are different builds. This gives the security manager a connector
+ * manager with an implementation for the overloading of authenticate() that
+ * includes a security context.
+ *
+ * This functionality will eventually be merged with the connector manager's
+ * {@link ProductionManager}.
+ */
+public class ConnectorManager extends ProductionManager {
+
+  private static final Logger LOGGER = Logger.getLogger(ConnectorManager.class.getName());
+
+  private BackEnd backEnd;
+  private boolean securityManagerConnectorsInitialized;
+
+  public ConnectorManager() {
+    super();
+    securityManagerConnectorsInitialized = false;
+  }
+
+  public BackEnd getBackEnd() {
+    return backEnd;
+  }
+
+  public void setBackEnd(BackEnd backEnd) {
+    backEnd.setConnectorManager(this);
+    this.backEnd = backEnd;
+  }
+
+  @Override
+  public boolean authenticate(String connectorName, 
+      AuthenticationIdentity identity) {
+    throw new UnsupportedOperationException();
+  }
+
+  /**
+   * This method will become part of the {@link Manager} interface
+   */
+  public AuthenticationResponse authenticate(String connectorName, AuthenticationIdentity id,
+      SecAuthnContext securityContext) {
+    AuthenticationManager authnManager = null;
+    try {
+      authnManager = instantiator.getAuthenticationManager(connectorName);
+    } catch (ConnectorNotFoundException e) {
+      LOGGER.log(Level.WARNING, "Connector " + connectorName + " Not Found: ", e);
+    } catch (InstantiatorException e) {
+      LOGGER.log(Level.WARNING, "Instantiator: ", e);
+    }
+
+    // Some connectors don't implement the AuthenticationManager interface, or
+    // there may been an instantiation problem.
+    if (authnManager == null) {
+      return null;
+    }
+
+    AuthnCaller authnCaller = new AuthnCaller(authnManager, id, securityContext);
+
+    return authnCaller.authenticate();
+  }
+
+  @Override
+  public List<ConnectorStatus> getConnectorStatuses() {
+    if (!securityManagerConnectorsInitialized) {
+      try {
+        this.setConnectorConfig("BasicAuth", "BasicAuthConnector",
+                                ImmutableMap.of("ServerUrl", "foo"),  // dummy parameter
+                                "en", false);
+        this.setConnectorConfig("FormAuth", "FormAuthConnector",
+                                ImmutableMap.of("CookieName", "bar"),  // dummy parameter
+                                "en", false);
+        this.setConnectorConfig("ConnAuth", "ConnAuthConnector",
+                                ImmutableMap.of("SpiVersion", "0"),
+                                "en", false);
+        securityManagerConnectorsInitialized = true;
+      } catch (ConnectorNotFoundException e) {
+        LOGGER.info("ConnectorNotFound: " + e.toString());
+      } catch (InstantiatorException e) {
+        LOGGER.info("Instantiator: " + e.toString());
+      } catch (PersistentStoreException e) {
+        LOGGER.info("PersistentStore: " + e.toString());
+      }
+    }
+    @SuppressWarnings("unchecked")
+    List<ConnectorStatus> result = super.getConnectorStatuses();
+    return result;
+  }
+}
