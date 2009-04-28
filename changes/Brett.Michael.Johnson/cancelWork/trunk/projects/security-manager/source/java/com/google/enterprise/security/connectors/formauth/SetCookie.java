e4a0ac600721a2d429b58b33df5bc63cd4d819c1
Brett.Michael.Johnson <Brett.Michael.Johnson@56db1fc5-3b1b-0410-9cff-4307dac5e09e>
2009-04-28 00:02:32 +0000

    While going over the workings of TraversalScheduler and WorkQueue* with Eric, we noticed a few anomolies with the cancel work improvements that were done under Issue 110. This clears up a couple of them.
    Changed Files:
    --------------
    M projects/connector-manager/source/java/com/google/enterprise/connector/common/WorkQueue.java
      - WorkQueue.cancelWork() now calls WorkQueueItem.cancelWork(),
        even during shutdown. This allows cancel notification for all
        the common cases of abandoning work: 1) system shutdown,
        2) connector deletion, 3) connector reconfigure, 4) traversal hang,
        5) restart traversal.
    
    M projects/connector-manager/source/java/com/google/enterprise/connector/instantiator/SpringInstantiator.java
      - If reconfiguring a connector, call TravesalScheduler.removeConnector().
        This will eventually force pending or in-progress traversal work to be
        abandoned.
    
    M projects/connector-manager/source/javatests/com/google/enterprise/connector/scheduler/TraversalSchedulerTest.java
      - Use CancellableQueryTraverser for testRemoveConnector() as removeConnector()
        will now call Traverser.cancelBatch().

---
+++ changes/Brett.Michael.Johnson/cancelWork/trunk/projects/security-manager/source/java/com/google/enterprise/security/connectors/formauth/SetCookie.java
@@ -0,0 +1,100 @@
+// Copyright 2008 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.enterprise.security.connectors.formauth;
+
+import javax.servlet.http.Cookie;
+/**
+ * Represent the cookie received from Web server via Set-Cookie and
+ * Set-Cookie2 HTTP header. Used with parser in SetCookieParser.java.
+ *
+ */
+public class SetCookie extends Cookie {
+
+  private String expires;
+  private boolean discard;
+  private boolean httpOnly;
+
+  public int[] ports;
+  public String commentURL;
+  public static int[] EMPTY_PORT = new int[1];
+
+  public SetCookie(String name, String value) {
+    super(name, value);
+  }
+
+  public void setExpires(String e) {
+    expires = e;
+  }
+  public String getExpires() {
+    return expires;
+  }
+
+  public void setDiscard(boolean d) {
+    discard = d;
+  }
+  public boolean getDiscard() {
+    return discard;
+  }
+
+  public void setHttpOnly(boolean h) {
+    httpOnly = h;
+  }
+  public boolean getHttpOnly() {
+    return httpOnly;
+  }
+
+  @Override
+  public String toString() {
+    StringBuffer strbuf = new StringBuffer("");
+    try {
+      strbuf.append(getName()).append("=");
+      if ( getValue() != null )
+        strbuf.append(getValue());
+      if ( getComment() != null )
+        strbuf.append("; comment=").append(getComment());
+      if ( getDomain() != null )
+        strbuf.append("; domain=").append(getDomain());
+      if ( getMaxAge() > 0 )
+        strbuf.append("; Max-Age=").append(getMaxAge());
+      if ( getPath() != null )
+        strbuf.append("; path=").append(getPath());
+      if ( getVersion() != -1 )
+        strbuf.append("; version=").append("" + getVersion());
+      if ( getSecure() )
+        strbuf.append("; secure");
+      if ( httpOnly )
+        strbuf.append("; HttpOnly");
+      if ( commentURL != null )
+        strbuf.append("; commentURL=").append(commentURL);
+      if ( expires != null )
+        strbuf.append("; expires=").append(expires);
+      if ( discard )
+        strbuf.append("; discard");
+
+      if ( ports != null && (ports.length != 0) ) {
+        strbuf.append("; ports=\"");
+        if ( ports != EMPTY_PORT ) {
+          for ( int i = 0; i < ports.length; i++ )
+            strbuf.append(" " + ports[i]);
+        }
+        strbuf.append("\"");
+      }
+
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    return strbuf.toString();
+  }
+}
