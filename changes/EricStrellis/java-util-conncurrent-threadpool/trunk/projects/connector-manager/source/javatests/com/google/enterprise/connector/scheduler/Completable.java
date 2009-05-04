67e5f09f926f045192746fd2121c261c1edc56a2
EricStrellis <EricStrellis@56db1fc5-3b1b-0410-9cff-4307dac5e09e>
2009-05-04 21:27:40 +0000

    Initial implementation of ThreadPool based on java.util.concurrent

--- changes/EricStrellis/java-util-conncurrent-threadpool/trunk/projects/connector-manager/source/javatests/com/google/enterprise/connector/scheduler/Completable.java
+++ changes/EricStrellis/java-util-conncurrent-threadpool/trunk/projects/connector-manager/source/javatests/com/google/enterprise/connector/scheduler/Completable.java
@@ -1,9 +1,34 @@
+// Copyright 2009 Google Inc.  All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package com.google.enterprise.connector.scheduler;
 
 /**
- * A {@link Runnable} that supports cancellation.
+ * A {@link Cancelable} that runs in a container and performs
+ * completion logic when the container has fully completed the
+ * execution or cancellation.
  * @author EricStrellis@gmail.com (Eric Strellis)
  */
-public interface Completable extends Runnable{
+public interface Completable extends Cancelable{
+  /**
+   * Event to indicate the container has completed all processing for
+   * this {@link Cancelable}. This will be called after one of.
+   * <p>
+   * <OL>
+   * <LI> {@link Cancelable#run} returns
+   * <LI> {@Link Cancelable#cancel} returns
+   * </OL>
+   */
   public void onCompletion();
 }
