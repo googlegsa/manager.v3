66614bcb159bbfbe4facb6df64f0dabc641f366b
Brett Michael Johnson <Brett.Michael.Johnson@gmail.com>
2009-05-05 06:05:44 +0000

    This change adds a MainClass to the Connector Manager JAR file. The ManagerMain.main() method dumps the Connector Manager version string extracted from the connector.jar file.  This idea was taken from the Livelink Connector, whose MainClass dumps the Manifest to the console.  I think idea this should be extended to all the Connectors, as it would aid in troubleshooting.
    Simply run:
      java -jar path/to/connector.jar
    and it dumps the Connector Manager version of the jar file to
    the console.
    
    Changed Files:
    --------------
    M  build.xml
       - Specify ManagerMain as MainClass in the Jar Manifest.
    
    A  source/java/com/google/enterprise/connector/manager/ManagerMain.java
       - main() that prints the Connector Manager Version from the Jar Manifest
         to System.out.

---
+++ projects/connector-manager/source/java/com/google/enterprise/connector/manager/ManagerMain.java
@@ -0,0 +1,32 @@
+// Copyright (C)2009 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.enterprise.connector.manager;
+
+import com.google.enterprise.connector.common.JarUtils;
+import com.google.enterprise.connector.servlet.ServletUtil;
+
+/**
+ * Dump the Version info from the Manifest for the Connector Manager's JAR file.
+ * This is set as the default main() for the JAR if running the jar stand-alone.
+ * Simply run the command:
+ *   java -jar /path/to/connector.jar
+ */
+public class ManagerMain {
+  public static void main(String[] args) throws Exception {
+    System.out.println(ServletUtil.MANAGER_NAME + " v"
+                       + JarUtils.getJarVersion(ManagerMain.class));
+  }
+}
+
