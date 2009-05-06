749002b93db8ff07da7e0889d2fb815963978205
mgronber <mgronber@56db1fc5-3b1b-0410-9cff-4307dac5e09e>
2009-05-06 17:55:54 +0000

    Description:
    Fix for Issue 146.  Added an index.html page to each of the WAR files that
    redirects to the TestConnectivity Servlet.  This way, when the Web Application
    is deployed on a server the customer can navigate to the root of the Web
    Application (http://<host>:<port>/connector-manager) and get some status
    information about the server rather than a 404.
    
    Tests Passed:
    
      - All Unit tests.
      - Tested without redirect and link on index page will navigate to the
        TestConnectivity Servlet.
    
    ChangeLog:
    
    A source/html/index.html
      - New Home Page for Web Application that redirects to the TestConnectivity
        Servlet.
    
    M build.xml
      - Added property for web content location and added FileSet to each of the WAR
        targets.

---
+++ changes/mgronber/fix-issue-146/trunk/projects/connector-manager/source/java/com/google/enterprise/connector/servlet/AuthorizationParser.java
@@ -0,0 +1,275 @@
+// Copyright 2006-2009 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.enterprise.connector.servlet;
+
+import com.google.enterprise.connector.spi.AuthenticationIdentity;
+import com.google.enterprise.connector.spi.SimpleAuthenticationIdentity;
+
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * This class parses the xml body of an Authorization request. 
+ * </p>
+ * An authorization is a sequence of {@code ConnectorQuery} elements.  Each
+ * {@code ConnectorQuery} has a single {@code Identity} element and a sequence
+ * of {@code Resource} elements.  For example:
+ * <pre>
+ * &lt;AuthorizationQuery>
+ *   &lt;ConnectorQuery>
+ *     &lt;Identity domain="domain" source="connector">user1</Identity>
+ *     &lt;Resource>googleconnector://connector1.localhost/doc?docid=doc1a</Resource>
+ *     &lt;Resource>googleconnector://connector2.localhost/doc?docid=doc2a</Resource>
+ *     ...
+ *   &lt;/ConnectorQuery>
+ *   ...
+ * </pre>
+ * Note that the {@code domain} attribute of the {@code Identity} element is
+ * optional.
+ */
+public class AuthorizationParser {
+
+  private static final Logger LOGGER = 
+      Logger.getLogger(AuthorizationParser.class.getName());
+
+  private String xmlBody;
+  private int status;
+  private int numDocs;
+  private Map<AuthenticationIdentity, ConnectorQueries> parseMap;
+
+  public AuthorizationParser(String xmlBody) {
+    this.xmlBody = xmlBody;
+    parseMap = new HashMap<AuthenticationIdentity, ConnectorQueries>();
+    status = ConnectorMessageCode.SUCCESS;
+    numDocs = 0;
+    parse();
+  }
+
+  /**
+   * Parse the Authorization Request XML into a hierarchy: AuthorizationParser,
+   * ConnectorQueries, QueryUrls.
+   * <p>
+   * The top-level, this object (AuthorizationParser) is a map from
+   * AuthorizationIdentity objects to ConnectorQueries objects. This partitions
+   * the request by Identity.
+   * <p>
+   * The second-level object, ConnectorQueries, is a map from connector name to
+   * QueryUrls objects. For each connector, it gives all the object for which
+   * the request wants a decision.
+   * <p>
+   * The third-level object, QueryUrls, is a map from docid strings to the
+   * corresponding parsed representation as a ParsedUrl.
+   * <p>
+   * In practice, for now, it is unlikely that the same connector will show up
+   * under more than one identity. In fact, the most likely case is that the top
+   * two levels (AuthorizationParser and ConnectorQueries) each have only one
+   * item.
+   */
+  private void parse() {
+    Element root = ServletUtil.parseAndGetRootElement(xmlBody, 
+        ServletUtil.XMLTAG_AUTHZ_QUERY);
+
+    if (root == null) {
+      status = ConnectorMessageCode.ERROR_PARSING_XML_REQUEST;
+      return;
+    }
+
+    NodeList queryList = 
+        root.getElementsByTagName(ServletUtil.XMLTAG_CONNECTOR_QUERY);
+
+    if (queryList.getLength() == 0) {
+      LOGGER.log(Level.WARNING, ServletUtil.LOG_RESPONSE_EMPTY_NODE);
+      return;
+    }
+
+    status = ConnectorMessageCode.SUCCESS;
+    numDocs = 0;
+
+    for (int i = 0; i < queryList.getLength(); ++i) {
+      Element queryItem = (Element) queryList.item(i);
+      parseIdentityGroup(queryItem);
+      if (status != ConnectorMessageCode.SUCCESS) {
+        return;
+      }
+    }
+
+    if (numDocs == 0) {
+      LOGGER.warning("No docid available.");
+      return;
+    }
+  }
+
+  private void parseIdentityGroup(Element queryItem) {
+    String username = ServletUtil.getFirstElementByTagName(queryItem, 
+        ServletUtil.XMLTAG_IDENTITY);
+    String domain =
+        ServletUtil.getFirstAttribute(queryItem, ServletUtil.XMLTAG_IDENTITY,
+            ServletUtil.XMLTAG_DOMAIN_ATTRIBUTE);
+    List<String> resources = ServletUtil.getAllElementsByTagName(queryItem, 
+        ServletUtil.XMLTAG_RESOURCE);
+    if (resources.isEmpty()) {
+      status = ConnectorMessageCode.RESPONSE_NULL_RESOURCE;
+      return;
+    }
+
+    AuthenticationIdentity identity = findIdentity(username, domain);
+    ConnectorQueries urlsByConnector = getConnectorQueriesForIdentity(identity);
+    if (urlsByConnector == null) {
+      urlsByConnector = new ConnectorQueries();
+      putConnectorQueriesForIdentity(identity, urlsByConnector);
+    }
+
+    for (String url : resources) {
+      parseResource(urlsByConnector, url);
+    }
+  }
+  
+  private boolean matchesIdentity(AuthenticationIdentity id, String username, 
+      String domain) {
+    if (username == null && id.getUsername() != null) {
+      return false;
+    }
+    if (!username.equals(id.getUsername())) {
+      return false;
+    }
+    if (domain == null && id.getDomain() != null) {
+      return false;
+    }
+    return (domain.equals(id.getDomain()));
+  }
+
+  private AuthenticationIdentity findIdentity(String username, String domain) {
+    for (AuthenticationIdentity identity : parseMap.keySet()) {
+      if (matchesIdentity(identity, username, domain)) {
+        return identity;
+      }
+    }
+    return new SimpleAuthenticationIdentity(username, "", domain);
+  }
+
+  private void parseResource(ConnectorQueries urlsByConnector, String url) {
+    ParsedUrl p = new ParsedUrl(url);
+    if (p.getStatus() == ConnectorMessageCode.SUCCESS) {
+      QueryUrls urlsByDocid = urlsByConnector.getQueryUrls(p.getConnectorName());
+      if (urlsByDocid == null) {
+        urlsByDocid = new QueryUrls();
+        urlsByConnector.putQueryUrls(p.getConnectorName(), urlsByDocid);
+      }
+      urlsByDocid.putUrl(p.getDocid(), p);
+      numDocs++;
+    } else {
+      status = p.getStatus();
+    }
+  }
+  
+  public int getNumDocs() {
+    return numDocs;
+  }
+
+  public int getStatus() {
+    return status;
+  }
+
+  private void putConnectorQueriesForIdentity(AuthenticationIdentity identity,
+      ConnectorQueries urlsByConnector) {
+    parseMap.put(identity, urlsByConnector);
+  }
+
+  public Collection<AuthenticationIdentity> getIdentities() {
+    return parseMap.keySet();
+  }
+
+  public ConnectorQueries getConnectorQueriesForIdentity(AuthenticationIdentity 
+      identity) {
+    return parseMap.get(identity);
+  }
+
+  // Note: this is for testing only -- thus package private
+  int countParsedIdentities() {
+    return parseMap.size();
+  }
+
+  /**
+   * {@code ConnectorQueries} is a map from connector name to QueryUrls objects.
+   * For each connector, it gives all the object for which the request wants a
+   * decision.
+   */
+  public static class ConnectorQueries {
+    private Map<String, QueryUrls> queryMap;
+
+    /*
+     * Private constructor so this class can only be constructed by
+     * AuthorizationParser.
+     */
+    private ConnectorQueries() {
+      queryMap = new HashMap<String, QueryUrls>();
+    }
+
+    public int size() {
+      return queryMap.size();
+    }
+
+    public Collection<String> getConnectors() {
+      return queryMap.keySet();
+    }
+
+    public QueryUrls getQueryUrls(String connector) {
+      return queryMap.get(connector);
+    }
+
+    private QueryUrls putQueryUrls(String connector, QueryUrls queryUrls) {
+      return queryMap.put(connector, queryUrls);
+    }
+  }
+
+  /**
+   * {@code QueryUrls} is a map from docid strings to the corresponding parsed
+   * representation as a ParsedUrl.
+   */
+  public static class QueryUrls {
+    Map<String, ParsedUrl> urlMap;
+
+    /*
+     * Private constructor so this class can only be constructed by
+     * AuthorizationParser.
+     */
+    private QueryUrls() {
+      urlMap = new HashMap<String, ParsedUrl>();
+    }
+
+    private void putUrl(String docid, ParsedUrl p) {
+      urlMap.put(docid, p);
+    }
+
+    public Collection<String> getDocids() {
+      return urlMap.keySet();
+    }
+
+    public int size() {
+      return urlMap.size();
+    }
+
+    public ParsedUrl getUrl(String docid) {
+      return urlMap.get(docid);
+    }
+  }
+}
---
+++ changes/mgronber/fix-issue-146/trunk/projects/connector-manager/source/javatests/com/google/enterprise/connector/servlet/AuthorizationParserTest.java
@@ -0,0 +1,270 @@
+// Copyright 2009 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.enterprise.connector.servlet;
+
+import com.google.enterprise.connector.servlet.AuthorizationParser.ConnectorQueries;
+import com.google.enterprise.connector.servlet.AuthorizationParser.QueryUrls;
+import com.google.enterprise.connector.spi.AuthenticationIdentity;
+import com.google.enterprise.connector.spi.SimpleAuthenticationIdentity;
+
+import junit.framework.TestCase;
+
+/**
+ * Tests the AuthorizationParser class
+ */
+public class AuthorizationParserTest extends TestCase {
+  private static final String TWO_IDENTITIES_TWO_CONNECTORS =
+      "<AuthorizationQuery>\n" + "<ConnectorQuery>\n"
+          + "  <Identity source=\"connector\">username</Identity>\n"
+          + "  <Resource>googleconnector://connector1.localhost/" 
+          + "doc?docid=doc1a</Resource>\n"
+          + "  <Resource>googleconnector://connector2.localhost/" 
+          + "doc?docid=doc2a</Resource>\n"
+          + "  <Resource>googleconnector://connector1.localhost/" 
+          + "doc?docid=doc1b</Resource>\n"
+          + "  <Resource>googleconnector://connector2.localhost/" 
+          + "doc?docid=doc2b</Resource>\n"
+          + "</ConnectorQuery>\n" + "<ConnectorQuery>\n"
+          + "  <Identity source=\"connector\">username2</Identity>\n"
+          + "  <Resource>googleconnector://connector1.localhost/" 
+          + "doc?docid=doc1c</Resource>\n"
+          + "  <Resource>googleconnector://connector2.localhost/doc?" 
+          + "docid=doc2c</Resource>\n"
+          + "  <Resource>googleconnector://connector1.localhost/doc?" 
+          + "docid=doc1d</Resource>\n"
+          + "  <Resource>googleconnector://connector2.localhost/doc?" 
+          + "docid=doc2d</Resource>\n"
+          + "</ConnectorQuery>\n" 
+          + "</AuthorizationQuery>\n";
+  private static final String ONE_IDENTITY_TWO_QUERIES =
+      "<AuthorizationQuery>\n" + "<ConnectorQuery>\n"
+          + "  <Identity source=\"connector\">username</Identity>\n"
+          + "  <Resource>googleconnector://connector1.localhost/" 
+          + "doc?docid=doc1a</Resource>\n"
+          + "  <Resource>googleconnector://connector2.localhost/" 
+          + "doc?docid=doc2a</Resource>\n"
+          + "  <Resource>googleconnector://connector1.localhost/" 
+          + "doc?docid=doc1b</Resource>\n"
+          + "  <Resource>googleconnector://connector2.localhost/" 
+          + "doc?docid=doc2b</Resource>\n"
+          + "</ConnectorQuery>\n" 
+          + "<ConnectorQuery>\n"
+          + "  <Identity source=\"connector\">username</Identity>\n"
+          + "  <Resource>googleconnector://connector1.localhost/" 
+          + "doc?docid=doc1c</Resource>\n"
+          + "  <Resource>googleconnector://connector2.localhost/" 
+          + "doc?docid=doc2c</Resource>\n"
+          + "  <Resource>googleconnector://connector1.localhost/" 
+          + "doc?docid=doc1d</Resource>\n"
+          + "  <Resource>googleconnector://connector2.localhost/" 
+          + "doc?docid=doc2d</Resource>\n"
+          + "</ConnectorQuery>\n" 
+          + "</AuthorizationQuery>\n";
+  private static final String TWO_IDENTITIES_MULTIPLE_QUERIES =
+      "<AuthorizationQuery>\n" 
+          + "<ConnectorQuery>\n"
+          + "  <Identity source=\"connector\">username1</Identity>\n"
+          + "  <Resource>googleconnector://connector1.localhost/" 
+          + "doc?docid=doc1a</Resource>\n"
+          + "</ConnectorQuery>\n" 
+          + "<ConnectorQuery>\n"
+          + "  <Identity source=\"connector\">username2</Identity>\n"
+          + "  <Resource>googleconnector://connector2.localhost/" 
+          + "doc?docid=doc2a</Resource>\n"
+          + "</ConnectorQuery>\n" 
+          + "<ConnectorQuery>\n"
+          + "  <Identity source=\"connector\">username1</Identity>\n"
+          + "  <Resource>googleconnector://connector1.localhost/" 
+          + "doc?docid=doc1b</Resource>\n"
+          + "</ConnectorQuery>\n" 
+          + "<ConnectorQuery>\n"
+          + "  <Identity source=\"connector\">username2</Identity>\n"
+          + "  <Resource>googleconnector://connector2.localhost/" 
+          + "doc?docid=doc2b</Resource>\n"
+          + "</ConnectorQuery>\n" 
+          + "</AuthorizationQuery>\n";
+
+  private static final String MALFORMED_XML =
+      "<AuthorizationQuery>\n" 
+          + "<ConnectorQuery>\n" 
+          + "</AuthorizationQuery>\n" 
+          + "";
+
+  private boolean compareDomains(String domain1, String domain2) {
+    if (domain1 == null) {
+      return (domain1 == null);
+    }
+    return domain1.equals(domain2);
+  }
+
+  private ConnectorQueries getConnectorQueriesByIdentity(AuthorizationParser p,
+      AuthenticationIdentity identity) {
+    ConnectorQueries result = null;
+    for (AuthenticationIdentity i : p.getIdentities()) {
+      String username = identity.getUsername();
+      String domain = identity.getDomain();
+      if (username.equals(i.getUsername()) && 
+          compareDomains(domain, i.getDomain())) {
+        assertNull("Should be only one identity with username \"" 
+            + username + "\" and domain \"" + domain + "\"", result);
+        result = p.getConnectorQueriesForIdentity(i);
+      }
+    }
+    return result;
+  }
+
+  private int countConnectorsForUsername(AuthorizationParser p, 
+      String username) {
+    AuthenticationIdentity i = new SimpleAuthenticationIdentity(username);
+    return countConnectorsForIdentity(p, i);
+  }
+
+  private int countConnectorsForIdentity(AuthorizationParser p, 
+      AuthenticationIdentity i) {
+    return getConnectorQueriesByIdentity(p, i).size();
+  }
+
+  private int countUrlsForUsernameConnectorPair(AuthorizationParser p, 
+      String username, String connectorName) {
+    AuthenticationIdentity i = new SimpleAuthenticationIdentity(username);
+    return countUrlsForIdentityConnectorPair(p, i, connectorName);
+  }
+
+  private int countUrlsForIdentityConnectorPair(AuthorizationParser p, 
+      AuthenticationIdentity i, String connectorName) {
+    ConnectorQueries cq = getConnectorQueriesByIdentity(p, i);
+    assertNotNull(cq);
+    QueryUrls queryUrls = cq.getQueryUrls(connectorName);
+    if (queryUrls == null) {
+      return 0;
+    }
+    return queryUrls.size();
+  }
+
+  public void testSimple() {
+    AuthorizationParser ap = 
+        new AuthorizationParser(AuthorizationTest.TEST_XML1);
+    assertEquals(2, ap.countParsedIdentities());
+    assertEquals(1, countConnectorsForUsername(ap, "username"));
+    assertEquals(2, countConnectorsForUsername(ap, "CN=foo"));
+    assertEquals(1, countUrlsForUsernameConnectorPair(ap, "username", 
+        "connector3"));
+  }
+
+  public void testTwoIdentitiesTwoConnectors() {
+    AuthorizationParser ap = 
+        new AuthorizationParser(TWO_IDENTITIES_TWO_CONNECTORS);
+    assertEquals(2, ap.countParsedIdentities());
+    assertEquals(2, countConnectorsForUsername(ap, "username"));
+    assertEquals(2, countConnectorsForUsername(ap, "username2"));
+    assertEquals(2, countUrlsForUsernameConnectorPair(ap, "username", 
+        "connector2"));
+  }
+
+  public void testOneIdentityTwoQueries() {
+    AuthorizationParser ap = 
+        new AuthorizationParser(ONE_IDENTITY_TWO_QUERIES);
+    assertEquals(1, ap.countParsedIdentities());
+    assertEquals(2, countConnectorsForUsername(ap, "username"));
+    assertEquals(4, countUrlsForUsernameConnectorPair(ap, "username", 
+        "connector2"));
+  }
+
+  public void testTwoIdentitiesMultipleQueries() {
+    AuthorizationParser ap = 
+        new AuthorizationParser(TWO_IDENTITIES_MULTIPLE_QUERIES);
+    assertEquals(2, ap.countParsedIdentities());
+    assertEquals(1, countConnectorsForUsername(ap, "username1"));
+    assertEquals(0, countUrlsForUsernameConnectorPair(ap, "username1", 
+        "connector2"));
+  }
+
+  public void testMalformedXml() {
+    AuthorizationParser ap = new AuthorizationParser(MALFORMED_XML);
+    assertEquals(ConnectorMessageCode.ERROR_PARSING_XML_REQUEST, 
+        ap.getStatus());
+  }
+
+  private static final String ONE_DOMAINSPECIFIC_IDENTITY =
+      "<AuthorizationQuery>\n" 
+          + "<ConnectorQuery>\n"
+          + "  <Identity domain=\"foodomain\" source=\"connector\">" 
+          + "username</Identity>\n"
+          + "  <Resource>googleconnector://connector1.localhost/" 
+          + "doc?docid=doc1a</Resource>\n"
+          + "  <Resource>googleconnector://connector2.localhost/" 
+          + "doc?docid=doc2a</Resource>\n"
+          + "  <Resource>googleconnector://connector1.localhost/" 
+          + "doc?docid=doc1b</Resource>\n"
+          + "  <Resource>googleconnector://connector2.localhost/" 
+          + "doc?docid=doc2b</Resource>\n"
+          + "</ConnectorQuery>\n" 
+          + "</AuthorizationQuery>\n";
+
+  public void testOneDomainSpecificIdentity() {
+    AuthorizationParser ap = 
+        new AuthorizationParser(ONE_DOMAINSPECIFIC_IDENTITY);
+    assertEquals(1, ap.countParsedIdentities());
+    SimpleAuthenticationIdentity id =
+        new SimpleAuthenticationIdentity("username", null, "foodomain");
+    assertEquals(2, countConnectorsForIdentity(ap, id));
+    assertEquals(2, countUrlsForIdentityConnectorPair(ap, id, "connector1"));
+    assertEquals(2, countUrlsForIdentityConnectorPair(ap, id, "connector2"));
+  }
+
+  private static final String TWO_DOMAINSPECIFIC_IDENTITIES_MULTIPLE_QUERIES =
+      "<AuthorizationQuery>\n" 
+          + "<ConnectorQuery>\n"
+          + "  <Identity domain=\"arglebargle\" source=\"connector\">" 
+          +	"xyzzy</Identity>\n"
+          + "  <Resource>googleconnector://connector1.localhost/" 
+          + "doc?docid=doc1a</Resource>\n"
+          + "</ConnectorQuery>\n" 
+          + "<ConnectorQuery>\n"
+          + "  <Identity domain=\"bazfaz\" source=\"connector\">" 
+          +	"xyzzy</Identity>\n"
+          + "  <Resource>googleconnector://connector2.localhost/" 
+          + "doc?docid=doc2a</Resource>\n"
+          + "</ConnectorQuery>\n" 
+          + "<ConnectorQuery>\n"
+          + "  <Identity domain=\"arglebargle\" source=\"connector\">" 
+          + "xyzzy</Identity>\n"
+          + "  <Resource>googleconnector://connector1.localhost/" 
+          + "doc?docid=doc1b</Resource>\n"
+          + "</ConnectorQuery>\n" 
+          + "<ConnectorQuery>\n"
+          + "  <Identity domain=\"bazfaz\" source=\"connector\">" 
+          +	"xyzzy</Identity>\n"
+          + "  <Resource>googleconnector://connector2.localhost/" 
+          + "doc?docid=doc2b</Resource>\n"
+          + "</ConnectorQuery>\n" 
+          + "</AuthorizationQuery>\n";
+
+  public void testTwoDomainSpecificIdentitiesMultipleQueries() {
+    AuthorizationParser ap =
+        new AuthorizationParser(TWO_DOMAINSPECIFIC_IDENTITIES_MULTIPLE_QUERIES);
+    assertEquals(2, ap.countParsedIdentities());
+    SimpleAuthenticationIdentity i1 =
+        new SimpleAuthenticationIdentity("xyzzy", null, "arglebargle");
+    assertEquals(1, countConnectorsForIdentity(ap, i1));
+    assertEquals(2, countUrlsForIdentityConnectorPair(ap, i1, "connector1"));
+    assertEquals(0, countUrlsForIdentityConnectorPair(ap, i1, "connector2"));
+    SimpleAuthenticationIdentity i2 = 
+        new SimpleAuthenticationIdentity("xyzzy", null, "bazfaz");
+    assertEquals(1, countConnectorsForIdentity(ap, i2));
+    assertEquals(0, countUrlsForIdentityConnectorPair(ap, i2, "connector1"));
+    assertEquals(2, countUrlsForIdentityConnectorPair(ap, i2, "connector2"));
+  }
+}
