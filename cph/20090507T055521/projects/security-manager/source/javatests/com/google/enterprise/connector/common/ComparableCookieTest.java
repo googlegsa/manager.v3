f3049e19d1a19d489cbf1ab61c28b3e5cd487bc3
Chris Hanson <cph@google.com>
2009-05-07 17:23:42 +0000

    Eliminate remainder of reflection; implement compareTo tests.

--- cph/20090507T055521/projects/security-manager/source/javatests/com/google/enterprise/connector/common/ComparableCookieTest.java
+++ cph/20090507T055521/projects/security-manager/source/javatests/com/google/enterprise/connector/common/ComparableCookieTest.java
@@ -16,7 +16,6 @@ package com.google.enterprise.connector.common;
 
 import com.google.enterprise.common.SecurityManagerTestCase;
 
-import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -38,59 +37,104 @@ public class ComparableCookieTest extends SecurityManagerTestCase {
 
   private final Runner runTestEquals;
   private final Runner runTestHashCode;
+  private final Runner runTestCompareTo;
+  private final Setter<String> valueSetter;
+  private final Setter<String> commentSetter;
+  private final Setter<Integer> versionSetter;
+  private final Setter<Integer> maxAgeSetter;
+  private final Setter<Boolean> secureSetter;
 
-  private final Method setValueMethod;
-  private final Method setCommentMethod;
-  private final Method setVersionMethod;
-  private final Method setMaxAgeMethod;
-  private final Method setSecureMethod;
-
-  public ComparableCookieTest() throws Exception {
-    runTestEquals = new EqualsRunner();
-    runTestHashCode = new HashCodeRunner();
-
-    setValueMethod = Cookie.class.getMethod("setValue", String.class);
-    setCommentMethod = Cookie.class.getMethod("setComment", String.class);
-    setVersionMethod = Cookie.class.getMethod("setVersion", Integer.TYPE);
-    setMaxAgeMethod = Cookie.class.getMethod("setMaxAge", Integer.TYPE);
-    setSecureMethod = Cookie.class.getMethod("setSecure", Boolean.TYPE);
+  public ComparableCookieTest() {
+    runTestEquals =
+        new Runner() {
+          @Override
+          public void run(TestElement element) {
+            element.testEquals();
+          }
+        };
+    runTestHashCode =
+        new Runner() {
+          @Override
+          public void run(TestElement element) {
+            element.testHashCode();
+          }
+        };
+    runTestCompareTo =
+        new Runner() {
+          @Override
+          public void run(TestElement element) {
+            element.testCompareTo();
+          }
+        };
+    valueSetter =
+        new Setter<String>() {
+          @Override
+          public void set(Cookie c, String value) {
+            c.setValue(value);
+          }
+        };
+    commentSetter =
+        new Setter<String>() {
+          @Override
+          public void set(Cookie c, String value) {
+            c.setComment(value);
+          }
+        };
+    versionSetter =
+        new Setter<Integer>() {
+          @Override
+          public void set(Cookie c, Integer value) {
+            c.setVersion(value);
+          }
+        };
+    maxAgeSetter =
+        new Setter<Integer>() {
+          @Override
+          public void set(Cookie c, Integer value) {
+            c.setMaxAge(value);
+          }
+        };
+    secureSetter =
+        new Setter<Boolean>() {
+          @Override
+          public void set(Cookie c, Boolean value) {
+            c.setSecure(value);
+          }
+        };
   }
 
-  public void testEquals() throws Exception {
+  private abstract class Runner {
+    public abstract void run(TestElement element);
+  }
+
+  private abstract class Setter<T> {
+    public abstract void set(Cookie c, T value);
+  }
+
+  public void testEquals() {
     runOverPairs(runTestEquals);
     runValueInvarianceTests(runTestEquals);
   }
 
-  public void testHashCode() throws Exception {
+  public void testHashCode() {
     runOverPairs(runTestHashCode);
     runValueInvarianceTests(runTestHashCode);
   }
 
-  private void runValueInvarianceTests(Runner runner) throws Exception {
-    runOverModifiedPairs(runner, setValueMethod, VALUES);
-    runOverModifiedPairs(runner, setCommentMethod, COMMENTS);
-    runOverModifiedPairs(runner, setVersionMethod, VERSIONS);
-    runOverModifiedPairs(runner, setMaxAgeMethod, MAXAGES);
-    runOverModifiedPairs(runner, setSecureMethod, SECURES);
+  public void testCompareTo() {
+    runOverPairs(runTestCompareTo);
+    runValueInvarianceTests(runTestCompareTo);
   }
 
-  private interface Runner {
-    public void run(TestElement element) throws Exception;
+  private void runValueInvarianceTests(Runner runner) {
+    runOverModifiedPairs(runner, valueSetter, VALUES);
+    runOverModifiedPairs(runner, commentSetter, COMMENTS);
+    runOverModifiedPairs(runner, versionSetter, VERSIONS);
+    runOverModifiedPairs(runner, maxAgeSetter, MAXAGES);
+    runOverModifiedPairs(runner, secureSetter, SECURES);
   }
 
-  private static class EqualsRunner implements Runner {
-    public void run(TestElement element) throws Exception {
-      element.testEquals();
-    }
-  }
-
-  private static class HashCodeRunner implements Runner {
-    public void run(TestElement element) throws Exception {
-      element.testHashCode();
-    }
-  }
-
-  private static void runOverPairs(Runner runner) throws Exception {
+  private void runOverPairs(Runner runner) {
     for (String name1 : NAMES) {
       for (String name2 : NAMES) {
         for (String domain1 : DOMAINS) {
@@ -114,53 +158,52 @@ public class ComparableCookieTest extends SecurityManagerTestCase {
     return ComparableCookie.wrap(c);
   }
 
-  private static <T> void runOverModifiedPairs(Runner runner, Method setter, T[] values)
-      throws Exception {
+  private <T> void runOverModifiedPairs(Runner runner, Setter<T> setter, T[] values) {
     runOverPairs(new ModifiedPairRunner<T>(runner, setter, values));
   }
 
-  private static class ModifiedPairRunner<T> implements Runner {
+  private class ModifiedPairRunner<T> extends Runner {
 
     private final Runner runner;
-    private final Method setter;
+    private final Setter<T> setter;
     private final T[] values;
 
-    public ModifiedPairRunner(Runner runner, Method setter, T[] values)
-      throws Exception {
+    public ModifiedPairRunner(Runner runner, Setter<T> setter, T[] values) {
       this.runner = runner;
       this.setter = setter;
       this.values = values;
     }
 
-    public void run(TestElement element) throws Exception {
+    @Override
+    public void run(TestElement element) {
       for (T value : values) {
         {
           TestElement element2 = element.copy();
-          setter.invoke(element2.getCookie1().getCookie(), value);
+          setter.set(element2.getCookie1().getCookie(), value);
           runner.run(element2);
         }
         {
           TestElement element2 = element.copy();
-          setter.invoke(element2.getCookie2().getCookie(), value);
+          setter.set(element2.getCookie2().getCookie(), value);
           runner.run(element2);
         }
         {
           TestElement element2 = element.copy();
-          setter.invoke(element2.getCookie1().getCookie(), value);
-          setter.invoke(element2.getCookie2().getCookie(), value);
+          setter.set(element2.getCookie1().getCookie(), value);
+          setter.set(element2.getCookie2().getCookie(), value);
           runner.run(element2);
         }
         for (T value2 : values) {
           TestElement element2 = element.copy();
-          setter.invoke(element2.getCookie1().getCookie(), value);
-          setter.invoke(element2.getCookie2().getCookie(), value2);
+          setter.set(element2.getCookie1().getCookie(), value);
+          setter.set(element2.getCookie2().getCookie(), value2);
           runner.run(element2);
         }
       }
     }
   }
 
-  private static class TestElement {
+  private class TestElement {
     private final ComparableCookie cc1;
     private final ComparableCookie cc2;
     private final String message;
@@ -187,9 +230,7 @@ public class ComparableCookieTest extends SecurityManagerTestCase {
     }
 
     public void testEquals() {
-      if (sameStringIgnoreCase(cc1.getCookie().getName(), cc2.getCookie().getName())
-          && sameStringIgnoreCase(cc1.getCookie().getDomain(), cc2.getCookie().getDomain())
-          && sameString(cc1.getCookie().getPath(), cc2.getCookie().getPath())) {
+      if (equalCookies(cc1.getCookie(), cc2.getCookie())) {
         assertTrue(message, cc1.equals(cc2));
       } else {
         assertFalse(message, cc1.equals(cc2));
@@ -197,20 +238,56 @@ public class ComparableCookieTest extends SecurityManagerTestCase {
     }
 
     public void testHashCode() {
-      if (cc1.equals(cc2)) {
+      if (equalCookies(cc1.getCookie(), cc2.getCookie())) {
         assertEquals(message, cc1.hashCode(), cc2.hashCode());
+      }
+    }
+
+    public void testCompareTo() {
+      int d1 = compareCookies(cc1.getCookie(), cc2.getCookie());
+      int d2 = cc1.compareTo(cc2);
+      if (d1 < 0) {
+        assertTrue(message, d2 < 0);
+      } else if (d1 > 0) {
+        assertTrue(message, d2 > 0);
+      } else {
+        assertTrue(message, d2 == 0);
       }
     }
   }
 
-  private static boolean sameString(String s1, String s2) {
-    return (s1 == null) ? (s2 == null) : s1.equals(s2);
+  private static boolean equalCookies(Cookie c1, Cookie c2) {
+    return
+        equalStringsIgnoreCase(c1.getName(), c2.getName())
+        && equalStringsIgnoreCase(c1.getDomain(), c2.getDomain())
+        && equalStrings(c1.getPath(), c2.getPath());
   }
 
-  private static boolean sameStringIgnoreCase(String s1, String s2) {
-    return (s1 == null) ? (s2 == null) : s1.equalsIgnoreCase(s2);
+  private static boolean equalStrings(String s1, String s2) {
+    return deNull(s1).equals(deNull(s2));
   }
 
-  // compareTo
+  private static boolean equalStringsIgnoreCase(String s1, String s2) {
+    return deNull(s1).equalsIgnoreCase(deNull(s2));
+  }
 
+  private static int compareCookies(Cookie c1, Cookie c2) {
+    int d = compareStringsIgnoreCase(c1.getName(), c2.getName());
+    if (d != 0) return d;
+    d = compareStringsIgnoreCase(c1.getDomain(), c2.getDomain());
+    if (d != 0) return d;
+    return compareStrings(c1.getPath(), c2.getPath());
+  }
+
+  private static int compareStrings(String s1, String s2) {
+    return deNull(s1).compareTo(deNull(s2));
+  }
+
+  private static int compareStringsIgnoreCase(String s1, String s2) {
+    return deNull(s1).compareToIgnoreCase(deNull(s2));
+  }
+
+  private static String deNull(String s) {
+    return ((s == null) ? "" : s);
+  }
 }
