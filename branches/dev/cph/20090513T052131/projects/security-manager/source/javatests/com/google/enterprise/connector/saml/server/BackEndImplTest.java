84f163e9d78220669b4d909ac975c3d84a3a185f
cph.mit <cph.mit@56db1fc5-3b1b-0410-9cff-4307dac5e09e>
2009-05-13 05:21:38 +0000

    New working branch.

---
+++ branches/dev/cph/20090513T052131/projects/security-manager/source/javatests/com/google/enterprise/connector/saml/server/BackEndImplTest.java
@@ -0,0 +1,93 @@
+// Copyright (C) 2008 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.enterprise.connector.saml.server;
+
+import com.google.enterprise.connector.common.SecurityManagerTestCase;
+import com.google.enterprise.connector.common.cookie.CookieUtil;
+import com.google.enterprise.connector.saml.server.AuthzResponderImpl;
+import com.google.enterprise.connector.saml.server.BackEndImpl;
+import com.google.enterprise.connector.security.identity.AuthnDomain;
+import com.google.enterprise.connector.security.identity.AuthnDomainGroup;
+import com.google.enterprise.connector.security.identity.AuthnMechanism;
+import com.google.enterprise.connector.security.identity.CredentialsGroup;
+import com.google.enterprise.connector.security.manager.LocalSessionManager;
+
+import org.springframework.mock.web.MockHttpSession;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.Cookie;
+
+/**
+ * Unit test for BackEndImpl.
+ */
+public class BackEndImplTest extends SecurityManagerTestCase {
+
+  private final List<AuthnDomainGroup> adgs;
+  private LocalSessionManager sm;
+  private BackEndImpl backend;
+
+  public BackEndImplTest() {
+    adgs = new ArrayList<AuthnDomainGroup>();
+    adgs.add(new AuthnDomainGroup("adg1"));
+    adgs.add(new AuthnDomainGroup("adg2"));
+    adgs.add(new AuthnDomainGroup("adg3"));
+
+    new AuthnDomain("basicDomain", AuthnMechanism.BASIC_AUTH, "basic_loginurl", adgs.get(0));
+    new AuthnDomain("formsDomain", AuthnMechanism.FORMS_AUTH, "forms_loginurl", adgs.get(1));
+    new AuthnDomain("connectorDomain", AuthnMechanism.CONNECTORS, "connector_loginurl",
+                    adgs.get(2));
+  }
+
+  @Override
+  public void setUp() throws Exception {
+    super.setUp();
+    sm = new LocalSessionManager();
+    backend = new BackEndImpl(sm, new AuthzResponderImpl());
+  }
+
+  public void testUpdateSessionManager() {
+    List<CredentialsGroup> cgs = CredentialsGroup.newGroups(adgs, new MockHttpSession());
+    CredentialsGroup basicCG = cgs.get(0);
+    CredentialsGroup formsCG = cgs.get(1);
+    // CredentialsGroup connectorCG = cgs.get(2);
+
+    basicCG.setUsername("user");
+    basicCG.setPassword("password");
+
+    String sid = sm.createSession();
+    List<CredentialsGroup> cgList = new ArrayList<CredentialsGroup>();
+    cgList.add(basicCG);
+    backend.updateSessionManager(sid, cgList);
+    assertEquals("user", backend.adapter.getUsername(sid));
+    assertEquals("password", backend.adapter.getPassword(sid));
+
+    formsCG.setUsername("joe");
+    formsCG.setPassword("bob");
+    formsCG.getElements().get(0).addCookie(new Cookie("cookieOne", "cookieOneVal"));
+
+    sid = sm.createSession();
+    cgList.add(formsCG);
+    backend.updateSessionManager(sid, cgList);
+    assertEquals("user", backend.adapter.getUsername(sid));
+    assertEquals("password", backend.adapter.getPassword(sid));
+    List<Cookie> cookies = CookieUtil.deserializeCookies(backend.adapter.getCookies(sid));
+    assertEquals(1, cookies.size());
+    assertEquals("cookieOne", cookies.get(0).getName());
+    assertEquals("cookieOneVal", cookies.get(0).getValue());
+  }
+
+}
