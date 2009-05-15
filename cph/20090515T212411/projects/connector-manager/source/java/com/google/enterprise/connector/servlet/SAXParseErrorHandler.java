37147f6a2ade27524f27f268b70d5d213d885937
Chris Hanson <cph@google.com>
2009-05-15 21:24:20 +0000

    New working branch.

---
+++ cph/20090515T212411/projects/connector-manager/source/java/com/google/enterprise/connector/servlet/SAXParseErrorHandler.java
@@ -0,0 +1,46 @@
+// Copyright (C) 2006 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.enterprise.connector.servlet;
+
+import org.xml.sax.ErrorHandler;
+import org.xml.sax.SAXParseException;
+
+import java.util.logging.*;
+
+/**
+ * A simple <code>ErrorHandler</code> implementation
+ * to be used for SAX Parser.
+ *
+ */
+public class SAXParseErrorHandler implements ErrorHandler {
+  private static final Logger LOG =
+    Logger.getLogger(SAXParseErrorHandler.class.getName());
+
+  public SAXParseErrorHandler() {
+  }
+
+  public void error(SAXParseException exception) {
+    LOG.log(Level.INFO, "Error", exception);
+  }
+
+  public void fatalError(SAXParseException exception) {
+    LOG.log(Level.INFO, "FatalError", exception);
+  }
+
+  public void warning(SAXParseException exception) {
+    LOG.log(Level.INFO, "Warning", exception);
+  }
+
+}
