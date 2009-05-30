1a5a3ebd00ee8404f70c2227d143c169ffd38bd9
Brett Michael Johnson <Brett.Michael.Johnson@gmail.com>
2009-05-30 20:46:06 +0000

    Merged from trunk: r2079,2091,2095,2098-2100: Add XmlFormatter, fix dtd location failure, Windows DocPusherTest failure, expose ParseURL for security-manager use, update RELEASE_NOTES.

--- projects/connector-manager/source/javatests/com/google/enterprise/connector/test/ConnectorTestUtils.java
+++ projects/connector-manager/source/javatests/com/google/enterprise/connector/test/ConnectorTestUtils.java
@@ -14,21 +14,15 @@
 
 package com.google.enterprise.connector.test;
 
-import com.google.enterprise.connector.manager.Context;
 import com.google.enterprise.connector.servlet.ServletUtil;
-
-import org.springframework.context.ApplicationContext;
-import org.springframework.core.io.Resource;
 
 import junit.framework.Assert;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.util.Map;
 import java.util.Set;
@@ -37,91 +31,6 @@ public class ConnectorTestUtils {
 
   private ConnectorTestUtils() {
     // prevents instantiation
-  }
-
-  /**
-   * Find a named file on the classpath then read the entire content as a
-   * String, skipping comment lines (lines that begin with #) and end-line
-   * comments (from the first occurrence of // to the end).
-   *
-   * @param filename The name of file on the classpath
-   * @return The contents of the reader (skipping comments)
-   */
-  public static String streamToString(String filename) {
-    if (filename == null) {
-      throw new IllegalArgumentException("filename is null");
-    }
-    if (filename.length() < 1) {
-      throw new IllegalArgumentException("filename is empty");
-    }
-    InputStream s = ConnectorTestUtils.class.getResourceAsStream(filename);
-    if (s == null) {
-      throw new IllegalArgumentException(
-          "filename must be found on the classpath: " + filename);
-    }
-    InputStreamReader isr = new InputStreamReader(s);
-    BufferedReader br = new BufferedReader(isr);
-    return streamToString(br);
-  }
-
-  /**
-   * Read a buffered reader and return the entire contents as a String, skipping
-   * comment lines (lines that begin with #) and end-line comments (from the
-   * first occurrence of // to the end).
-   *
-   * @param br An Buffered Reader ready for reading
-   * @return The contents of the reader (skipping comments)
-   */
-  public static String streamToString(BufferedReader br) {
-    StringBuilder b = new StringBuilder(1024);
-    String line;
-    try {
-      while ((line = br.readLine()) != null) {
-        if (line.startsWith("#")) {
-          // skip comment lines
-          continue;
-        }
-        int index = line.indexOf("//");
-        if (index == -1) {
-          b.append(line);
-        } else {
-          b.append(line.subSequence(0, index));
-        }
-        b.append('\n');
-      }
-    } catch (IOException e) {
-      throw new RuntimeException(e);
-    }
-    return b.toString();
-  }
-
-  /**
-   * Read an entire InputStream and return its contents as a String
-   *
-   * @param is InputStream to read
-   * @return contents as a String
-   */
-  public static String streamToString(InputStream is) {
-    byte buf[] = new byte[2048];
-    int bytesRead;
-    try {
-      bytesRead = is.read(buf);
-    } catch (IOException e) {
-      throw new IllegalArgumentException("I/O problem reading stream");
-    }
-    String res = new String(buf, 0, bytesRead);
-    return res;
-  }
-
-  /**
-   * Normalizes strings with \r\n newlines to just \n
-   *
-   * @param input String to normalize
-   * @return the normalized result
-   */
-  public static String normalizeNewlines(String input) {
-    String result = input.replaceAll("\r\n", "\n");
-    return result;
   }
 
   /**
@@ -138,39 +47,9 @@ public class ConnectorTestUtils {
   }
 
   /**
-   * Removes the connector manager version string from the string.
-   * This allows the tests that compare actual output to expected
-   * output to function accross versions, jvms, and platforms.
-   */
-  public static String removeManagerVersion(String string) {
-    StringBuffer buffer = new StringBuffer(string);
-    removeManagerVersion(buffer);
-    return buffer.toString();
-  }
-
-  /**
-   * Gets the full path of a file by resolving it using the Context. This allows
-   * there to be a different root directory if this is a junit test rather than
-   * a servlet context.  For now, this routine is only for testing.
-   *
-   * @param fileName A relative file name to resolve
-   * @param context The context
-   * @return The full path name
-   * @throws IOException
-   */
-  public static String getFileFullPath(String fileName, Context context)
-      throws IOException {
-    ApplicationContext applicationContext = context.getApplicationContext();
-    Resource resource = applicationContext.getResource(fileName);
-    File file = resource.getFile();
-    String path = file.getAbsolutePath();
-    return path;
-  }
-
-  /**
    * Compare two maps.  The maps need not be identical, but map1
    * should be a subset of map2.  Note that this is slightly different
-   * behaviour than earlier versions of compareMaps.
+   * behavior than earlier versions of compareMaps.
    *
    * @param map1 a Map that should be a subset of map2
    * @param map2 a Map that should be a superset of map1
@@ -197,7 +76,7 @@ public class ConnectorTestUtils {
         for(int i = 0; i < files.length; i++) {
             res &= deleteAllFiles(files[i]);
         }
-        res = dir.delete();//Delete dir itself
+        res = dir.delete(); // Delete dir itself.
     } else {
         res = dir.delete();
     }
