9dffc6d109de6d5d872a216b483c81d00f6281e9
Brett.Michael.Johnson <Brett.Michael.Johnson@56db1fc5-3b1b-0410-9cff-4307dac5e09e>
2009-06-13 20:52:16 +0000

    This set of changes adds Connector Manager support for compressed feeds - Issue 153
    To improve feed throughput of content+metadata feeds, we have decided to compress the
    document content in the feed.  We have decided to use ZLIB to compress the raw content, then
    Base64 encode the result.
    
    This is done using a new CompressedFilterInputStream that sits between the content InputStream
    and the Base64FilterInputStream in DocPusher.
    
    The use of compression is subject to appropriate support in the GSA, so the FeedConnection
    interface was enhanced to provide the supported content encodings (among other enhancements).
    
    
    Change Log:
    ----------
    A  projects/connector-manager/source/java/com/google/enterprise/connector/common/CompressedFilterInputStream.java
       - A FilterInputStream that compresses its throughput using java.util.zip.Deflater.
    
    A  projects/connector-manager/source/javatests/com/google/enterprise/connector/common/CompressedFilterInputStreamTest.java
       - Unit tests for CompressedFilterInputStream.
    
    M  projects/connector-manager/source/java/com/google/enterprise/connector/pusher/DocPusher.java
       - Adds CompressedFilterInputStream to the document content pipeline if the GSA supports it.
    
    M  projects/connector-manager/source/javatests/com/google/enterprise/connector/pusher/DocPusherTest.java
       - Add a test for compressed content feeds.
    
    M  projects/connector-manager/source/java/com/google/enterprise/connector/pusher/FeedConnection.java
    M  projects/connector-manager/source/java/com/google/enterprise/connector/pusher/GsaFeedConnection.java
    M  projects/connector-manager/source/java/com/google/enterprise/connector/pusher/NullFeedConnection.java
    M  projects/connector-manager/source/javatests/com/google/enterprise/connector/pusher/MockFeedConnection.java
       - Enhanced FeedConnection Interface, adding methods to get feed backlog count (Issue 124),
         supported Schedule formats (Issues 66, 67, 79, 127, 144), and supported content encodings
         (Issue 153).

--- trunk/projects/connector-manager/source/java/com/google/enterprise/connector/pusher/FeedConnection.java
+++ trunk/projects/connector-manager/source/java/com/google/enterprise/connector/pusher/FeedConnection.java
@@ -27,16 +27,56 @@ public interface FeedConnection {
   /**
    * Sends data contained in the given data object identified as the given data
    * source name.
-   * @param dataSource the name of the data source.  The data source name should
-   *    match the regex [a-zA-Z_][a-zA-Z0-9_-]*, the first character must be a
-   *    letter or underscore, the rest of the characters can be alphanumeric,
-   *    dash, or underscore.
+   *
+   * @param dataSource the name of the data source. The data source name should
+   *        match the regex [a-zA-Z_][a-zA-Z0-9_-]*, the first character must
+   *        be a letter or underscore, the rest of the characters can be
+   *        alphanumeric, dash, or underscore.
    * @param feedData an object that encapsulates the feed data that needs to be
-   *    sent by the <code>FeedConnection</code>.
+   *        sent by the <code>FeedConnection</code>.
    * @return response from the feed server.
    * @throws FeedException if problem extracting the data or sending it.
    * @throws RepositoryException if problem retrieving data from the Connector.
    */
   public String sendData(String dataSource, FeedData feedData)
       throws FeedException, RepositoryException;
+
+  /**
+   * Returns the count of unprocessed Feed items. The Feed host may temporarily
+   * stop processing Feed items during periodic maintenance, when resetting the
+   * index, during system configuration, or due to certain error conditions.
+   * The Feed client may make use of the backlog count to throttle back its
+   * feeds.
+   *
+   * @return number of outstanding feed items, or -1 if this feature is not
+   *         supported.
+   */
+  public int getBacklogCount();
+
+  /**
+   * Return the version of serialized {@link Schedule} string supported.
+   * The Schedule versions are:
+   *  <ul>
+   *  <li>0 - Unknown</li>
+   *  <li>1 - <code>connectorName:hostLoad:timeIntervals...</code></li>
+   *  <li>2 - <code>connectorName:hostLoad:retryDelayMillis:timeIntervals...</code>
+   *          adds retryDelayMillis.</li>
+   *  <li>3 - <code>#connectorName:hostLoad:retryDelayMillis:timeIntervals...</code>
+   *          where leading '#' indicates disabled schedule, and a
+   *          retryDelayMillis value of -1 indicates traverse to until
+   *          no new content, then automatically disable.</li>
+   *  </ul>
+   *
+   * @return Schedule version number supported.
+   */
+  public int getScheduleFormat();
+
+  /**
+   * Return a String consisting of a comma-separated list supported content
+   * encodings.  For instance: "base64binary, base64compressed".
+   *
+   * @return supported content encodings.
+   */
+  public String getContentEncodings();
+
 }
