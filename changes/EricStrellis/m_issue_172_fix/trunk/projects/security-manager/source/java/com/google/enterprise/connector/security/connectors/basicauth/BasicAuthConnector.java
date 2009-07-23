478eb49db2eb709b0fbcc8bdbd81a3323422a51b
EricStrellis <EricStrellis@56db1fc5-3b1b-0410-9cff-4307dac5e09e>
2009-07-23 22:09:42 +0000

    This fix prevents shutdown from hanging by forcefully shutting down the ScheduledThreadPoolExecutor that runs timeout tasks for canceling hunb/stalled traversals.
    The change addresses issue 172 described here
    http://code.google.com/p/google-enterprise-connector-manager/issues/detail?id=172&sort=-id%20-type
    
    In additon this change introduces a ThreadFactory that includes an application
    defined prefix for thread names and uses the factory to identify the
    executor service for the thread.
    
    In addition this change

---
+++ changes/EricStrellis/m_issue_172_fix/trunk/projects/security-manager/source/java/com/google/enterprise/connector/security/connectors/basicauth/BasicAuthConnector.java
@@ -0,0 +1,101 @@
+// Copyright (C) 2008 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.enterprise.connector.security.connectors.basicauth;
+
+import com.google.enterprise.connector.common.HttpExchange;
+import com.google.enterprise.connector.common.SecurityManagerUtil;
+import com.google.enterprise.connector.spi.AuthenticationIdentity;
+import com.google.enterprise.connector.spi.AuthenticationManager;
+import com.google.enterprise.connector.spi.AuthenticationResponse;
+import com.google.enterprise.connector.spi.AuthorizationManager;
+import com.google.enterprise.connector.spi.Connector;
+import com.google.enterprise.connector.spi.RepositoryException;
+import com.google.enterprise.connector.spi.SecAuthnIdentity;
+import com.google.enterprise.connector.spi.Session;
+import com.google.enterprise.connector.spi.TraversalManager;
+import com.google.enterprise.connector.spi.VerificationStatus;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.logging.Logger;
+
+public class BasicAuthConnector implements Connector, Session, AuthenticationManager {
+
+  @SuppressWarnings("unused")
+  private final String something;
+
+  private static final Logger LOGGER =
+    Logger.getLogger(BasicAuthConnector.class.getName());
+
+  public BasicAuthConnector(String data) {
+    this.something = data;      // not used
+  }
+
+  public AuthenticationResponse authenticate(AuthenticationIdentity raw)
+      throws RepositoryException {
+    SecAuthnIdentity identity = SecAuthnIdentity.class.cast(raw);
+    AuthenticationResponse notfound = new AuthenticationResponse(false, null);
+    String username = identity.getUsername();
+    String password = identity.getPassword();
+    if (username == null || password == null) {
+      identity.setVerificationStatus(VerificationStatus.INDETERMINATE);
+      return notfound;
+    }
+
+    LOGGER.info("BasicAuth attempting on login url: " + identity.getSampleUrl());
+    URL loginUrl;
+    try {
+      loginUrl = new URL(identity.getSampleUrl());
+    } catch (IOException e) {
+      throw new RepositoryException(e);
+    }
+    HttpExchange exchange = SecurityManagerUtil.getHttpClient().getExchange(loginUrl);
+    exchange.setBasicAuthCredentials(username, password);
+    exchange.setRequestHeader("User-Agent", "SecMgr");
+    int status = 0;
+    try {
+      status = exchange.exchange();
+    } catch (IOException e) {
+      LOGGER.info(e.toString());
+      throw new RepositoryException(e);
+    } finally {
+      exchange.close();
+    }
+
+    LOGGER.info("BasicAuth authenticate status: " + status);
+    if (status == 200) {
+      identity.setVerificationStatus(VerificationStatus.VERIFIED);
+      return new AuthenticationResponse(true, username);
+    }
+    identity.setVerificationStatus(VerificationStatus.REFUTED);
+    return notfound;
+  }
+
+  public Session login() {
+    return this;
+  }
+
+  public AuthenticationManager getAuthenticationManager() {
+    return this;
+  }
+
+  public AuthorizationManager getAuthorizationManager() {
+    throw new UnsupportedOperationException();
+  }
+
+  public TraversalManager getTraversalManager() {
+    throw new UnsupportedOperationException();
+  }
+}
