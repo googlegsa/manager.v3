1fddddb521447b1dad58f6f8a080190bc8b0c7bc
Brett.Michael.Johnson <Brett.Michael.Johnson@56db1fc5-3b1b-0410-9cff-4307dac5e09e>
2009-10-10 00:44:54 +0000

    Issue 143 - Add Ignored set to MimeTypeMap to skip documents.
    TraversalContext MimeTypeMap currently has classes 'preferred',
    'supported', 'unsupported', and 'unknown'.  Even with 'unsupported' and
    'unknown' mime-types, the connectors are permitted (actually encouraged) to
    submit meta-data about the document, but not submit the document content
    itself.
    
    Several customers have expressed a desire to skip certain classes of
    document completely.  They do not wish these documents to be fed, even
    if only meta-data would indexed.
    
    This change adds a new set of mime types to the MimeTypeMap, 'ignored'.
    Connectors should skip this class of documents entirely.  Do not feed
    content.  Do not feed meta-data.
    
    Change Log:
    ----------
    M  projects/connector-manager/source/java/com/google/enterprise/connector/traversal/MimeTypeMap.java
       - Enhanced the documentation for the mimeTypeSupportLevel() method,
         differentiating between 'unsupported' and 'ignored'.
       - Added setIgnoredMimeTypes() method.
       - Collapsed all possible support levels for 'ignored' and 'unsupported'
         to -1 and 0, respectively.
    
    M  projects/connector-manager/source/java/com/google/enterprise/connector/spi/TraversalContext.java
       - Enhanced the documentation for the mimeTypeSupportLevel() method,
         differentiating between 'unsupported' and 'ignored'.
    
    M  projects/connector-manager/source/javatests/com/google/enterprise/connector/traversal/MimeTypeMapTest.java
       - Added tests for ignoredMimeTypes.
    
    M  projects/connector-manager/etc/applicationContext.xml
       - Added new Set of mime types to the MimeTypeMap configuration,
         'ignoredMimeTypes'.
       - Moved some mimetypes that were explicitly specified in the default
         GSA Do-not-Crawl-or-Index page from 'unsupported' to 'ignored'.

---
+++ changes/Brett.Michael.Johnson/issue_143/trunk/projects/connector-manager/source/javatests/com/google/enterprise/connector/servlet/AuthenticateTest.java
@@ -0,0 +1,151 @@
+// Copyright (C) 2006-2009 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.enterprise.connector.servlet;
+
+import com.google.enterprise.connector.common.StringUtils;
+import com.google.enterprise.connector.manager.MockManager;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.logging.Logger;
+
+/**
+ * Tests the AuthenticateTest servlet class
+ *
+ */
+public class AuthenticateTest extends TestCase {
+  private static final Logger LOGGER =
+    Logger.getLogger(AuthenticateTest.class.getName());
+
+  public void testHandleDoPost() {
+    String xmlBody =
+      "<AuthnRequest>\n" +
+      "  <Credentials>\n" +
+      "    <Username>fooUser</Username>\n" +
+      "    <Password>fooPassword</Password>\n" +
+      "  </Credentials>\n" +
+      "</AuthnRequest>";
+
+    String expectedResult =
+      "<CmResponse>\n" +
+      "  <AuthnResponse>\n" +
+      "    <Success ConnectorName=\"connector1\">\n" +
+      "      <Identity>fooUser</Identity>\n" +
+      "    </Success>\n" +
+      "    <Success ConnectorName=\"connector2\">\n" +
+      "      <Identity>fooUser</Identity>\n" +
+      "    </Success>\n" +
+      "  </AuthnResponse>\n" +
+      "</CmResponse>\n";
+    doTest(xmlBody, expectedResult, null, "fooUser", "fooPassword");
+  }
+
+  public void testWithDomain() {
+    String xmlBody =
+      "<AuthnRequest>\n" +
+      "  <Credentials>\n" +
+      "    <Username>fooUser</Username>\n" +
+      "    <Domain>fooDomain</Domain>" +
+      "    <Password>fooPassword</Password>\n" +
+      "  </Credentials>\n" +
+      "</AuthnRequest>";
+
+    String expectedResult =
+      "<CmResponse>\n" +
+      "  <AuthnResponse>\n" +
+      "    <Success ConnectorName=\"connector1\">\n" +
+      "      <Identity>fooUser</Identity>\n" +
+      "    </Success>\n" +
+      "    <Success ConnectorName=\"connector2\">\n" +
+      "      <Identity>fooUser</Identity>\n" +
+      "    </Success>\n" +
+      "  </AuthnResponse>\n" +
+      "</CmResponse>\n";
+    doTest(xmlBody, expectedResult, "fooDomain", "fooUser", "fooPassword");
+  }
+
+  public void testWithTwoConnectorNameElements() {
+    String xmlBody =
+      "<AuthnRequest>\n" +
+      "  <Connectors>\n" +
+      "    <ConnectorName>connector1</ConnectorName>\n" +
+      "    <ConnectorName>connector2</ConnectorName>\n" +
+      "  </Connectors>\n" +
+      "  <Credentials>\n" +
+      "    <Username>fooUser</Username>\n" +
+      "    <Domain>fooDomain</Domain>" +
+      "    <Password>fooPassword</Password>\n" +
+      "  </Credentials>\n" +
+      "</AuthnRequest>";
+
+    String expectedResult =
+      "<CmResponse>\n" +
+      "  <AuthnResponse>\n" +
+      "    <Success ConnectorName=\"connector1\">\n" +
+      "      <Identity>fooUser</Identity>\n" +
+      "    </Success>\n" +
+      "    <Success ConnectorName=\"connector2\">\n" +
+      "      <Identity>fooUser</Identity>\n" +
+      "    </Success>\n" +
+      "  </AuthnResponse>\n" +
+      "</CmResponse>\n";
+    doTest(xmlBody, expectedResult, "fooDomain", "fooUser", "fooPassword");
+  }
+
+  public void testWithOneConnectorNameElement() {
+    String xmlBody =
+      "<AuthnRequest>\n" +
+      "  <Connectors>\n" +
+      "    <ConnectorName>connector1</ConnectorName>\n" +
+      "  </Connectors>\n" +
+      "  <Credentials>\n" +
+      "    <Username>fooUser</Username>\n" +
+      "    <Domain>fooDomain</Domain>" +
+      "    <Password>fooPassword</Password>\n" +
+      "  </Credentials>\n" +
+      "</AuthnRequest>";
+
+    String expectedResult =
+      "<CmResponse>\n" +
+      "  <AuthnResponse>\n" +
+      "    <Success ConnectorName=\"connector1\">\n" +
+      "      <Identity>fooUser</Identity>\n" +
+      "    </Success>\n" +
+      "  </AuthnResponse>\n" +
+      "</CmResponse>\n";
+    doTest(xmlBody, expectedResult, "fooDomain", "fooUser", "fooPassword");
+  }
+
+  private void doTest(String xmlBody, String expectedResult, String domain,
+      String username, String password) {
+    LOGGER.info("xmlBody: " + xmlBody);
+    MockManager manager = MockManager.getInstance();
+    manager.setShouldVerifyIdentity(true);
+    manager.setExpectedIdentity(domain, username, password);
+    StringWriter writer = new StringWriter();
+    PrintWriter out = new PrintWriter(writer);
+    Authenticate.handleDoPost(xmlBody, manager, out);
+    out.flush();
+    StringBuffer result = writer.getBuffer();
+    LOGGER.info(result.toString());
+    LOGGER.info(expectedResult);
+    Assert.assertEquals(StringUtils.normalizeNewlines(expectedResult),
+        StringUtils.normalizeNewlines(result.toString()));
+    out.close();
+  }
+}
