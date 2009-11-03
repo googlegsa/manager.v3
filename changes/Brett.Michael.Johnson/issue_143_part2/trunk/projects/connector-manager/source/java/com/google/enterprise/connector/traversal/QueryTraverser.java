f6594e591ebbb56abfeb259de61a787895b60397
Brett.Michael.Johnson <Brett.Michael.Johnson@56db1fc5-3b1b-0410-9cff-4307dac5e09e>
2009-11-03 19:24:11 +0000

    Issue 143 - Adds SkippedDocumentException for support of 'ignored' mimetypes.
    Revision r2281 adds a new MimeTypeMap set for documents
    that should be 'ignored' (skipped).  Basically the use is:
    
    If TraversalContext.mimeTypeSupportLevel(String mimetype)
    returns -1, the document should be skipped.  Do not index
    metadata. Do not index content.
    
    At this time, support for this feature still rests with the
    connector implementation.  John and I were discussing the
    best way to implement skipping documents that fall into the
    'ignored' set.  The connector could skip them internally -
    either silently or log a message.  This would be easy for
    the Livelink connector, but difficult for the Documentum
    connector.
    
    Months ago, Jeff and Marty mentioned that customers wanted
    to know which documents were skipped and for what reason.
    From that perspective, it makes more sense for the Connector
    to throw a RepositoryDocumentException, which will skip the
    document and log the action.  From the implementation point
    of view, it is already supported by the CM, and could be
    easily implemented in the Livelink and Documentum Connectors
    with 2-4 lines of code.
    
    One downside is that RepositoryDocumentExceptions are
    logged at WARNING level.  This change creates a new subclass
    of RepositoryDocumentException, SkipDocumentException,
    that gets caught separately and logged at FINER.  Connectors
    could use it not just for 'ignored' mimetypes, but any other
    benevolent reason that a document gets skipped (perhaps some
    other advanced config option).

---
+++ changes/Brett.Michael.Johnson/issue_143_part2/trunk/projects/connector-manager/source/java/com/google/enterprise/connector/traversal/QueryTraverser.java
@@ -0,0 +1,300 @@
+// Copyright (C) 2006-2008 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.enterprise.connector.traversal;
+
+import com.google.enterprise.connector.pusher.FeedException;
+import com.google.enterprise.connector.pusher.PushException;
+import com.google.enterprise.connector.pusher.Pusher;
+import com.google.enterprise.connector.pusher.PusherFactory;
+import com.google.enterprise.connector.spi.Document;
+import com.google.enterprise.connector.spi.DocumentList;
+import com.google.enterprise.connector.spi.RepositoryDocumentException;
+import com.google.enterprise.connector.spi.RepositoryException;
+import com.google.enterprise.connector.spi.SkippedDocumentException;
+import com.google.enterprise.connector.spi.SpiConstants;
+import com.google.enterprise.connector.spi.TraversalContext;
+import com.google.enterprise.connector.spi.TraversalContextAware;
+import com.google.enterprise.connector.spi.TraversalManager;
+import com.google.enterprise.connector.spi.Value;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Traverser for a repository implemented using a TraversalManager
+ */
+public class QueryTraverser implements Traverser {
+  private static final Logger LOGGER =
+      Logger.getLogger(QueryTraverser.class.getName());
+
+  private final PusherFactory pusherFactory;
+  private final TraversalManager queryTraversalManager;
+  private final TraversalStateStore stateStore;
+  private final String connectorName;
+  private final TraversalContext traversalContext;
+
+  // Synchronize access to cancelWork.
+  private final Object cancelLock = new Object();
+  private boolean cancelWork = false;
+
+  public QueryTraverser(PusherFactory pusherFactory,
+      TraversalManager traversalManager, TraversalStateStore stateStore,
+      String connectorName, TraversalContext traversalContext) {
+    this.pusherFactory = pusherFactory;
+    this.queryTraversalManager = traversalManager;
+    this.stateStore = stateStore;
+    this.connectorName = connectorName;
+    this.traversalContext = traversalContext;
+    if (queryTraversalManager instanceof TraversalContextAware) {
+      TraversalContextAware contextAware =
+          (TraversalContextAware)queryTraversalManager;
+      try {
+        contextAware.setTraversalContext(traversalContext);
+      } catch (Exception e) {
+        LOGGER.log(Level.WARNING, "Unable to set TraversalContext", e);
+      }
+    }
+  }
+
+  //@Override
+  public void cancelBatch() {
+    synchronized(cancelLock) {
+      cancelWork = true;
+    }
+    LOGGER.fine("Cancelling traversal for connector " + connectorName);
+  }
+
+  public boolean isCancelled() {
+    synchronized(cancelLock) {
+      return cancelWork;
+    }
+  }
+
+  //@Override
+  public BatchResult runBatch(BatchSize batchSize) {
+    final long timeoutTime = System.currentTimeMillis()
+      + traversalContext.traversalTimeLimitSeconds() * 1000;
+
+    if (isCancelled()) {
+        LOGGER.warning("Attempting to run a cancelled QueryTraverser");
+      return new BatchResult(TraversalDelayPolicy.ERROR, 0);
+    }
+    try {
+      queryTraversalManager.setBatchHint(batchSize.getHint());
+    } catch (RepositoryException e) {
+      LOGGER.log(Level.WARNING, "Unable to set batch hint", e);
+    }
+
+    String connectorState;
+    try {
+      if (stateStore != null) {
+        connectorState = stateStore.getTraversalState();
+      } else {
+        throw new IllegalStateException("null TraversalStateStore");
+      }
+    } catch (IllegalStateException ise) {
+      // We get here if the ConnectorStateStore for connector is disabled.
+      // That happens if the connector was deleted while we were asleep.
+      // Our connector seems to have been deleted.  Don't process a batch.
+      LOGGER.finer("Halting traversal..." + ise.getMessage());
+      return new BatchResult(TraversalDelayPolicy.ERROR, 0);
+    }
+
+    DocumentList resultSet = null;
+    if (connectorState == null) {
+      try {
+        LOGGER.finer("Starting traversal...");
+        resultSet = queryTraversalManager.startTraversal();
+      } catch (Exception e) {
+        LOGGER.log(Level.WARNING, "startTraversal threw exception: ", e);
+        return new BatchResult(TraversalDelayPolicy.ERROR, 0);
+      }
+    } else {
+      try {
+        LOGGER.finer("Resuming traversal...");
+        resultSet = queryTraversalManager.resumeTraversal(connectorState);
+      } catch (Exception e) {
+        LOGGER.log(Level.WARNING, "resumeTraversal threw exception: ", e);
+        return new BatchResult(TraversalDelayPolicy.ERROR, 0);
+      }
+    }
+
+    // If the traversal returns null, that means that the repository has
+    // no new content to traverse.
+    if (resultSet == null) {
+      LOGGER.finer("Result set is NULL, no documents returned for traversal.");
+      return new BatchResult(TraversalDelayPolicy.POLL, 0);
+    }
+
+    Pusher pusher = null;
+    BatchResult result = null;
+    int counter = 0;
+    try {
+      // Get a Pusher for feeding the returned Documents.
+      pusher = pusherFactory.newPusher(connectorName);
+
+      while (counter < batchSize.getMaximum()) {
+        if (Thread.currentThread().isInterrupted() || isCancelled()) {
+          LOGGER.fine("Traversal for connector " + connectorName
+                      + " has been interrupted...breaking out of batch run.");
+          break;
+        }
+        if (System.currentTimeMillis() >= timeoutTime) {
+          LOGGER.fine("Traversal for connector " + connectorName
+              + " is completing due to time limit.");
+          break;
+        }
+
+        Document nextDocument = null;
+        String docid = null;
+        try {
+          LOGGER.finer("Pulling next document from connector " + connectorName);
+          nextDocument = resultSet.nextDocument();
+          if (nextDocument == null) {
+            break;
+          } else {
+            // Since there are a couple of places below that could throw
+            // exceptions but not exit the while loop, the counter should be
+            // incremented here to insure it represents documents returned from
+            // the list.  Note the call to nextDocument() could also throw a
+            // RepositoryDocumentException signaling a skipped document in which
+            // case the call will not be counted against the batch maximum.
+            counter++;
+            // Fetch DocId to use in messages.
+            try {
+              docid = Value.getSingleValueString(nextDocument,
+                                                 SpiConstants.PROPNAME_DOCID);
+            } catch (IllegalArgumentException e1) {
+                LOGGER.fine("Unable to get document id for document ("
+                            + nextDocument + "): " + e1.getMessage());
+            } catch (RepositoryException e1) {
+                LOGGER.fine("Unable to get document id for document ("
+                            + nextDocument + "): " + e1.getMessage());
+            }
+          }
+          LOGGER.finer("Sending document (" + docid + ") from connector "
+              + connectorName + " to Pusher");
+          pusher.take(nextDocument);
+        } catch (SkippedDocumentException e) {
+          // Skip this document.  Proceed on to the next one.
+          LOGGER.log(Level.FINER, "Skipping document (" + docid
+              + ") from connector " + connectorName, e);
+        } catch (RepositoryDocumentException e) {
+          // Skip individual documents that fail.  Proceed on to the next one.
+          LOGGER.log(Level.WARNING, "Skipping document (" + docid
+              + ") from connector " + connectorName, e);
+        } catch (RuntimeException e) {
+          // Skip individual documents that fail.  Proceed on to the next one.
+          LOGGER.log(Level.WARNING, "Skipping document (" + docid
+              + ") from connector " + connectorName, e);
+        }
+      }
+      // No more documents. Wrap up any accumulated feed data and send it off.
+      if (!isCancelled()) {
+        pusher.flush();
+      }
+    } catch (OutOfMemoryError e) {
+      pusher.cancel();
+      System.runFinalization();
+      System.gc();
+      result = new BatchResult(TraversalDelayPolicy.ERROR, 0);
+      try {
+        LOGGER.severe("Out of JVM Heap Space.  Will retry later.");
+        LOGGER.log(Level.FINEST, e.getMessage(), e);
+      } catch (Throwable t) {
+        // OutOfMemory state may prevent us from logging the error.
+        // Don't make matters worse by rethrowing something meaningless.
+      }
+    } catch (RepositoryException e) {
+      // Drop the entire batch on the floor.  Do not call checkpoint
+      // (as there is a discrepancy between what the Connector thinks
+      // it has fed, and what actually has been pushed).
+      LOGGER.log(Level.SEVERE, "Repository Exception during traversal.", e);
+      result = new BatchResult(TraversalDelayPolicy.ERROR, 0);
+    } catch (PushException e) {
+      LOGGER.log(Level.SEVERE, "Push Exception during traversal.", e);
+      // Drop the entire batch on the floor.  Do not call checkpoint
+      // (as there is a discrepancy between what the Connector thinks
+      // it has fed, and what actually has been pushed).
+      result = new BatchResult(TraversalDelayPolicy.ERROR, 0);
+    } catch (FeedException e) {
+      LOGGER.log(Level.SEVERE, "Feed Exception during traversal.", e);
+      // Drop the entire batch on the floor.  Do not call checkpoint
+      // (as there is a discrepancy between what the Connector thinks
+      // it has fed, and what actually has been pushed).
+      result = new BatchResult(TraversalDelayPolicy.ERROR, 0);
+    } catch (Throwable t) {
+      LOGGER.log(Level.SEVERE, "Uncaught Exception during traversal.", t);
+      // Drop the entire batch on the floor.  Do not call checkpoint
+      // (as there is a discrepancy between what the Connector thinks
+      // it has fed, and what actually has been pushed).
+      result = new BatchResult(TraversalDelayPolicy.ERROR, 0);
+   } finally {
+      // If we have cancelled the work, abandon the batch.
+      if (isCancelled()) {
+        result = new BatchResult(TraversalDelayPolicy.ERROR, 0);
+      }
+
+      // Checkpoint completed work as well as skip past troublesome documents
+      // (e.g. documents that are too large and will always fail).
+      if ((result == null) && (checkpointAndSave(resultSet) == null)) {
+        // Unable to get a checkpoint, so wait a while, then retry batch.
+        result = new BatchResult(TraversalDelayPolicy.ERROR, 0);
+      }
+    }
+    if (result == null) {
+      result = new BatchResult(TraversalDelayPolicy.IMMEDIATE, counter);
+    } else if (pusher != null) {
+      // We are returning an error from this batch. Cancel any feed that
+      // might be in progress.
+      pusher.cancel();
+    }
+    return result;
+  }
+
+  private String checkpointAndSave(DocumentList pm) {
+    String connectorState = null;
+    LOGGER.finest("Checkpointing for connector " + connectorName + " ...");
+    try {
+      connectorState = pm.checkpoint();
+    } catch (RepositoryException re) {
+      // If checkpoint() throws RepositoryException, it means there is no
+      // new checkpoint.
+      return null;
+    } catch (Exception e) {
+      // If checkpoint() throws some general Exception, it is probably
+      // an older connector that doesn't understand the newer empty
+      // DocumentList and Exception handling from runBatch() model.
+      return null;
+    }
+    try {
+      if (connectorState != null) {
+        if (stateStore != null) {
+          stateStore.storeTraversalState(connectorState);
+        } else {
+          throw new IllegalStateException("null TraversalStateStore");
+        }
+        LOGGER.finest("...checkpoint " + connectorState + " created.");
+      }
+      return connectorState;
+    } catch (IllegalStateException ise) {
+      // We get here if the ConnectorStateStore for connector is disabled.
+      // That happens if the connector was deleted while we were working.
+      // Our connector seems to have been deleted.  Don't save a checkpoint.
+      LOGGER.finest("...checkpoint " + connectorState + " discarded.");
+    }
+    return null;
+  }
+}
