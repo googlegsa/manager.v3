0df874107a4e8b87db54eb93fdcaca4a7493fab2
Brett.Michael.Johnson <Brett.Michael.Johnson@56db1fc5-3b1b-0410-9cff-4307dac5e09e>
2009-11-05 20:23:56 +0000

    Issue 143: Rename 'ignored' set of mime-types to 'excluded' mime-types.
    This is more obvious and in-line with the wording in the GSA configuration.
    No logic changes were made - just a name change.

---
+++ changes/Brett.Michael.Johnson/issue_143_part3/trunk/projects/connector-manager/source/java/com/google/enterprise/connector/spi/SkippedDocumentException.java
@@ -0,0 +1,42 @@
+// Copyright (C) 2009 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.enterprise.connector.spi;
+
+/**
+ * Thrown for documents that should be skipped for non-error reasons.
+ * The Connector Manager skips over documents that throw
+ * RepositoryDocumentExceptions, proceeding on to the next document.
+ * Although the parent class RepositoryDocumentExceptions are logged at
+ * WARNING level, due to their potential error conditions, these
+ * SkippedDocumentExceptions are logged at FINER level as they can be
+ * considered expected per-document events.
+ * It is the responsibility of the Connector to construct a checkpoint
+ * that skips over the offending Document if DocumentList.checkpoint()
+ * is called after SkippedDocumentException is thrown.
+ */
+/* TODO (bmj): This is a temporary solution and should be replaced.
+ * It uses Exceptions for non-exceptional cases.
+ */
+public class SkippedDocumentException extends RepositoryDocumentException {
+  /**
+   * Constructs a SkippedDocumentException with a supplied message.
+   * The message should indicate the reason this document was skipped.
+   *
+   * @param message the message.
+   */
+  public SkippedDocumentException(String message) {
+    super(message);
+  }
+}
