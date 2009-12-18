e885df3ce712d81ca251a69c047598631a0461de
mgronber <mgronber@56db1fc5-3b1b-0410-9cff-4307dac5e09e>
2009-12-18 02:47:28 +0000

    Fix for Issue 204.
    Note the stack trace pasted into the issue report is actually a problem with the
    customer's Connector.  It appears the Connector is not properly escaping the
    config values when putting together the formSnippet.  See Issue 186 for details.
    
    However, Issue 204 exposed a problem when the CDATA end marker ']]>' is actually
    embedded in the formSnippet.  Because the formSnippet is surrounded by a CDATA
    section, this can cause the Admin Console to truncate the XML for the
    formSnippet causing an incomplete element to be extracted and a
    NullPointerException on the Search Appliance.
    
    
    Tests Passed:
    
      - All Unit tests.
      - Tested with TestConnector and GSA using string from the issue report.
        String was returned when Connector was edited.
    
    
    ChangeLog:
    
      M java/com/google/enterprise/connector/servlet/ServletUtil.java
        - Fixed the removeNestedMarkers() utility to only work with pairs of
          markers. TODO, this could still remove the wrong end marker since the
          matching of the end marker doesn't work from the back of the form.
        - Added another utility to escape the CDATA end marker so it won't be mistaken
          for the real end marker when the form is wrapped.
    
      M java/com/google/enterprise/connector/servlet/ConnectorManagerGetServlet.java
        - Added additional call to escape any CDATA end markers so they don't truncate
          the form on the Search Appliance side.
    
      M javatests/com/google/enterprise/connector/servlet/ServletUtilTest.java
        - Added related tests.

---
+++ changes/mgronber/fix-issue-204/trunk/projects/security-manager/source/javatests/com/google/enterprise/connector/security/connectors/simplecookie/SimpleCookieIdentityConnectorTest.java
@@ -0,0 +1,59 @@
+// Copyright 2008 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.enterprise.connector.security.connectors.simplecookie;
+
+import com.google.enterprise.connector.common.SecurityManagerTestCase;
+import com.google.enterprise.connector.spi.AuthenticationResponse;
+import com.google.enterprise.connector.spi.SecAuthnIdentity;
+import com.google.enterprise.connector.security.identity.DomainCredentials;
+import com.google.enterprise.connector.security.connectors.simplecookie.CookieIdentityExtractor;
+import com.google.enterprise.connector.security.connectors.simplecookie.RegexCookieIdentityExtractor;
+import com.google.enterprise.connector.security.connectors.simplecookie.SimpleCookieIdentityConnector;
+
+import org.springframework.mock.web.MockHttpSession;
+
+import javax.servlet.http.Cookie;
+
+/**
+ * Tests for the {@link SimpleCookieIdentityConnector} class.
+ */
+public class SimpleCookieIdentityConnectorTest extends SecurityManagerTestCase {
+
+  public void testAuthenticate() {
+    String cookieName = "arglebargle";
+    String idCookieName = "xyzzy";
+    String pattern = "username=(.*)";
+    CookieIdentityExtractor c = new RegexCookieIdentityExtractor(pattern);
+    SimpleCookieIdentityConnector s =
+        new SimpleCookieIdentityConnector(cookieName, idCookieName, c);
+    runOneAuthenticationTest(cookieName, idCookieName, s, "username=fred", "fred");
+    runOneAuthenticationTest(cookieName, idCookieName, s, "randomness", null);
+  }
+
+  private void runOneAuthenticationTest(String cookieName, String idCookieName,
+      SimpleCookieIdentityConnector s, String cookieValue, String expectedIdentity) {
+    SecAuthnIdentity id = DomainCredentials.dummy(new MockHttpSession());
+    id.addCookie(new Cookie(cookieName, cookieValue));
+    AuthenticationResponse r = s.authenticate(id);
+    if (expectedIdentity != null) {
+    assertTrue(r.isValid());
+    Cookie c = id.getCookieNamed(idCookieName);
+    assertNotNull(c);
+    assertEquals(expectedIdentity, c.getValue());
+    } else {
+      assertFalse(r.isValid());
+    }
+  }
+}
