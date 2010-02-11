408b959621572453468213164ca7988b79be9a66
John Felton <johnfelton@google.com>
2010-02-11 00:45:41 +0000

    Merge John and Eric's code.

---
+++ projects/sharepoint-proto/source/javatests/com/google/enterprise/connector/sp2c_migration/MockSharepointServer.java
@@ -0,0 +1,49 @@
+package com.google.enterprise.connector.sp2c_migration;
+
+
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MockSharepointServer implements SharepointSite {
+  
+  private FolderTraverser traverser;
+  
+  public MockSharepointServer(String sharePointUrl, String loginName, String password) {
+    traverser = new FolderTraverser(sharePointUrl);
+  }
+
+  @Override
+  public InputStream getDocumentContent(Document document) throws Exception {
+    return traverser.getDocumentContents(document.getId());
+  }
+
+  @Override
+  public List<Document> getDocuments(Folder rootfolder) throws Exception {
+    return traverser.getDocuments();
+  }
+
+  @Override
+  public List<Folder> getFolders(Folder rootfolder) throws Exception {
+    return traverser.getFolders();
+  }
+
+  @Override
+  public String getId() {
+    return null;
+  }
+
+  @Override
+  public List<Folder> getRootFolders() throws Exception {
+    List<Folder> roots = new ArrayList<Folder>();
+    roots.add(traverser.getRootFolder());
+    return roots;
+  }
+
+  @Override
+  public String getUrl() {
+    return "Site URL";
+  }
+
+}
