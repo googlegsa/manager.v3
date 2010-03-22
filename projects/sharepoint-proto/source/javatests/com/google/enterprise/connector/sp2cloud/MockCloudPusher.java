a20fd575e2daa34e333be0f36ceafb9921e7d60a
Eric Strellis <strellis@google.com>
2010-03-22 21:50:25 +0000

    OAuth + linking to allow owners and writers with existing permission

--- projects/sharepoint-proto/source/javatests/com/google/enterprise/connector/sp2cloud/MockCloudPusher.java
+++ projects/sharepoint-proto/source/javatests/com/google/enterprise/connector/sp2cloud/MockCloudPusher.java
@@ -1,21 +1,22 @@
-//package com.google.enterprise.connector.sp2cloud;
-//
-//import com.google.enterprise.connector.sp2c_migration.Document;
-//import com.google.enterprise.connector.sp2c_migration.Folder;
-//
-//import java.io.InputStream;
-//
-//public class MockCloudPusher implements CloudPusher {
-//
-//  @Override
-//  public void pushDocument(Folder parent, Document document, InputStream inputStream)
-//      throws Exception {
-//    System.out.println("Document - name: '" + document.getName() + "'  id: " + document.getId() + "  parent id: " + document.getParentId() + "  owner: " + document.getOwner());
-//  }
-//
-//  @Override
-//  public void pushFolder(Folder parent, Folder folder) throws Exception {
-//    System.out.println("Folder - name: '" + folder.getName() + "'  id: " + folder.getId() + "  parent id: " + folder.getParentId() + "  owner: " + folder.getOwner());
-//  }
-//
-//}
+package com.google.enterprise.connector.sp2cloud;
+
+import com.google.enterprise.connector.sp2c_migration.Document;
+import com.google.enterprise.connector.sp2c_migration.Folder;
+
+import java.io.InputStream;
+import java.util.List;
+
+public class MockCloudPusher implements CloudPusher {
+
+  @Override
+  public void pushDocument(Folder parent, Document document,  List<CloudAce> cloudAcl, 
+      InputStream inputStream) throws Exception {
+    System.out.println("Document - name: '" + document.getName() + "'  id: " + document.getId() + "  parent id: " + document.getParentId() + "  owner: " + document.getOwner());
+  }
+
+  @Override
+  public void pushFolder(Folder parent, Folder folder, List<CloudAce> cloudAcl) throws Exception {
+    System.out.println("Folder - name: '" + folder.getName() + "'  id: " + folder.getId() + "  parent id: " + folder.getParentId() + "  owner: " + folder.getOwner());
+  }
+
+}
