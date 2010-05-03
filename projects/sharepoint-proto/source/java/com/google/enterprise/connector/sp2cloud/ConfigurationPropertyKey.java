a503568c2911b0dd21f8286b3d7372fbcefebd99
Eric Strellis <strellis@google.com>
2010-05-03 18:37:00 +0000

    Add sp2c configuration support classes

---
+++ projects/sharepoint-proto/source/java/com/google/enterprise/connector/sp2cloud/ConfigurationPropertyKey.java
@@ -0,0 +1,16 @@
+// Copyright 2010 Google Inc. All Rights Reserved.
+
+package com.google.enterprise.connector.sp2cloud;
+
+enum ConfigurationPropertyKey {
+  // TODO: add convert, cloud_root_folder, sp_root_folder
+  property_file_name,
+  cloud_root_folder,
+  cloud_admin_id,
+  cloud_consumer_key,
+  cloud_consumer_secret,
+  sp_admin_id,
+  sp_domain,
+  sp_url,
+  sp_admin_password
+}
