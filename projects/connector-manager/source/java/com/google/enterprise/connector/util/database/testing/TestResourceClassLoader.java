e2df6a982c26c37fb68dc8b46a12b510899d8456
Brett Michael Johnson <Brett.Michael.Johnson@gmail.com>
2010-10-26 02:57:40 +0000

    Adds implemenations for SPI interfaces LocalDatabase, ConnectorPersistentStore, and ConnectorPersistentStoreAware.
    http://codereview.appspot.com/2282041/
    
    Change Log:
    ----------
    M   projects/connector-manager/source/java/com/google/enterprise/connector/instantiator/ConnectorCoordinatorImplFactory.java
    M   projects/connector-manager/source/java/com/google/enterprise/connector/instantiator/ConnectorCoordinatorImpl.java
    A   projects/connector-manager/source/java/com/google/enterprise/connector/database/ConnectorPersistentStoreFactory.java
    M   projects/connector-manager/etc/applicationContext.xml
        - ConnectorPersistentStoreFactory is used to manufacture instances of
          ConnectorPersistentStore for each Connector instance.
    
    A   projects/connector-manager/source/java/com/google/enterprise/connector/database/LocalDatabaseImpl.java
        - Implementation of the SPI LocalDatabase interface.
    
    A   projects/connector-manager/source/javatests/com/google/enterprise/connector/database/LocalDatabaseTest.java
        - LocalDatabaseImpl unit tests.
    
    A   projects/connector-manager/source/java/com/google/enterprise/connector/util/database/testing/TestLocalDatabase.java
    A   projects/connector-manager/source/java/com/google/enterprise/connector/util/database/testing/TestResourceClassLoader.java
    A   projects/connector-manager/source/java/com/google/enterprise/connector/util/database/testing/TestJdbcDatabase.java
        - Test classes useful to Connector developers (and the CM).

---
+++ projects/connector-manager/source/java/com/google/enterprise/connector/util/database/testing/TestResourceClassLoader.java
@@ -0,0 +1,53 @@
+// Copyright 2010 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.enterprise.connector.util.database.testing;
+
+import com.google.enterprise.connector.spi.DatabaseResourceBundle;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * A {@code ClassLoader} that looks for resources relative to the
+ * specified resources directory, or the current working directory,
+ * if none is specified.
+ * <p>
+ * This only overrides the 1 method we use to locate
+ * {@link DatabaseResourceBundles}, so it is not a good candidate
+ * to stand on its own.
+ */
+public class TestResourceClassLoader extends ClassLoader {
+  private final File resourceDir;
+
+  TestResourceClassLoader(File resourceDir) {
+    this.resourceDir = (resourceDir == null) ?
+        new File(System.getProperty("user.dir")) : resourceDir;
+  }
+
+  @Override
+  public URL getResource(String name) {
+    try {
+      File file = new File(resourceDir, name);
+      if (file.exists() && file.isFile()) {
+        return file.toURI().toURL();
+      }
+    } catch (MalformedURLException e) {
+      // Fall through and look on classpath.
+    }
+    return this.getClass().getClassLoader().getResource(name);
+  }
+}
+
