9dc8dd6fe0620afcdf9cc50e3b7688740a285801
Brett Michael Johnson <Brett.Michael.Johnson@gmail.com>
2011-04-14 02:32:28 +0000

    Merge IOExceptionHelper and FakeDocumentSnapshotRepositoryMonitorManager change from trunk; Delete ConnectorFieldsTest now that ConnectorFields has been removed.

--- projects/connector-manager/source/java/com/google/enterprise/connector/util/diffing/CheckpointAndChangeQueue.java
+++ projects/connector-manager/source/java/com/google/enterprise/connector/util/diffing/CheckpointAndChangeQueue.java
@@ -15,6 +15,7 @@
 package com.google.enterprise.connector.util.diffing;
 
 import com.google.common.base.Charsets;
+import com.google.enterprise.connector.util.IOExceptionHelper;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -238,7 +239,7 @@ public class CheckpointAndChangeQueue {
         JSONObject json = new JSONObject(contents);
         return json;
       } catch (JSONException e) {
-        throw newIOException("Failed reading persisted JSON queue.", e);
+        throw IOExceptionHelper.newIOException("Failed reading persisted JSON queue.", e);
       }
     }
   }
@@ -264,7 +265,7 @@ public class CheckpointAndChangeQueue {
       try {
         queueJson.write(writer);
       } catch (JSONException e) {
-        throw newIOException("Failed writing recovery file.", e);
+        throw IOExceptionHelper.newIOException("Failed writing recovery file.", e);
       }
       writer.write(SENTINAL);
       writer.flush();
@@ -287,7 +288,7 @@ public class CheckpointAndChangeQueue {
       JSONObject jsonMonPoints = json.getJSONObject(MONITOR_STATE_JSON_TAG);
       monitorPoints = new MonitorRestartState(jsonMonPoints);
     } catch (JSONException e) {
-      throw newIOException("Failed reading persisted JSON queue.", e);
+      throw IOExceptionHelper.newIOException("Failed reading persisted JSON queue.", e);
     }
   }
 
@@ -506,16 +507,5 @@ public class CheckpointAndChangeQueue {
       String errmsg = "Failed to delete: " + persistDir.getAbsolutePath();
       LOG.severe(errmsg);
     }
-  }
-
-  /**
-   * Makes a new IOException with the supplied
-   * message and cause in a manner that is supported by
-   * java 5.
-   */
-  private static IOException newIOException(String msg, Throwable cause) {
-    IOException result = new IOException(msg);
-    result.initCause(cause);
-    return result;
   }
 }
--- projects/connector-manager/source/javatests/com/google/enterprise/connector/util/diffing/DiffingConnectorTraversalManagerTest.java
+++ projects/connector-manager/source/javatests/com/google/enterprise/connector/util/diffing/DiffingConnectorTraversalManagerTest.java
@@ -20,6 +20,7 @@ import com.google.enterprise.connector.spi.RepositoryException;
 import com.google.enterprise.connector.spi.SpiConstants;
 import com.google.enterprise.connector.spi.TraversalContext;
 import com.google.enterprise.connector.spi.Value;
+import com.google.enterprise.connector.util.diffing.testing.FakeDocumentSnapshotRepositoryMonitorManager;
 import com.google.enterprise.connector.util.diffing.testing.FakeTraversalContext;
 
 import junit.framework.TestCase;
