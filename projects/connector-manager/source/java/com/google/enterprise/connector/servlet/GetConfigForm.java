5c6577990cde8be1bcb8d5dfc9ab7b2fdb85d399
Brett Michael Johnson <Brett.Michael.Johnson@gmail.com>
2011-08-25 21:26:34 +0000

    Branch 2.8.x: Merge revision r2754 from trunk - Pass the connectorInstance.xml with the connector's configuration to/from the GSA for advanced configuration editing.

--- projects/connector-manager/source/java/com/google/enterprise/connector/servlet/GetConfigForm.java
+++ projects/connector-manager/source/java/com/google/enterprise/connector/servlet/GetConfigForm.java
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-
 package com.google.enterprise.connector.servlet;
 
 import com.google.enterprise.connector.instantiator.InstantiatorException;
@@ -31,11 +30,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
 /**
- * Admin servlet to get the config form for
- * a given connector type and language.
- *
+ * Admin servlet to get the config form for a given connector type and language.
  */
 public class GetConfigForm extends HttpServlet {
   private static final Logger LOGGER =
@@ -58,7 +54,6 @@ public class GetConfigForm extends HttpServlet {
       return;
     }
 
-    ConnectorMessageCode status = new ConnectorMessageCode();
     String connectorTypeName =
         req.getParameter(ServletUtil.XMLTAG_CONNECTOR_TYPE);
     res.setContentType(ServletUtil.MIMETYPE_XML);
@@ -67,14 +62,6 @@ public class GetConfigForm extends HttpServlet {
 
     NDC.push("Config " + connectorTypeName);
     try {
-
-      if (connectorTypeName == null || connectorTypeName.length() < 1) {
-        ServletUtil.writeResponse(
-            out, ConnectorMessageCode.RESPONSE_NULL_CONNECTOR_TYPE);
-        LOGGER.log(Level.WARNING, ServletUtil.LOG_RESPONSE_NULL_CONNECTOR_TYPE);
-        return;
-      }
-
       String language = req.getParameter(ServletUtil.QUERY_PARAM_LANG);
       if (language == null || language.length() < 1) {
         LOGGER.log(Level.WARNING, "Language is null");
@@ -82,23 +69,8 @@ public class GetConfigForm extends HttpServlet {
       }
 
       Manager manager = Context.getInstance().getManager();
+      handleDoGet(connectorTypeName, language, manager, out);
 
-      try {
-        ConfigureResponse configResponse =
-            manager.getConfigForm(connectorTypeName, language);
-        handleDoGet(configResponse, status, out);
-      } catch (ConnectorTypeNotFoundException e) {
-        status = new ConnectorMessageCode(
-            ConnectorMessageCode.EXCEPTION_CONNECTOR_TYPE_NOT_FOUND,
-            connectorTypeName);
-        ServletUtil.writeResponse(out, status);
-        LOGGER.log(Level.WARNING,
-            ServletUtil.LOG_EXCEPTION_CONNECTOR_TYPE_NOT_FOUND, e);
-      } catch (InstantiatorException e) {
-        status.setMessageId(ConnectorMessageCode.EXCEPTION_INSTANTIATOR);
-        ServletUtil.writeResponse(out, status);
-        LOGGER.log(Level.WARNING, ServletUtil.LOG_EXCEPTION_INSTANTIATOR, e);
-      }
     } finally {
       out.close();
       NDC.clear();
@@ -121,12 +93,41 @@ public class GetConfigForm extends HttpServlet {
   /**
    * Handler for doGet in order to do unit tests.
    *
-   * @param configResponse ConfigureResponse
-   * @param status ConnectorMessageCode
+   * @param connectorTypeName The name of a connector implementation - it should
+   *        be one that this manager knows about (one that would be returned by
+   *        a call to getConnectorTypes()).
+   * @param language A locale string, such as "en" or "fr_CA" which the
+   *        implementation may use to produce appropriate descriptions and
+   *        messages
+   * @param manager the Connector Manager
    * @param out PrintWriter where the response is written
    */
-  public static void handleDoGet(ConfigureResponse configResponse,
-      ConnectorMessageCode status, PrintWriter out) {
-    ConnectorManagerGetServlet.writeConfigureResponse(out, status, configResponse);
+  public static void handleDoGet(String connectorTypeName, String language,
+      Manager manager, PrintWriter out) {
+    ConnectorMessageCode status = new ConnectorMessageCode();
+
+    if (connectorTypeName == null || connectorTypeName.length() < 1) {
+      status.setMessageId(ConnectorMessageCode.RESPONSE_NULL_CONNECTOR_TYPE);
+      ServletUtil.writeResponse(out, status);
+      LOGGER.log(Level.WARNING, ServletUtil.LOG_RESPONSE_NULL_CONNECTOR_TYPE);
+      return;
+    }
+
+    try {
+      ConfigureResponse configResponse =
+          manager.getConfigForm(connectorTypeName, language);
+      ConnectorManagerGetServlet.writeConfigureResponse(out, status, configResponse);
+    } catch (ConnectorTypeNotFoundException e) {
+      status = new ConnectorMessageCode(
+          ConnectorMessageCode.EXCEPTION_CONNECTOR_TYPE_NOT_FOUND,
+          connectorTypeName);
+      ServletUtil.writeResponse(out, status);
+      LOGGER.log(Level.WARNING,
+          ServletUtil.LOG_EXCEPTION_CONNECTOR_TYPE_NOT_FOUND, e);
+    } catch (InstantiatorException e) {
+      status.setMessageId(ConnectorMessageCode.EXCEPTION_INSTANTIATOR);
+      ServletUtil.writeResponse(out, status);
+      LOGGER.log(Level.WARNING, ServletUtil.LOG_EXCEPTION_INSTANTIATOR, e);
+    }
   }
 }
