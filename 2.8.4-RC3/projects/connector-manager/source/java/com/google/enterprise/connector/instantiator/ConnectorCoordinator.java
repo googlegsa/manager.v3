e0a6b5a04b0201a87a23d81669c1651537e67951
Brett Michael Johnson <Brett.Michael.Johnson@gmail.com>
2012-02-10 22:32:08 +0000

    Tagging 2.8.4-RC3 build

---
+++ 2.8.4-RC3/projects/connector-manager/source/java/com/google/enterprise/connector/instantiator/ConnectorCoordinator.java
@@ -0,0 +1,257 @@
+// Copyright 2009 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.enterprise.connector.instantiator;
+
+import com.google.common.annotations.VisibleForTesting;
+import com.google.enterprise.connector.persist.ConnectorExistsException;
+import com.google.enterprise.connector.persist.ConnectorNotFoundException;
+import com.google.enterprise.connector.scheduler.Schedule;
+import com.google.enterprise.connector.spi.AuthenticationManager;
+import com.google.enterprise.connector.spi.AuthorizationManager;
+import com.google.enterprise.connector.spi.ConfigureResponse;
+import com.google.enterprise.connector.spi.TraversalManager;
+import com.google.enterprise.connector.traversal.Traverser;
+
+import java.util.Locale;
+
+/**
+ * Management interface for a connector instance.
+ * <p>
+ * This interface provides a single point of control to allow for proper
+ * synchronization between concurrent
+ * <OL>
+ * <LI>Configuration operations
+ * <LI>Running of traversals
+ * </OL>
+ * <p>
+ * Note that a {@link ConnectorCoordinator} may be created with a name but not a
+ * complete full configuration for purposes of providing synchronization during
+ * the creation process. The configuration can be specified using
+ * {@link #setConnectorConfiguration(TypeInfo, Configuration, Locale, boolean)}
+ * The {@link #exists()} method will return false for a
+ * {@link ConnectorCoordinator} without a complete configuration. In addition
+ * many of the methods in this interface will throw a
+ * {@link ConnectorNotFoundException} if the {@link ConnectorCoordinator} does
+ * not have a complete configuration.
+ * <p>
+ * It is expected the caller will guarantee that each
+ * {@link ConnectorCoordinator} in the system has a unique name.
+ */
+@VisibleForTesting
+public interface ConnectorCoordinator {
+  /**
+   * Returns true if this {@link ConnectorCoordinator} holds a
+   * configured usable connector instance. This function is for reporting
+   * purposes only. Concurrent operations by other threads can invalidate
+   * the result of calling this quickly so in code such as
+   * <pre>
+   * if (c.exists()) {
+   *   doSomething();
+   * }
+   * </pre>
+   * The {@link ConnectorCoordinator} may not exist when <code>doSomething()
+   * </code> actually runs.
+   */
+  public boolean exists();
+
+  /**
+   * Returns the name for this {@link ConnectorCoordinator}.
+   */
+  public String getConnectorName();
+
+  /**
+   * Removes this {@link ConnectorCoordinator} from the system. After this
+   * returns {@link ConnectorCoordinator#exists()} will return false until
+   * someone re-creates the connector configuration by calling
+   * {@link #setConnectorConfiguration(TypeInfo, Configuration, Locale, boolean)}.
+   * This is a noop if this {@link ConnectorCoordinator} does not exist.
+   */
+  public void removeConnector();
+
+  /**
+   * Returns the {@link AuthenticationManager} for this
+   * {@link ConnectorCoordinator}
+   *
+   * @throws ConnectorNotFoundException if this {@link ConnectorCoordinator}
+   *         does not exist.
+   * @throws InstantiatorException if unable to instantiate the requested
+   *         {@link AuthenticationManager}
+   */
+  public AuthenticationManager getAuthenticationManager()
+      throws ConnectorNotFoundException, InstantiatorException;
+
+  /**
+   * Returns the {@link AuthorizationManager} for this
+   * {@link ConnectorCoordinator}
+   *
+   * @throws ConnectorNotFoundException if this {@link ConnectorCoordinator}
+   *         does not exist.
+   * @throws InstantiatorException if unable to instantiate the requested
+   *         {@link AuthorizationManager}
+   */
+  public AuthorizationManager getAuthorizationManager()
+      throws ConnectorNotFoundException, InstantiatorException;
+
+  /**
+   * Returns the {@link TraversalManager} for this
+   * {@link ConnectorCoordinator}
+   *
+   * @throws ConnectorNotFoundException if this {@link ConnectorCoordinator}
+   *         does not exist.
+   * @throws InstantiatorException if unable to instantiate the requested
+   *         {@link Traverser}
+   */
+  @VisibleForTesting
+  public TraversalManager getTraversalManager()
+      throws ConnectorNotFoundException, InstantiatorException;
+
+  /**
+   * Returns {@link ConfigureResponse} with a configuration form or a
+   * message indicating the problem.
+    *
+   * @throws ConnectorNotFoundException if this {@link ConnectorCoordinator}
+   *         does not exist.
+   * @throws InstantiatorException if unable to instantiate the support
+   *         classes needed to construct the {@link ConfigureResponse}
+   */
+  public ConfigureResponse getConfigForm(Locale locale)
+      throws ConnectorNotFoundException, InstantiatorException;
+
+  /**
+   * Restarts the traversal for this {@link ConnectorCoordinator} by
+   * <OL>
+   * <LI> Canceling the current traversal,
+   * <LI> Clearing the current checkpoint.
+   * <LI> Starting the traversal again.
+   * </OL>
+   *
+   * @throws ConnectorNotFoundException if this {@link ConnectorCoordinator}
+   *         does not exist.
+   */
+  public void restartConnectorTraversal() throws ConnectorNotFoundException;
+
+  /**
+   * Sets the {@link Schedule} for this {@link ConnectorCoordinator}. If this
+   * {@link ConnectorCoordinator} supports persistence this will persist the
+   * new Schedule.
+   *
+   * @throws ConnectorNotFoundException if this {@link ConnectorCoordinator}
+   *         does not exist.
+   */
+  public void setConnectorSchedule(Schedule connectorSchedule)
+      throws ConnectorNotFoundException;
+
+  /**
+   * Returns the {@link Schedule} for this {@link ConnectorCoordinator}.
+   *
+   * @throws ConnectorNotFoundException if this {@link ConnectorCoordinator}
+   *         does not exist.
+   */
+  public Schedule getConnectorSchedule() throws ConnectorNotFoundException;
+
+  /**
+   * Set the Connector's traversal state.
+   *
+   * @param state a String representation of the state to store.
+   *        If null, any previous stored state is discarded.
+   * @throws ConnectorNotFoundException if this {@link ConnectorCoordinator}
+   *         does not exist.
+   */
+  public void setConnectorState(String state)
+     throws ConnectorNotFoundException;
+
+  /**
+   * Returns the Connector's traversal state.
+   *
+   * @return String representation of the stored state, or
+   *         null if no state is stored.
+   * @throws ConnectorNotFoundException if this {@link ConnectorCoordinator}
+   *         does not exist.
+   */
+  public String getConnectorState() throws ConnectorNotFoundException;
+
+  /**
+   * Returns the type name for this {@link ConnectorCoordinator}.
+   *
+   * @throws ConnectorNotFoundException if this {@link ConnectorCoordinator}
+   *         does not exist.
+   */
+  public String getConnectorTypeName() throws ConnectorNotFoundException;
+
+  /**
+   * Sets the configuration for this {@link ConnectorCoordinator}. If this
+   * {@link ConnectorCoordinator} supports persistence this will persist the new
+   * configuration.
+   * <p>
+   * Upon success this function returns
+   * <OL>
+   * <LI>null
+   * <LI> {@link ConfigureResponse} with a null message
+   * <LI> {@link ConfigureResponse} with a zero length message
+   * </OL>
+   * <p>
+   * Upon success if this modifies the configuration parameters then calling
+   * {@link ConfigureResponse#getConfigData()} on the returned value returns the
+   * updated configuration parameters.
+   *
+   * @param newTypeInfo the new {@link TypeInfo} for this
+   *        {@link ConnectorCoordinator}.
+   * @param configuration the replacement {@link Configuration} properties for
+   *        this {@link ConnectorCoordinator}.
+   * @param locale the locale for use in constructing the returned
+   *        {@link ConfigureResponse}.
+   * @param update true means to update and existing configuration and flase
+   *        means to create a new one.
+   *
+   * @throws ConnectorNotFoundException if {@link #exists()} returns false and
+   *         update is true.
+   * @throws ConnectorExistsException {@link #exists()} returns true and update
+   *         is false.
+   */
+  public ConfigureResponse setConnectorConfiguration(TypeInfo newTypeInfo,
+      Configuration configuration, Locale locale, boolean update)
+      throws ConnectorNotFoundException, ConnectorExistsException,
+      InstantiatorException;
+
+  /**
+   * Returns the {@link Configuration} for this {@link ConnectorCoordinator}.
+   *
+   * @throws ConnectorNotFoundException if this {@link ConnectorCoordinator}
+   *         does not exist.
+   */
+  public Configuration getConnectorConfiguration()
+      throws ConnectorNotFoundException;
+
+  /**
+   * Sets {@code GData} configuration for GData aware Connectors.
+   */
+  public void setGDataConfig()
+      throws ConnectorNotFoundException, InstantiatorException;
+
+  /**
+   * Starts running a batch for this {@link ConnectorCoordinator} if a batch is
+   * not already running.
+   *
+   * @return true if this call started a batch
+   * @throws ConnectorNotFoundException if this {@link ConnectorCoordinator}
+   *         does not exist.
+   */
+  public boolean startBatch() throws ConnectorNotFoundException;
+
+  /**
+   * Shuts down this {@link ConnectorCoordinator} if {@link #exists()}.
+   */
+  public void shutdown();
+}
---
+++ 2.8.4-RC3/projects/connector-manager/source/java/com/google/enterprise/connector/servlet/ConnectorMessageCode.java
@@ -0,0 +1,191 @@
+// Copyright (C) 2006 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+package com.google.enterprise.connector.servlet;
+
+/**
+ * Error/message codes returning from Connector Manager to GSA AdminConsole
+ *
+ */
+public class ConnectorMessageCode {
+  public static final int SUCCESS = 0;
+
+  /**
+   * IDs from 5210 to 6000 are reserved for GSA / Connector Manager
+   */
+  public static final int RESPONSE_HTTP_FAIL = 5210;
+  public static final int RESPONSE_EMPTY_REQUEST = 5211;
+  public static final int RESPONSE_EMPTY_RESPONSE = 5212;
+  public static final int RESPONSE_EMPTY_NODE = 5213;
+  public static final int RESPONSE_STATUS_ID_NON_INTEGER = 5214;
+  public static final int RESPONSE_NULL_CONNECTOR = 5215;
+  public static final int RESPONSE_NULL_CONNECTOR_TYPE = 5216;
+  public static final int RESPONSE_NULL_CONNECTOR_STATUS = 5217;
+  public static final int RESPONSE_NULL_FORM_SNIPPET = 5218;
+  public static final int RESPONSE_NULL_CONFIG_DATA = 5219;
+  public static final int RESPONSE_NULL_CONFIGURE = 5220;
+  public static final int RESPONSE_NULL_RESOURCE = 5221;
+  public static final int RESPONSE_NULL_DOCID = 5222;
+  public static final int INVALID_CONNECTOR_CONFIG = 5223;
+  public static final int RESPONSE_NULL_IDENTITY = 5224;
+
+  public static final int ERROR_PARSING_XML_REQUEST = 5300;
+  public static final int EXCEPTION_CONNECTOR_MANAGER = 5301;
+  public static final int EXCEPTION_CONNECTOR_EXISTS = 5302;
+  public static final int EXCEPTION_CONNECTOR_NOT_FOUND = 5303;
+  public static final int EXCEPTION_CONNECTOR_TYPE_NOT_FOUND = 5304;
+  public static final int EXCEPTION_INSTANTIATOR = 5305;
+  public static final int EXCEPTION_PERSISTENT_STORE = 5306;
+  public static final int EXCEPTION_THROWABLE = 5307;
+
+  public static final int EXCEPTION_IO = 5308;
+  public static final int EXCEPTION_MALFORMED_URL = 5309;
+  public static final int EXCEPTION_NUMBER_FORMAT = 5310;
+  public static final int EXCEPTION_SAX = 5311;
+  public static final int EXCEPTION_HTTP_CONNECTION = 5312;
+  public static final int EXCEPTION_HTTP_SERVLET = 5313;
+
+  public static final int DUPLICATE_CONNECTOR_MANAGER_ID = 5400;
+  public static final int INVALID_CONNECTOR_MANAGER_NAME = 5401;
+  public static final int INVALID_CONNECTOR_MANAGER_URL = 5402;
+  public static final int MISMATCHED_CONNECTOR_MANAGER = 5403;
+  public static final int UNKNOWN_CONNECTOR_MANAGER_ID = 5404;
+  public static final int INVALID_CONNECTOR_MANAGER_TRUSTED_IP = 5405;
+  public static final int CANNOT_CERT_AUTH_ON_HTTPS = 5406;
+
+  public static final int CANNOT_ADD_INTERNAL_CONNECTOR_MANAGER = 5407;
+  public static final int ATTEMPT_TO_CHANGE_INTERNAL_MANAGER = 5408;
+  public static final int ATTEMPT_TO_DELETE_INTERNAL_MANAGER = 5409;
+  public static final int ATTEMPT_TO_DELETE_ACTIVE_MANAGER = 5410;
+
+  public static final int INVALID_CONNECTOR_NAME = 5411;
+  public static final int MISSING_OR_UNKNOWN_CONNECTOR_SCHEDULE_VERSION = 5412;
+  public static final int OVERLAP_BAD_SCHEDULE = 5413;
+  public static final int CONNECTOR_DISCONNECTED = 5414;
+  public static final int CONNECTOR_MANAGER_DISCONNECTED = 5415;
+
+  public static final int ATTEMPT_TO_CHANGE_LOCKED_CONNECTOR_MANAGER = 5416;
+
+  // Specific success message codes - 5500-5599
+  public static final int FIRST_SUCCESS_MESSAGE_CODE = 5500;
+  public static final int SUCCESS_RESTART_TRAVERSAL = 5500;
+  public static final int LAST_SUCCESS_MESSAGE_CODE = 5599;
+
+  /** A code with no message in the translation bundle, for testing. */
+  public static final int UNDEFINED_CODE = 5996;
+  public static final int UNSUPPORTED_CALL = 5997;
+  public static final int UNSUPPORTED_MESSAGE_CODE = 5998;
+  public static final int WRONG_MESSAGE_CODE = 5999;
+  public static final int LAST_CONNECTOR_MESSAGE_CODE = 6000;
+
+  public static final Object[] EMPTY_PARAMS = new Object[0];
+
+  /**
+   * Performs a copy of the given array. To mimic a deep copy and
+   * isolate the copy from changes to the original, we call
+   * {@code toString} on each entry.
+   *
+   * @param params an array of message parameters; may be {@code null}
+   * @return an independent non-null copy of the array; an empty array
+   *     if the argument was {@code null}
+   */
+  private static Object[] copyOf(Object[] params) {
+    if (params == null || params.length == 0) {
+      return EMPTY_PARAMS;
+    } else {
+      Object[] copy = new Object[params.length];
+      for (int i = 0; i < params.length; i++) {
+        copy[i] = params[i].toString();
+      }
+      return copy;
+    }
+  }
+
+  private int messageId;
+  private String message;
+  private Object[] params;
+
+  public ConnectorMessageCode() {
+    this(SUCCESS);
+  }
+
+  public ConnectorMessageCode(int messageId) {
+    set(messageId, null, EMPTY_PARAMS);
+  }
+
+  public ConnectorMessageCode(int messageId, Object[] params) {
+    set(messageId, null, copyOf(params));
+  }
+
+  public ConnectorMessageCode(int messageId, String param) {
+    set(messageId, null, new Object[] { param });
+  }
+
+  public ConnectorMessageCode(
+      int messageId, String message, Object[] params) {
+    set(messageId, message, copyOf(params));
+  }
+
+  /**
+   * Helper method to assign all of the fields. This method assumes
+   * that parameter checking has been done.
+   */
+  private void set(int messageId, String message, Object[] params) {
+    this.messageId = messageId;
+    this.message = message;
+    this.params = params;
+  }
+
+  public boolean isSuccess() {
+    return (messageId == SUCCESS);
+  }
+  
+  public boolean isSuccessMessage() {
+    return (messageId >= FIRST_SUCCESS_MESSAGE_CODE &&
+            messageId <= LAST_SUCCESS_MESSAGE_CODE);
+  }
+
+  public boolean hasMessage() {
+    return (!isSuccess() && message != null && message.length() > 1);
+  }
+
+  public int getMessageId() {
+    return messageId;
+  }
+
+  public void setMessageId(int messageId) {
+    this.messageId = messageId;
+    if (messageId == SUCCESS) {
+      this.message = null;
+      this.params = EMPTY_PARAMS;
+    }
+  }
+
+  public Object[] getParams() {
+    return params;
+  }
+
+  public void setParams(Object[] params) {
+    this.params = copyOf(params);
+  }
+
+  public String getMessage() {
+    return message;
+  }
+
+  public void setMessage(String message) {
+    this.message = message;
+  }
+}
