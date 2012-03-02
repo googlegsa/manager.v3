be681b60501116defa1fb1171f636d1d5ef92f7c
Brett Michael Johnson <Brett.Michael.Johnson@gmail.com>
2012-03-02 20:19:23 +0000

    Branch 3.0.x: Revert the GetConfig servlet, restoring the returned zipped directory behavior. See r2803 for details.

--- projects/connector-manager/source/java/com/google/enterprise/connector/servlet/GetConfig.java
+++ projects/connector-manager/source/java/com/google/enterprise/connector/servlet/GetConfig.java
@@ -14,30 +14,56 @@
 
 package com.google.enterprise.connector.servlet;
 
-import com.google.enterprise.connector.importexport.ExportConnectors;
-import com.google.enterprise.connector.importexport.ExportManager;
 import com.google.enterprise.connector.logging.NDC;
 import com.google.enterprise.connector.manager.Context;
 
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FilenameFilter;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.zip.ZipOutputStream;
+import java.util.zip.ZipEntry;
 
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
  * <p>Admin servlet to retrieve the configuration of the Connector Manager
- * and all Connector instances.  This servlet returns an XML document
- * containing the configuration.  Access to this servlet is restricted to
- * either localhost or gsa.feed.host, based upon the HTTP RemoteAddress.</p>
+ * and all Connector instances.  This servlet returns a ZIP archive of all
+ * the configuration files.  Access to this servlet is restricted to either
+ * localhost or gsa.feed.host, based upon the HTTP RemoteAddress.</p>
  *
  * <p><b>Usage:</b>
- * <br>To retrieve the configuration as an XML document:
+ * <br>To retrieve the zipped configuration archive:
  * <br><pre>  http://[cm_host_addr]/connector-manager/getConfig</pre>
+ * <br>or
+ * <br><pre>  http://[cm_host_addr]/connector-manager/getConfig/configuration.zip</pre>
  * </p>
+ *
+ * <p><br><b>Redirects and curl:</b>
+ * When requesting the configuration files, this servlet returns a redirect
+ * to the actual ZIP filename, configuration.zip.
+ * This allows the browser, wget, or curl to pull the true filename off
+ * the redirected URL so that it can name the file when storing locally.
+ * If using curl to retrieve the files, please to use 'curl -L' to tell
+ * curl to follow the redirect.  Unfortunately, when using 'curl -O' to
+ * save the file locally, curl uses the pre-redirected name, rather than
+ * the post-redirected name when naming the local file.  This forces you
+ * to use 'curl -L -o output_filename'.</p>
+ *
+ * <p>Wget handles redirects appropriately without intervention, and names
+ * the saved file as expected.</p>
  */
 public class GetConfig extends HttpServlet {
+  private static final String archiveName = "configuration.zip";
+  private static final String[] excludedFiles =
+      { "lib", "connector_manager.keystore", "web.xml" };
+  private static final FilenameFilter fileFilter = new ConfigFilenameFilter();
 
   /**
    * Retrieves the Configuration files for the Connector Manager and all the
@@ -49,7 +75,7 @@ public class GetConfig extends HttpServlet {
    */
   @Override
   protected void doPost(HttpServletRequest req, HttpServletResponse res)
-      throws IOException {
+      throws IOException, FileNotFoundException {
     doGet(req, res);
   }
 
@@ -63,7 +89,7 @@ public class GetConfig extends HttpServlet {
    */
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse res)
-      throws IOException {
+      throws IOException, FileNotFoundException {
     // Make sure this requester is OK
     if (!RemoteAddressFilter.getInstance()
           .allowed(RemoteAddressFilter.Access.RED, req.getRemoteAddr())) {
@@ -71,14 +97,33 @@ public class GetConfig extends HttpServlet {
       return;
     }
 
-    res.setContentType(ServletUtil.MIMETYPE_XML);
-    PrintWriter out = res.getWriter();
-    NDC.pushAppend("Support");
+    NDC.push("Support");
     try {
-      handleDoGet(out);
+      Context context = Context.getInstance();
+
+      // Fetch the name of the archive file to return. getPathInfo() returns
+      // items with a leading '/', so we want to pull off only the basename.
+      // WARNING: For security reasons, the PathInfo parameter must never
+      // be passed directly to a File() or shell command.
+      String fileName = baseName(req.getPathInfo());
+      if (fileName == null) {
+        // Force a redirect to the archive file.
+        res.sendRedirect(res.encodeRedirectURL(
+            baseName(req.getServletPath()) + "/" + archiveName));
+      } else if (fileName.equalsIgnoreCase(archiveName)) {
+        res.setContentType(ServletUtil.MIMETYPE_ZIP);
+        ServletOutputStream out = res.getOutputStream();
+        try {
+          handleDoGet(context.getCommonDirPath(), out);
+        } finally {
+          out.close();
+        }
+      } else {
+        // Force a redirect to the archive file.
+        res.sendRedirect(res.encodeRedirectURL(archiveName));
+      }
     } finally {
-      out.close();
-      NDC.pop();
+      NDC.clear();
     }
   }
 
@@ -95,14 +140,116 @@ public class GetConfig extends HttpServlet {
   /**
    * Handler for doGet in order to do unit tests.
    *
-   * @param out PrintWriter where the response is written
+   * @param configDir root of configuration files
+   * @param out OutputStream where the response is written
+   * @throws IOException
+   * @throws FileNotFoundException
+   */
+  public static void handleDoGet(String configDir, OutputStream out)
+      throws IOException, FileNotFoundException {
+    File dir = new File(configDir);
+    String relativeDir = dir.getParentFile().getCanonicalPath() + "/";
+
+    if (dir.exists() && dir.isDirectory()) {
+      //create a ZipOutputStream to zip the data to
+      ZipOutputStream zout = new ZipOutputStream(out);
+      zipDir(relativeDir, dir, zout);
+      //close the stream
+      zout.finish();
+    } else {
+      throw new FileNotFoundException("Configuration directory "
+          + configDir + " not found.");
+    }
+  }
+
+  /**
+   * Add the directory and its contents to the ZipOutputStream
+   *
+   * @param relativeDir the directory make ZipEntries relative to.
+   * @param dir the directory to add to the ZIP file.
+   * @param zout a ZipOutputStream
    * @throws IOException
    */
-  public static void handleDoGet(PrintWriter out)
+  public static void zipDir(String relativeDir, File dir, ZipOutputStream zout)
       throws IOException {
-    Context context = Context.getInstance();
-    ExportConnectors exportConnectors = (ExportConnectors)
-        context.getRequiredBean("ExportConnectors", ExportConnectors.class);
-    new ExportManager().toXml(out, 0, exportConnectors.getConnectors());
+    // Get a listing of the directory content.
+    String[] dirList = dir.list(fileFilter);
+    // Loop through dirList, and zip the files.
+    for (int i = 0; i < dirList.length; i++) {
+      File file = new File(dir, dirList[i]);
+      if (file.isDirectory()) {
+        // If the File object is a directory, call this
+        // function again to add its content recursively.
+        zipDir(relativeDir, file, zout);
+        continue;
+      }
+      // We have plain file, not a directory.
+      // Add a ZIP entry describing the file.
+      String relativePath = file.getCanonicalPath();
+      if (relativePath.startsWith(relativeDir)) {
+        relativePath = relativePath.substring(relativeDir.length());
+      }
+      ZipEntry zentry = new ZipEntry(relativePath);
+      zentry.setSize(file.length());
+      zentry.setTime(file.lastModified());
+      zout.putNextEntry(zentry);
+
+      // Copy the file to the compressed stream.
+      InputStream in = new FileInputStream(file);
+      byte[] buf = new byte[16384];
+      int bytesRead;
+      while ((bytesRead = in.read(buf)) > 0) {
+        zout.write(buf, 0, bytesRead);
+      }
+      in.close();
+      zout.closeEntry();
+    }
+  }
+
+  /**
+   * Return the base filename part of the pattern or log name.
+   * For instance "/x/y/z"  returns "z", "/x/y/" returns "".
+   *
+   * @param name unix-style pathname or pattern.
+   * @return the base filename (may be null or empty)
+   */
+  private static String baseName(String name) {
+    if (name != null) {
+      // FileHandler patterns use '/' as separatorChar by default.
+      int sep = name.lastIndexOf('/');
+      // If no '/', then look for system separatorChar.
+      if ((sep == -1) && (File.separatorChar != '/')) {
+        sep = name.lastIndexOf(File.separatorChar);
+      }
+      return name.substring(sep + 1);
+    }
+    return null;
+  }
+
+  /**
+   * This is a FilenameFilter that filters out configuration
+   * files that we do not wish to return.
+   */
+  private static class ConfigFilenameFilter implements FilenameFilter {
+    /**
+     * Tests if the specified file matches one of the files to exclude.
+     *
+     * @param dir the directory containing the file.
+     * @param fileName a file in the directory.
+     * @returns true if the fileName is not excluded, false otherwise.
+     */
+    public boolean accept(File dir, String fileName) {
+      // Ignore ".", "..", hidden files, and old file versions.
+      if (fileName.startsWith(".") || fileName.endsWith("~")) {
+        return false;
+      }
+      // Iterate over the excluded filenames, looking for a match.
+      for (int i = 0; i < excludedFiles.length; i++) {
+        if (fileName.equalsIgnoreCase(excludedFiles[i])) {
+          return false;
+        }
+      }
+      return true;
+    }
   }
 }
